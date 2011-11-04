d6dc8890286480f45b2807f0fa512b99d00986bb
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-11-04 17:28:00 -0400

    install script to create a shortcut in users startup and manually start the script this time

---
+++ dtksneak/tools/chocolateyInstall.ps1
@@ -0,0 +1,32 @@
+﻿$name = 'autohotkey'
+
+$startup      = "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+
+$tools        = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$content      = Join-Path (Split-Path $tools) "content"
+
+$targetPath   = Join-Path "$content" "dtk.ahk"
+$iconPath     = Join-Path "$content" "dtk.ico"
+$shortcutPath = Join-Path "$startup" "dtksneak.lnk"
+
+try 
+{ 
+  #Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage
+  #Run-ChocolateyProcessAsAdmin 'STATEMENTS_TO_RUN' 'Optional_Application_If_Not_PowerShell'
+  
+  $shell = New-Object -ComObject "Wscript.Shell"
+  $shortcut = $shell.CreateShortcut($shortcutPath)
+  $shortcut.TargetPath = $targetPath  
+  $shortcut.WorkingDirectory = $content
+  $shortcut.IconLocation = $iconPath
+  $shortcut.Save()
+  
+  & $shortcutPath
+  
+  Write-ChocolateySuccess "$name"
+} 
+catch 
+{
+  Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
+  throw 
+}
