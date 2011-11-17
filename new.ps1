56cd160fd19d241bf7ec0e85700fb921c5f5c74e
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-11-17 13:39:34 -0500

    created a script to make a new package/branch quickly

---
+++ new.ps1
@@ -0,0 +1,20 @@
+param([string]$name)
+
+git checkout -b $name
+
+Copy-Item "_template" "$name" -recurse
+
+Push-Location "$name"
+
+$nuspec = "$name.nuspec"
+
+Rename-Item "__NAME__.nuspec" "$nuspec"
+Get-Content "$nuspec" `
+  | %{ $_ -replace "__NAME__" "$name" } `
+  | Set-Content "$nuspec"
+
+git add .
+git commit -am "created new package: $name"
+git push origin "$name"
+
+Pop-Location "$name"
