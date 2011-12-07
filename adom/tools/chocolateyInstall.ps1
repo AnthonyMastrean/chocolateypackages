0e43adc3ad607b4e2d20412663e62c6c4ad29b8b
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-12-07 02:26:44 -0500

    added a block comment discussing the save scum bat file

--- adom/tools/chocolateyInstall.ps1
+++ adom/tools/chocolateyInstall.ps1
@@ -5,24 +5,64 @@ try
     $tools   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
     $content = Join-Path (Split-Path $tools) 'content'
 
-    # We should rename the file in the ZIP to a non version specific 'adom.exe'
+    # We should rename the file in the ZIP to a non version 
+    # specific 'adom.exe'
+
     Install-ChocolateyZipPackage "$name" 'http://www.adom.de/adom/download/windows/111/adom_winbeta4.zip' "$content"
     Move-Item "$content\adom_winbeta4.exe" "$content\adom.exe" -Force
     
-    # ADOM writes an 'ADOM_DAT' directory in the current directory at startup
-    # and as part of save game system. We need to modify the batch file to 
-    # push the 'content' directory, with the ADOM executable and after the 
-    # command, return to the normal directory.
-    
+    # ADOM writes an 'ADOM_DAT' directory in the current 
+    # directory at startup. We want that directory to be
+    # in the 'content directory by default.
+
     $bat = "$ENV:CHOCOLATEYINSTALL\bin\adom.bat"
-    $strings = Get-Content $bat
     
 @"
-$strings[0]
+@echo off
 pushd "$content"
-$strings[1]
+adom.exe %*
 popd
 "@ | Out-File $bat -Encoding ASCII
+    
+    <#
+        There's a sneaky way around the permanent death 
+        in ADOM. Copy your save files to a temp location
+        and restore them when needed! Here's what an 
+        adom.bat file would look like with save-scumming
+        enabled.
+        
+        Usage:
+        
+            adom save (backup the save game)
+            adom load (load a backup)
+            adom      (play normally)
+        
+        ===================================================
+    
+        @echo off
+
+        SET CONTENT="$content"
+
+        if '%1'=='save' goto save
+        if '%1'=='load' goto load
+
+        pushd %CONTENT%
+        adom.exe %*
+        popd
+
+        goto :eof
+
+        :save
+        mkdir "%CONTENT%\backup"
+        copy "%CONTENT%\adom_dat\savedg" "%CONTENT%\backup"
+
+        goto :eof
+
+        :load
+        copy "%CONTENT%\backup" "%CONTENT%\adom_dat\savedg"
+
+        goto :eof
+    #>
     
     Write-ChocolateySuccess "$name"
 } 
