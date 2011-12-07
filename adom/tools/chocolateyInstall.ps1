7ef49b3d74cc5144dd692c0d7c057cb84ed50dfa
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-12-07 02:05:36 -0500

    added the pushd/popd commands to the adom bat file to fix the current directory when the game runs. but it won't work. choco makes the bat file _after_ the install runs.

--- adom/tools/chocolateyInstall.ps1
+++ adom/tools/chocolateyInstall.ps1
@@ -5,9 +5,24 @@ try
     $tools   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
     $content = Join-Path (Split-Path $tools) 'content'
 
+    # We should rename the file in the ZIP to a non version specific 'adom.exe'
     Install-ChocolateyZipPackage "$name" 'http://www.adom.de/adom/download/windows/111/adom_winbeta4.zip' "$content"
-        
     Move-Item "$content\adom_winbeta4.exe" "$content\adom.exe" -Force
+    
+    # ADOM writes an 'ADOM_DAT' directory in the current directory at startup
+    # and as part of save game system. We need to modify the batch file to 
+    # push the 'content' directory, with the ADOM executable and after the 
+    # command, return to the normal directory.
+    
+    $bat = "$ENV:CHOCOLATEYINSTALL\bin\adom.bat"
+    $strings = Get-Content $bat
+    
+@"
+$strings[0]
+pushd "$content"
+$strings[1]
+popd
+"@ | Out-File $bat -Encoding ASCII
     
     Write-ChocolateySuccess "$name"
 } 
