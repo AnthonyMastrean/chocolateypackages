9cb418df0de87bc571257607bc5d84fcdb0fdbf4
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-01-04 20:03:46 -0500

    new script to tag and publish

---
+++ Publish-Package.ps1
@@ -0,0 +1,11 @@
+params([switch]$createOnly)
+
+$nuspec  = Resolve-Path *.nuspec
+$xml     = [xml] (Get-Content $nuspec)
+$version = $xml.package.metadata.version
+
+git tag -a "v$version" -m "Publishing version: $version"
+
+$package = Resolve-Path *.nupkg
+
+nuget push $package -source 'http://chocolatey.org/' "$(if($createOnly) { '-CreateOnly' })"
