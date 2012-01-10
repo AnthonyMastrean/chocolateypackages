778e8717277b7dfe06bdd2fc3d6087c40af8285c
AnthonyMastrean <anthony.mastrean@gmail.com>
2012-01-10 08:58:38 -0500

    got the silent params right-ish

--- javaruntime/tools/chocolateyInstall.ps1
+++ javaruntime/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 $name = 'javaruntime'
 $url  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=58124'
-Install-ChocolateyPackage $name 'EXE' '/S' $url
+Install-ChocolateyPackage $name 'EXE' '/QN /NORESTART' $url
