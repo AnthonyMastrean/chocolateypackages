6a36d2af89c914b44ebb953332991396e2685e06
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-02-21 00:42:12 -0500

    updated to 6.0.31 and trying to fix paths

--- javaruntime.x64/tools/chocolateyInstall.ps1
+++ javaruntime.x64/tools/chocolateyInstall.ps1
@@ -1,3 +1,23 @@
-$name = 'javaruntime.x64'
-$url  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=58126'
-Install-ChocolateyPackage $name 'EXE' '/QN /NORESTART' $url
+try
+{
+	$name = 'javaruntime.x64'
+	$url  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=60338'
+	
+	Install-ChocolateyPackage $name 'EXE' '/QN /NORESTART' $url
+	
+	$java = Join-Path $env:ProgramFiles 'Java\jre6'
+	$bin  = Join-Path $java 'bin'
+    $cmd  = @"
+[Environment]::SetEnvironmentVariable('JAVA_HOME', "$java", 'Machine')
+"@
+	
+	Start-ChocolateyProcessAsAdmin $cmd
+	Install-ChocolateyPath $bin 'Machine'
+	
+	Write-ChocolateySuccess $name
+}
+catch
+{
+	Write-ChocolateyFailure $name $($_.Exception.Message)
+	return
+}
--- javaruntime/tools/chocolateyInstall.ps1
+++ javaruntime/tools/chocolateyInstall.ps1
@@ -1,3 +1,23 @@
-$name = 'javaruntime'
-$url  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=58124'
-Install-ChocolateyPackage $name 'EXE' '/QN /NORESTART' $url
+try
+{
+	$name = 'javaruntime'
+	$url  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=61043'
+	
+	Install-ChocolateyPackage $name 'EXE' '/QN /NORESTART' $url
+	
+	$java = Join-Path ${env:ProgramFiles(x86)} 'Java\jre6'
+	$bin  = Join-Path $java 'bin'
+    $cmd  = @"
+[Environment]::SetEnvironmentVariable('JAVA_HOME', "$java", 'Machine')
+"@
+	
+    Start-ChocolateyProcessAsAdmin $cmd
+    Install-ChocolateyPath $bin 'Machine'
+	
+	Write-ChocolateySuccess $name
+}
+catch
+{
+	Write-ChocolateyFailure $name $($_.Exception.Message)
+	return
+}
