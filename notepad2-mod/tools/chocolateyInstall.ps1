a3b48817896c7434bcc60173a1b671a9755716d3
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-03-20 10:58:46 -0400

    correct static url

--- notepad2-mod/tools/chocolateyInstall.ps1
+++ notepad2-mod/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 $name = 'notepad2-mod'
-$url  = 'http://code.google.com/p/notepad2-mod/downloads/detail?name=Notepad2-mod.4.2.25.720.exe'
+$url  = 'http://notepad2-mod.googlecode.com/files/Notepad2-mod.4.2.25.720.exe'
 $args = '/VERYSILENT'
 Install-ChocolateyPackage $name 'EXE' $args $url
