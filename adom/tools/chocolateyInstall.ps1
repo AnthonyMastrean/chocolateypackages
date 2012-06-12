f65033fe9f83eef2fe50436996e3438783a517c2
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-06-11 23:35:44 -0400

    updated the installer to the latest idioms

--- adom/tools/chocolateyInstall.ps1
+++ adom/tools/chocolateyInstall.ps1
@@ -1,73 +1,43 @@
+$name = 'adom'
+$url  = 'http://www.adom.de/adom/download/windows/111/adom_winbeta4.zip'
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat     = "$ENV:CHOCOLATEYINSTALL\bin\adom.bat"
+
 try 
 { 
-    $name = 'adom'
-    
-    $tools   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-    $content = Join-Path (Split-Path $tools) 'content'
-
-    # We should rename the file in the ZIP to a non version 
-    # specific 'adom.exe'
-
-    Install-ChocolateyZipPackage "$name" 'http://www.adom.de/adom/download/windows/111/adom_winbeta4.zip' "$content"
-    Move-Item "$content\adom_winbeta4.exe" "$content\adom.exe" -Force
-    
-    # ADOM writes an 'ADOM_DAT' directory in the current 
-    # directory at startup. We want that directory to be
-    # in the 'content directory by default.
-
-    $bat = "$ENV:CHOCOLATEYINSTALL\bin\adom.bat"
-    
+    Install-ChocolateyZipPackage $name $url $content
+    Move-Item $content\adom_winbeta4.exe $content\adom.exe -force
+ 
 @"
 @echo off
-pushd "$content"
+
+if '%1'=='save' goto save
+if '%1'=='load' goto load
+
+:: ADOM writes an 'ADOM_DAT' directory in the current directory at startup. 
+:: We want that directory to be in the 'content directory by default.
+
+pushd %~dp0
 adom.exe %*
 popd
+goto :eof
+
+:save
+mkdir "%~dp0\backup"
+copy "%~dp0\adom_dat\savedg" "%~dp0\backup"
+goto :eof
+
+:load
+copy "%~dp0\backup" "%~dp0\adom_dat\savedg"
+goto :eof
 "@ | Out-File $bat -Encoding ASCII
     
-    <#
-        There's a sneaky way around the permanent death 
-        in ADOM. Copy your save files to a temp location
-        and restore them when needed! Here's what an 
-        adom.bat file would look like with save-scumming
-        enabled.
-        
-        Usage:
-        
-            adom save (backup the save game)
-            adom load (load a backup)
-            adom      (play normally)
-        
-        ===================================================
-    
-        @echo off
-
-        SET CONTENT="$content"
-
-        if '%1'=='save' goto save
-        if '%1'=='load' goto load
-
-        pushd %CONTENT%
-        adom.exe %*
-        popd
-
-        goto :eof
-
-        :save
-        mkdir "%CONTENT%\backup"
-        copy "%CONTENT%\adom_dat\savedg" "%CONTENT%\backup"
-
-        goto :eof
-
-        :load
-        copy "%CONTENT%\backup" "%CONTENT%\adom_dat\savedg"
-
-        goto :eof
-    #>
-    
-    Write-ChocolateySuccess "$name"
+    Write-ChocolateySuccess $name
 } 
 catch 
 {
-    Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
+    Write-ChocolateyFailure $name $($_.Exception.Message)
     throw 
 }
