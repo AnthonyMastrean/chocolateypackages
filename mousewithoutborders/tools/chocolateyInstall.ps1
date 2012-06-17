8ec7f7ca3f921554c090f4e9ae13bb63446ef76c
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-06-16 23:40:39 -0400

    trying to update to 2.1.2, but there aren't any silent switches!

--- mousewithoutborders/tools/chocolateyInstall.ps1
+++ mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,2 +1,6 @@
-﻿$name = 'mousewithoutborders'
-Install-ChocolateyPackage "$name" 'MSI' '/quiet' 'http://aka.ms/MouseWithOutBorders' 
+﻿$name   = 'mousewithoutborders'
+$url    = 'https://www.yousendit.com/directDownload?phi_action=app/directDownload&fl=SWhZekZqMGM5RmJtcXV2RkN4RFFIVE9yZWt5UmdteDRsUjJuWENHRzVZbz0'
+$type   = 'MSI'
+$silent = '/quiet'
+
+Install-ChocolateyPackage $name $type $silent $url
