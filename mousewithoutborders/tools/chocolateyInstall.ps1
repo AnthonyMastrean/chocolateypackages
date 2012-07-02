767abda602dee3e020378dda9bb8d23d9c8fe861
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-07-02 11:52:24 -0400

    new version, one feature

--- mousewithoutborders/tools/chocolateyInstall.ps1
+++ mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name   = 'mousewithoutborders'
-$url    = 'https://www.yousendit.com/directDownload?phi_action=app/directDownload&fl=SWhZekZqMGM1UjY1eDhSYTJVYnVnVE9yZWt5UmdteDRsUjJuWENHRzVZbz0&experience=bas'
+$url    = 'https://www.yousendit.com/directDownload?phi_action=app/directDownload&fl=SWhZekZoZ1BsamNaQ1lONjJVYnVnVE9yZWt5UmdteDRsUjJuWENHRzVZbz0&experience=bas'
 $type   = 'MSI'
 $silent = '/quiet'
 
