5209e80a41767b56fd9fb277519c40186d769a42
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-07-12 16:59:16 -0400

    tweaked the chocolatey install and nuspec

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+$name    = 'reconsole'
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+    
+try 
+{    
+    $startup      = "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+    $shortcutPath = Join-Path $startup 'reconsole.lnk'
+
+    $shell = New-Object -ComObject 'Wscript.Shell'
+    $shortcut = $shell.CreateShortcut($shortcutPath)
+    $shortcut.WorkingDirectory = $content
+    $shortcut.TargetPath = Join-Path $content 'reconsole.ahk'
+    #$shortcut.IconLocation = Join-Path $content 'reconsole.ico'
+    $shortcut.Save()
+  
+    & $shortcutPath
+  
+    Write-ChocolateySuccess $name
+} 
+catch 
+{
+    Write-ChocolateyFailure $name $($_.Exception.Message)
+    throw 
+}
