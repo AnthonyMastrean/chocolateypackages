af8d9d7b5f44c4ac8c56c486fd18ddbfd87081c8
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-08-21 00:14:15 -0400

    ws

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
-﻿try 
-{ 
-    $name    = 'winperamdisk'
+﻿$name    = 'winperamdisk'
+
+try { 
     $tools   = Split-Path $MyInvocation.MyCommand.Definition
     $content = Join-Path (Split-Path $tools) 'content'
     $bin     = Join-Path $env:chocolateyinstall 'bin'
     
-    # Move the existing batch files to the bin directory
+    # Move the existing batch files to the bin directory, as a hack to get them
+    # in the path!
 	Move-Item $content\*.bat $bin 
     
     Write-ChocolateySuccess $name
 } 
-catch 
-{
-    Write-ChocolateyFailure $name "$($_.Exception.Message)"
+catch {
+    Write-ChocolateyFailure $name $($_.Exception.Message)
     throw 
 }
