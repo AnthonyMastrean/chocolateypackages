ccb0e1e9ae1fa09548f978a6f86c6bcfd83b2360
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-08-28 00:24:45 -0400

    fixed the lnk icon and updated the depends

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,33 +1,25 @@
-﻿function Create-Shortcut
-{
-  $shell = New-Object -ComObject 'Wscript.Shell'
-  $shortcut = $shell.CreateShortcut($shortcutPath)
-  $shortcut.TargetPath = $targetPath  
-  $shortcut.WorkingDirectory = $content
-  $shortcut.IconLocation = $iconPath
-  $shortcut.Save()
-}
+﻿$name    = 'testnamingmode'
+$startup = "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
 
-try 
-{
-    $name = 'testnamingmode'
- 
-    $startup      = "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+try {
+    $script = Join-Path $content 'testnamingmode.ahk'
+    $icon   = Join-Path $content 'testnamingmode.ico'
+    $target = Join-Path $startup 'testnamingmode.lnk'
 
-    $tools        = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-    $content      = Join-Path (Split-Path $tools) 'content'
-
-    $targetPath   = Join-Path "$content" 'testnamingmode.ahk'
-    $iconPath     = Join-Path "$content" 'testnamingmode_16.ico'
-    $shortcutPath = Join-Path "$startup" 'testnamingmode.lnk'
-
-    Create-Shortcut
-    & $shortcutPath
+    $shell = New-Object -ComObject 'Wscript.Shell'
+    $shortcut = $shell.CreateShortcut($target)
+    $shortcut.TargetPath = $script  
+    $shortcut.WorkingDirectory = $content
+    $shortcut.IconLocation = $icon
+    $shortcut.Save()
   
-    Write-ChocolateySuccess "$name"
+    & $target
+  
+    Write-ChocolateySuccess $name
 } 
-catch 
-{
-    Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
+catch {
+    Write-ChocolateyFailure $name $($_.Exception.Message)
     throw 
 }
