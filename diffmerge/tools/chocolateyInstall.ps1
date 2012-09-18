9e7e96da507778d7888dcb272c398ed281230cd1
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-09-18 15:20:26 -0400

    I'm using more variables for easier updating

--- diffmerge/tools/chocolateyInstall.ps1
+++ diffmerge/tools/chocolateyInstall.ps1
@@ -1,2 +1,7 @@
-﻿$name = 'diffmerge'
-Install-ChocolateyPackage "$name" 'MSI' '/quiet' 'http://download-us.sourcegear.com/DiffMerge/3.3.1/Windows/DiffMerge_3.3.1.1118_x86.msi' 'http://download-us.sourcegear.com/DiffMerge/3.3.1/Windows/DiffMerge_3.3.1.1118_x64.msi'
+﻿$name   = 'diffmerge'
+$url    = 'http://download-us.sourcegear.com/DiffMerge/3.3.2/DiffMerge_3.3.2.1139_x86.msi'
+$url64  = 'http://download-us.sourcegear.com/DiffMerge/3.3.2/DiffMerge_3.3.2.1139_x64.msi'
+$kind   = 'MSI'
+$silent = '/quiet'
+
+Install-ChocolateyPackage $name $kind $silent $url $url64
