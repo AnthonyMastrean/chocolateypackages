2f07287c8e1a1513deb13dae44a572d30731a847
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-09-24 20:42:03 -0400

    Publishing keepass.2.20

--- keepass/tools/chocolateyInstall.ps1
+++ keepass/tools/chocolateyInstall.ps1
@@ -1,3 +1,6 @@
-$name = 'keepass'
-$url  = 'http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.18/KeePass-2.18-Setup.exe?r=&ts=1325968281&use_mirror=voxel'
-Install-ChocolateyPackage $name 'EXE' '/VERYSILENT' $url
+$name   = 'keepass'
+$url    = 'http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.20/KeePass-2.20-Setup.exe?r=&ts=1348533391'
+$kind   = 'EXE'
+$silent = '/VERYSILENT'
+
+Install-ChocolateyPackage $name $kind $silent $url
