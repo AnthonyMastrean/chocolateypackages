d9f62fbc0595b06b28dfd7bddcdf47c901eb90e5
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-10-01 15:24:55 -0400

    show my work yesterday

---
+++ chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$name = 'git.alias.standup'
+
+try { 
+  $author = ''
+
+  cmd.exe /c "git config --global alias.standup log --since yesterday --oneline --author '$author'"
+  
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw 
+}
---
+++ readme.markdown
@@ -0,0 +1,4 @@
+# Standup
+What did you do yesterday? Alias `git standup` to
+
+    cmd> git log --since yesterday --oneline --author <user-name>
