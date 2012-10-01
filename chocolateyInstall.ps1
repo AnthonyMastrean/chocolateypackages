b0e28cd492c1a19213e9aca1818e384ddfdc8a20
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-10-01 15:44:53 -0400

    hate using here-strings, but this needs to work in cmd and powershell

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,9 +1,16 @@
 ﻿$name = 'git.alias.standup'
 
 try { 
-  $author = ''
-
-  cmd.exe /c "git config --global alias.standup log --since yesterday --oneline --author '$author'"
+  # Whoa, basically null-coalescing: $author = git config ?? env
+  $author = if($author = cmd.exe /c "git config --global user.name") { 
+    $author 
+  } else { 
+    $author = $ENV:USERNAME 
+  }
+  
+  cmd.exe /c @"
+git config --global alias.standup "log --since yesterday --oneline --author '$author'"
+"@
   
   Write-ChocolateySuccess $name
 } 
