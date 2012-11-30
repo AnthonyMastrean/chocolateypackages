cf534ade298d41ff3d8d4adbcd9cd84ba01970c4
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-11-30 02:59:19 -0500

    fixing some tabs

--- javaruntime/chocolateyInstall.ps1
+++ javaruntime/chocolateyInstall.ps1
@@ -7,19 +7,19 @@ $java   = Join-Path $env:ProgramFiles 'Java\jre7'
 $bin    = Join-Path $java 'bin'
 	
 try {	
-	Install-ChocolateyPackage $name $type $silent $url
+  Install-ChocolateyPackage $name $type $silent $url
 	
   $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
-	if($is64bit) { 
+  if($is64bit) { 
     Install-ChocolateyPackage $name $type $silent $url64
   }
 	
-	Install-ChocolateyPath $bin 'Machine'
+  Install-ChocolateyPath $bin 'Machine'
   Start-ChocolateyProcessAsAdmin @"
 [Environment]::SetEnvironmentVariable('JAVA_HOME', "$java", 'Machine')
 "@
 	
-	Write-ChocolateySuccess $name
+  Write-ChocolateySuccess $name
 }
 catch {
 	Write-ChocolateyFailure $name $($_.Exception.Message)
