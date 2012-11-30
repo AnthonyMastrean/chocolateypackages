382bc9e400771ca82c2ef724c0c6b02ef3bb1f97
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-11-30 03:40:57 -0500

    new package on msdn

---
+++ mousewithoutborders/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = 'mousewithoutborders'
+$url    = 'http://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi'
+$type   = 'MSI'
+$silent = '/quiet'
+
+Install-ChocolateyPackage $name $type $silent $url
--- mousewithoutborders/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$name   = 'mousewithoutborders'
-$url    = 'https://www.yousendit.com/directDownload?phi_action=app/directDownload&fl=SWhZekZoZ1BIcWVFTlpEQVZpT1JCek9yZWt5UmdteDRsUjJuWENHRzVZbz0&experience=bas'
-$type   = 'MSI'
-$silent = '/quiet'
-
-Install-ChocolateyPackage $name $type $silent $url
