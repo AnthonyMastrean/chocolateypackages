f205d643041f49f1645815e1dc505314c1762c95
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-11-30 02:58:23 -0500

    each bitness java package installs to that environment\'s programfiles

--- javaruntime/chocolateyInstall.ps1
+++ javaruntime/chocolateyInstall.ps1
@@ -3,19 +3,21 @@ $url    = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=64152'
 $url64  = 'http://javadl.sun.com/webapps/download/AutoDL?BundleId=64153'
 $type   = 'EXE'
 $silent = '/s'
-
-$java = Join-Path $env:ProgramFiles 'Java\jre7'
-$bin  = Join-Path $java 'bin'
-$cmd  = @"
-[Environment]::SetEnvironmentVariable('JAVA_HOME', "$java", 'Machine')
-"@
-
+$java   = Join-Path $env:ProgramFiles 'Java\jre7'
+$bin    = Join-Path $java 'bin'
+	
 try {	
 	Install-ChocolateyPackage $name $type $silent $url
-	Install-ChocolateyPackage $name $type $silent $url64
 	
-  Start-ChocolateyProcessAsAdmin $cmd
-  Install-ChocolateyPath $bin 'Machine'
+  $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+	if($is64bit) { 
+    Install-ChocolateyPackage $name $type $silent $url64
+  }
+	
+	Install-ChocolateyPath $bin 'Machine'
+  Start-ChocolateyProcessAsAdmin @"
+[Environment]::SetEnvironmentVariable('JAVA_HOME', "$java", 'Machine')
+"@
 	
 	Write-ChocolateySuccess $name
 }
