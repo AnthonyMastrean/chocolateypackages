a6bd84eebb528f1186e572eee0beab08c3350d5d
Justin Dearing <zippy1981@gmail.com>
2012-12-20 15:58:22 -0500

    Now a proper gui version of dependency walker is created.

--- dependencywalker/tools/chocolateyInstall.ps1
+++ dependencywalker/tools/chocolateyInstall.ps1
@@ -3,6 +3,5 @@
 $url = 'http://www.dependencywalker.com/depends22_x86.zip'
 
 $tools = Split-Path -parent $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
 
-Install-ChocolateyZipPackage $name $url $content
+Install-ChocolateyZipPackage $name $url $tools
