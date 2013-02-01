c9633cad99bd4d7eca17c767d6cd134bf6479690
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-01 14:45:02 -0500

    nuget package manager vsix

---
+++ nuget-package-manager/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+$name = 'nuget-package-manager-2010'
+$url  = 'http://visualstudiogallery.msdn.microsoft.com/27077b70-9dad-4c64-adcf-c7cf6bc9970c/file/37502/28/NuGet.Tools.vsix'
+
+Install-ChocolateyVsixPackage $name $url
