1b2b4335ffdfb56a25252a77baa77d0bb6037d39
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-04 12:03:43 -0500

    hot new vsix packages

---
+++ IndentGuides/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+$name = 'IndentGuides'
+$url  = 'http://visualstudiogallery.msdn.microsoft.com/e792686d-542b-474a-8c55-630980e72c30/file/48932/17/IndentGuide.vsix'
+
+Install-ChocolateyVsixPackage $name $url
