ea85f307ebee89c53f705cc3cea1b4d0ad22f33f
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-07 14:41:55 -0500

    deal with that zip archive

--- com0com/chocolateyInstall.ps1
+++ com0com/chocolateyInstall.ps1
@@ -4,13 +4,17 @@ $kind   = 'EXE'
 $silent = '/S'
 
 $tools  = Split-Path $MyInvocation.MyCommand.Definition
-$zip    = Join-Path $tools $zip
-$exe    = Join-Path $tools 'setup.exe'
+$zip    = Join-Path $tools "$name.zip"
+$dir    = Join-Path $tools 'com0com-3.0.0.0-i386-and-x64'
+$exe    = Join-Path $dir  'setup.exe'
 
 try {
   Get-ChocolateyWebFile $name $zip $url
   Get-ChocolateyUnzip $zip $tools
   Install-ChocolateyInstallPackage $name $kind $silent $exe
+  
+  Remove-Item $zip -Force -Recurse
+  Remove-Item $dir -Force -Recurse
 
   Write-ChocolateySuccess $name
 } 
