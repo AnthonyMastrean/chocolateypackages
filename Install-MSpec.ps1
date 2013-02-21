64251ced4a56351998b257ebb2bccd2b6d4dc54b
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-02-21 17:45:17 -0500

    new hotness

--- Install-MSpec.ps1
+++ Install-MSpec.ps1
@@ -1,11 +1,17 @@
+function Get-LatestItem([string]$path) { 
+  $item = @(Resolve-Path $path) | Sort-Object | Select-Last 1  
+  if(-not(Test-Path $path)) {
+    throw "path not found: $path"
+  }
+  
+  $item
+}
+
 # TODO: support choosing an mspec version?
-
-$mspec = @(Resolve-Path "$pwd\packages\Machine.Specifications.*") | Sort-Object | Select-Last 1
-$tools = Join-Path $mspec 'tools'
-
 # TODO: find your installed resharper version by default
 # TODO: support choosing a resharper version?
 
-$bat   = @(Resolve-Path "$tools\InstallResharperRunner.*.bat") | Sort-Object | Select-Last 1
+$mspec = Get-LatestItem -Path 'packages\Machine.Specifications.*'
+$bat   = Get-LatestItem -Path (Join-Path $mspec 'tools\InstallResharperRunner.*.bat')
 
 & $bat
