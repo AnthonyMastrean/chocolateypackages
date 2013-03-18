d4845bfdd18b8ead4526def34a52ac1ff8efb605
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-03-18 11:25:34 -0400

    no gui

--- zoomit/chocolateyInstall.ps1
+++ zoomit/chocolateyInstall.ps1
@@ -1,7 +1,10 @@
 ﻿$name   = 'zoomit'
 $url    = 'http://download.sysinternals.com/files/ZoomIt.zip'
 
-$tools = Split-Path $MyInvocation.MyCommand.Definition
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
+$nogui   = Join-Path $content 'ZoomIt.exe.gui'
+
+New-Item $nogui -Type 'File' -Force
 
 Install-ChocolateyZipPackage $name $url $content
