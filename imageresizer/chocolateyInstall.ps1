8258798c0666c8029392d35da83b809e038e98ae
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-03-18 10:51:23 -0400

    better silent switch

--- imageresizer/chocolateyInstall.ps1
+++ imageresizer/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $name   = 'imageresizer'
 $url    = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=imageresizer&DownloadId=347439&FileTime=129779776836370000&Build=20074'
 $kind   = 'EXE'
-$silent = '/Q'
+$silent = '/quiet /norestart'
 
 Install-ChocolateyPackage $name $kind $silent $url
