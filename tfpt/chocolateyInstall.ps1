2b525d0b3b0a0798e54fce22e064334e1d8431a4
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-04-18 13:47:54 -0400

    add all features

--- tfpt/chocolateyInstall.ps1
+++ tfpt/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$name   = 'tfpt'
 $url    = 'http://visualstudiogallery.msdn.microsoft.com/c255a1e4-04ba-4f68-8f4e-cd473d6b971f/file/35473/4/tfpt.msi'
-$silent = '/Q'
+$silent = '/Q ADDLOCAL=ALL'
 $kind   = 'MSI'
 
 Install-ChocolateyPackage $name $kind $silent $url
