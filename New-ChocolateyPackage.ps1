1f75060a0ba696f144c04ff71d4e2a43a544dfd6
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-18 16:54:24 -0400

    We need physical files to dot-source the functions inside

--- New-ChocolateyPackage.ps1
+++ New-ChocolateyPackage.ps1
@@ -16,6 +16,9 @@
 #>
 param([string]$id)
 
+. .\Debug-ChocolateyPackage.ps1
+. .\Publish-ChocolateyPackage.ps1
+
 function Replace-Token {
   param([string]$token, [string]$replacement, [Parameter(ValueFromPipeline = $true)][string]$path)
   (Get-Content $path) | %{ $_ -replace $token, $replacement } | Set-Content $path
@@ -34,69 +37,3 @@ Get-ChildItem | Replace-Token '__NAME__' $id
 git add -A
 git commit -am "new package: $id"
 git push
-
-function Debug-ChocolateyPackage {
-<#
-    .SYNOPSIS
-    Debug a Chocolatey package.
-    
-    .DESCRIPTION
-    This script will debug the current package. It expects that you 
-    are in the package directory. For example
-    
-        PS\dependencywalker>
-
-    The script will force remove any existing chocolatey package by
-    the same name in your CHOCOLATEYINSTALL directory. It will 
-    recreate the nuget package and invoke 'cinst' on the same in this 
-    directory.
-    
-    .EXAMPLE
-    .\Debug-ChocolateyPackage.ps1
-    
-    Invoke the debug script (located in the root directory) from in 
-    this package directory.
-#>
-
-  $nuspec  = Resolve-Path *.nuspec
-  $nupkg   = Resolve-Path *.nupkg
-  $xml     = [xml] (Get-Content $nuspec)
-
-  $id      = $xml.package.metadata.id
-  $version = $xml.package.metadata.version
-
-  $temp    = Join-Path $ENV:TEMP "chocolatey\$id"
-  $lib     = Join-Path $ENV:ChocolateyInstall "lib\$id.$version"
-
-  '##################################################'
-  "Debugging package: $id.$version"
-  '##################################################'
-
-  Clear-Host
-
-  if(Test-NullPath $nupkg) { Remove-Item $nupkg }
-  if(Test-NullPath $temp)  { Remove-Item $temp -recurse -force }
-  if(Test-NullPath $lib)   { Remove-Item $lib -recurse -force }
-
-  cpack $nuspec
-  cinst $id -source $pwd -force
-}
-
-function Publish-ChocolateyPackage {
-  param([switch]$createOnly)
-
-  $nuspec  = Resolve-Path *.nuspec
-  $xml     = [xml] (Get-Content $nuspec)
-  $id      = $xml.package.metadata.id
-  $version = $xml.package.metadata.version
-
-  git add .
-  git add -u
-  git commit -am "Publishing $id.$version"
-  git tag -a "$id.$version" -m "Publishing $id.$version"
-  git push --tags
-
-  $package = Resolve-Path *.nupkg
-
-  cpush $package "$(if($createOnly) { '-CreateOnly' })"
-}
---
+++ Publish-ChocolateyPackage.ps1
@@ -0,0 +1,18 @@
+function Publish-ChocolateyPackage {
+  param([switch]$createOnly)
+
+  $nuspec  = Resolve-Path *.nuspec
+  $xml     = [xml] (Get-Content $nuspec)
+  $id      = $xml.package.metadata.id
+  $version = $xml.package.metadata.version
+
+  git add .
+  git add -u
+  git commit -am "Publishing $id.$version"
+  git tag -a "$id.$version" -m "Publishing $id.$version"
+  git push --tags
+
+  $package = Resolve-Path *.nupkg
+
+  cpush $package "$(if($createOnly) { '-CreateOnly' })"
+}
