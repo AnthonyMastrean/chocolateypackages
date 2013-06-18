a3b681c2714ab9b833cddf6fd2af2cca1cdf7e11
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-18 16:23:34 -0400

    the token replace isn't working in the install script

--- New-Package.ps1
+++ New-Package.ps1
@@ -3,36 +3,23 @@
     Create a new chocolatey package from the template.
     
     .DESCRIPTION
-    Create a new chocolatey package from the template. First, check
-    out a new branch. Then, replace all the '__NAME__' tokens. And, 
-    finally, commit the new files and push the branch to 'origin'.
+    Copy the template to a new location, by package ID. Replace all 
+    the '__NAME__' tokens. Commit the new files and push.
     
     .PARAMETER id
-    The id of this package. It will be used to create the branch,
-    folder id, and to otherwise replace all the '__NAME__' tokens.
+    The id of this package.
     
     .EXAMPLE
     .\New-Package.ps1 diffmerge
     
-    Create a new branch/package 'diffmerge' from the template.
+    Create a new package 'diffmerge' from the template.
 #>
-
 param([string]$id)
 
 function Replace-Token {
-  param(
-    [string]$token,     
-    [string]$replacement, 
-    [Parameter(ValueFromPipeline = $true)]
-    [string]$path
-  )
-
-  (Get-Content $path) `
-    | %{ $_ -replace $token,$replacement } `
-    | Set-Content $path
+  param([string]$token, [string]$replacement, [Parameter(ValueFromPipeline = $true)][string]$path)
+  (Get-Content $path) | %{ $_ -replace $token, $replacement } | Set-Content $path
 }
-
-git checkout -b $id
 
 Copy-Item '_template' $id -Recurse
 Push-Location $id
@@ -41,4 +28,4 @@ Get-ChildItem | Replace-Token '__NAME__' $id
 
 git add -A
 git commit -am "new package: $id"
-git push origin $id
+git push
