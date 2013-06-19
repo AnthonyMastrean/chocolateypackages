b02fedc508884260c644049fb23fa7ab7018da93
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-18 21:51:58 -0400

    scite4autohotkey 3.0.02

--- scite4autohotkey/chocolateyInstall.ps1
+++ scite4autohotkey/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 $name   = 'scite4autohotkey'
 $url    = 'http://www.autohotkey.net/~fincs/SciTE4AutoHotkey_3/SciTE4AHK3002_Install.exe'
 $silent = '/SILENT'
+$kind   = 'EXE'
 
-Install-ChocolateyPackage $name 'EXE' $silent $url
+Install-ChocolateyPackage $name $kind $silent $url
