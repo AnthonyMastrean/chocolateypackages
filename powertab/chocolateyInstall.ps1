8e56bf4a72da7efe5fa5f758f4180e08514f97a8
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-06-20 15:55:53 -0400

    can't get the common prog files create to work

--- powertab/chocolateyInstall.ps1
+++ powertab/chocolateyInstall.ps1
@@ -2,9 +2,12 @@
 $url    = 'http://powertab.codeplex.com/downloads/get/159925'
 $target = Join-Path $ENV:COMMONPROGRAMFILES 'Modules'
 
-Install-ChocolateyZipPackage $name $url $target
-
 try {
+  # never works in chocolatey, but works in a manual admin prompt :(
+  Start-ChocolateyProcessAsAdmin "New-Item $target -Type Directory -Force"
+  
+  Install-ChocolateyZipPackage $name $url $target
+
   if(-not($ENV:PSMODULEPATH.Contains($target))) {
     [System.Environment]::SetEnvironmentVariable('PSMODULEPATH', "$target;" + $ENV:PSMODULEPATH)
   }
