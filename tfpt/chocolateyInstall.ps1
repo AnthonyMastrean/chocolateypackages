10bd11f2aefb1d0427b91e70455ec8f54aed1ca7
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-07-01 15:10:31 -0400

    11.0 does not automatically add tools to path

--- tfpt/chocolateyInstall.ps1
+++ tfpt/chocolateyInstall.ps1
@@ -4,3 +4,16 @@ $silent = '/Q ADDLOCAL=ALL'
 $kind   = 'MSI'
 
 Install-ChocolateyPackage $name $kind $silent $url
+
+try {
+	$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
+	$programfiles = if($is64bit){${ENV:PROGRAMFILES(X86)}}else{$ENV:PROGRAMFILES}
+	$bin = Join-Path $programfiles 'Microsoft Team Foundation Server 2012 Power Tools'
+	
+	Install-ChocolateyPath $bin 'Machine'
+	
+	Write-ChocolateySuccess $name
+} catch {
+	Write-ChocolateyFailure $name $_.Exception.Message
+	throw
+}
