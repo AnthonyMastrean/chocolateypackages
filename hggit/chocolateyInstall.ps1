b24c6da1cb47aaa550c182d1ae0335d3a10cc1dd
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-21 15:14:27 -0400

    not sure if this hg-git package is going to work

---
+++ hggit/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$name   = 'hggit'
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+
+try {
+  # hg clone https://bitbucket.org/durin42/hg-git $ENV:CHOCOLATEY_BIN_ROOT
+  # Write-Host @"
+  # Add the following lines to your .hg\hgrc or ~\.hgrc file
+  # 
+  # [extensions]
+  # bookmarks = 
+  # hggit = $ENV:CHOCOLATEY_BIN_ROOT\hg-git\hggit
+  #
+  # @"
+
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw 
+}
