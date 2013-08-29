67fd5b3fafdc8658c06fa0053b5b97aaeee8b300
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-29 19:41:11 -0400

    starting on dashlane

---
+++ dashlane/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "dashlane"
+$url    = "https://d3qm0vl2sdkrc.cloudfront.net/alpha/Dashlane_Launcher-1375429898.exe"
+$kind   = "EXE"
+$silent = "/SILENT"
+
+Install-ChocolateyPackage $name $kind $silent $url
