4db3209830f0355a4b4828d7e9dc48cc59b1713b
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-29 17:55:07 -0400

    start8 and modernmix

---
+++ modernmix/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "modernmix"
+$url    = "http://software-files-a.cnet.com/s/software/13/30/16/87/ModernMix-cnet-setup.exe?lop=link&ptype=3001&ontid=2072&siteId=4&edId=3&pid=13301687&psid=75886833&token=1377849023_7a0341c192a68d1409b3a2a5aa17f6e4&fileName=ModernMix-cnet-setup.exe"
+$kind   = "EXE"
+$silent = "/SILENT /NOREBOOT"
+
+Install-ChocolateyPackage $name $kind $silent $url
---
+++ start8/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "start8"
+$url    = "http://software-files-a.cnet.com/s/software/13/30/16/85/Start8-cnet-setup.exe?lop=link&ptype=3001&ontid=2072&siteId=4&edId=3&pid=13301685&psid=75732532&token=1377848307_7390ecf1ce98bcb3d31b1429e0df38e2&fileName=Start8-cnet-setup.exe"
+$kind   = "EXE"
+$silent = "/SILENT /NOREBOOT"
+
+Install-ChocolateyPackage $name $kind $silent $url
