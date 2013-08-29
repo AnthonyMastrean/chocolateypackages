27f86a5fad100ece4915fc5d6c7befc360938bd9
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-29 19:53:41 -0400

    pushed wudt

--- wudt/chocolateyInstall.ps1
+++ wudt/chocolateyInstall.ps1
@@ -1,38 +1,20 @@
-<#
----------------------------
-Microsoft ISO Backup Tool
----------------------------
-Command line options:
+$name = "wudt"
+$url  = "http://images2.store.microsoft.com/prod/clustera/framework/w7udt/1.0/en-us/Windows7-USB-DVD-tool.exe"
 
-/Q -- Quiet modes for package,
-/T:<full path> -- Specifies temporary working folder,
-/C -- Extract files only to the folder when used also with /T.
-/C:<Cmd> -- Override Install Command defined by author.
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$archive = Join-Path $tools "wudt.exe"
+$target  = Join-Path $tools "wudt.msi"
 
----------------------------
-OK   
----------------------------
-#>
+try { 
+  # The URL is a self-extracting CAB. I have to manually unpack it and 
+  # run the MSI inside.
+  Get-ChocolateyWebFile $name $archive $url
+  & $archive /Q /T:"$tools" /C
+  Install-ChocolateyInstallPackage $name "MSI" "/QN" $target
 
-try 
-{ 
-    $name = 'microsoft.isobackuptool'
-    $url  = 'http://images2.store.microsoft.com/prod/clustera/framework/w7udt/1.0/en-us/Windows7-USB-DVD-tool.exe'
-    
-    $tools   = Split-Path $MyInvocation.MyCommand.Definition
-    $archive = Join-Path $tools 'wudt.exe'
-    $target  = Join-Path $tools 'wudt.msi'
-    
-    # The URL is a self-extracting CAB. I have to manually unpack it and 
-    # run the MSI inside.
-    Get-ChocolateyWebFile $name $archive $url
-    & $archive /Q /T:"$tools" /C
-    Install-ChocolateyInstallPackage $name 'MSI' '/QN' $target
-
-    Write-ChocolateySuccess $name
+  Write-ChocolateySuccess $name
 } 
-catch 
-{
-    Write-ChocolateyFailure $name $($_.Exception.Message)
-    throw 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
 }
