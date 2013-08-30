a939112ea1564c2e39df5d0397c06b8e5a417f4f
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-30 14:31:08 -0400

    trying this boxstarter thang

---
+++ myboxstarter/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿Update-ExecutionPolicy Unrestricted
+Enable-RemoteDesktop
+
+cinst binroot
+cinst visualstudio2012premium
+cinst start8
+cinst modernmix
+cinst all -source https://myget.org/F/fullblowndotnet
+
+cinst Microsoft-Hyper-V-All -source windowsFeatures
+
+Install-ChocolateyPinnedTaskBarItem "${ENV:PROGRAMFILES(X86)}\Google\Chrome\Application"
+Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
+Install-ChocolateyPinnedTaskBarItem "$ENV:PROGRAMFILES\Notepad2\notepad2.exe"
+Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\SnippingTool.exe"
+
+Install-WindowsUpdates -AcceptEula
