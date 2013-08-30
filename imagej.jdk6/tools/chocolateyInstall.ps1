ba681a661dd6d29343d5e95dc36a269612a9e077
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-29 20:01:35 -0400

    renaming the imagej-jdk6 package

--- imagej.jdk6/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$name  = 'imagej.jdk6'
-$url   = 'http://rsbweb.nih.gov/ij/download/win32/ij145-jdk6-setup.exe'
-$url64 = 'http://rsbweb.nih.gov/ij/download/win32/ij145-jdk6-64bit-setup.exe'
-Install-ChocolateyPackage $name 'EXE' '/SILENT' $url $url64
-
-$target = 'C:\Program Files (x86)\ImageJ\ImageJ.exe'
-Install-ChocolateyDesktopLink $target
