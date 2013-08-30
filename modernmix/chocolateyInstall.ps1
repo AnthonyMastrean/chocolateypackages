e003b9f7c780f931905222e9793af541ee8feae3
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-08-30 15:12:31 -0400

    new, tokenless url

--- modernmix/chocolateyInstall.ps1
+++ modernmix/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name   = "modernmix"
-$url    = "http://software-files-a.cnet.com/s/software/13/30/16/87/ModernMix-cnet-setup.exe?lop=link&ptype=3001&ontid=2072&siteId=4&edId=3&pid=13301687&psid=75886833&token=1377849023_7a0341c192a68d1409b3a2a5aa17f6e4&fileName=ModernMix-cnet-setup.exe"
+$url    = "http://download1us.softpedia.com/dl/e159d9e2881e127d64db650fd32bcd18/5220ee5d/100231460/software/system/ModernMix_setup.exe"
 $kind   = "EXE"
 $silent = "/SILENT /NOREBOOT"
 
