e763357e3be6aba6f06abba499f2f3fa38bf950b
unknown <amastrean@anthony-mbp.roughstone.local>
2013-09-04 19:11:51 -0400

    better, stronger build task

--- underscore/rakefile.rb
+++ underscore/rakefile.rb
@@ -1,12 +1,25 @@
 require "albacore"
 
+specs = FileList["*.nuspec"]
+
 file "bin/underscore.exe.gui"
 
+desc "compile the script into an exe"
 exec :compile => ["bin/underscore.exe.gui"] do |cmd| 
-  cmd.command = Join-Path ENV["PROGRAMFILES"], "AutoHotkey\Compiler\ahd2exe.exe"
+  cmd.command = (File.join ENV["PROGRAMFILES"], "AutoHotkey/Compiler/ahk2exe.exe")
   cmd.parameters = [
     "/in", "lib/underscore.ahk", 
     "/out", "bin/underscore.exe", 
     "/icon", "lib/underscore_on.ico"
   ]
 end
+
+specs.each do |spec|
+  exec spec do |cmd|
+    cmd.command = "cpack"
+    cmd.parameters = spec
+  end
+end
+
+desc "build all chocolatey packages"
+task :build => [:compile, specs].flatten
