8708b3e04650b80c0849f4dc9df800b73c49eb65
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-09-09 11:19:17 -0400

    Working on the PowerShell module cloning part. Fixes #14.
    Moved the Windows Update command to the top, which appears to be required
    to properly install VS (in some cases). And it gets a bunch of reboots out
    of the way early.
    
    Maybe we'll have git on the command line by the time we run the powershell
    module commands. We'll see!

--- myboxstarter/chocolateyInstall.ps1
+++ myboxstarter/chocolateyInstall.ps1
@@ -1,4 +1,5 @@
 ﻿Update-ExecutionPolicy Unrestricted
+Install-WindowsUpdates -AcceptEula
 Enable-RemoteDesktop
 
 cinst binroot
@@ -9,9 +10,24 @@ cinst all -source https://myget.org/F/fullblowndotnet
 
 cinst Microsoft-Hyper-V-All -source windowsFeatures
 
+$profile_path = Join-Path $ENV:USERPROFILE "Documents\WindowsPowerShell"
+$modules_path = Join-Path $profile_path "Modules"
+
+$modules = @(
+  "git@github.com:AnthonyMastrean/remember.git",
+  "git@github.com:AnthonyMastrean/powertab.git",
+  "git@github.com:dahlbyk/posh-git.git",
+  "git@github.com:dahlbyk/posh-hg.git",
+  "git@github.com:Iristyle/Posh-VsVars.git",
+)
+
+git clone git@github.com:AnthonyMastrean/WindowsPowerShell.git $profile_path
+New-Item $modules_path -Type Directory
+Push-Location $modules_path
+$modules | %{ git clone $_ }
+Pop-Location
+
 Install-ChocolateyPinnedTaskBarItem "${ENV:PROGRAMFILES(X86)}\Google\Chrome\Application"
 Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
 Install-ChocolateyPinnedTaskBarItem "$ENV:PROGRAMFILES\Notepad2\notepad2.exe"
 Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\SnippingTool.exe"
-
-Install-WindowsUpdates -AcceptEula
