4d178a86f184e3239f56fb85fa9ee93bd3900f32
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-09-13 18:59:52 -0400

    Update chocolateyInstall.ps1

--- myboxstarter/chocolateyInstall.ps1
+++ myboxstarter/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-function Invoke-GitClone($remote, $path) {
+function Invoke-GitClone($repository, $path) {
   Push-Location $path
-  git clone $_
+  git clone $repository
   Pop-Location
 }
 
@@ -25,7 +25,7 @@ $ps_modules_path = Join-Path $profile_path "Modules"
 
 New-Item $ps_modules_path -Type Directory
 
-Invoke-GitClone -remote git@github.com:AnthonyMastrean/WindowsPowerShell.git -path $ps_profile_path
+Invoke-GitClone -repository git@github.com:AnthonyMastrean/WindowsPowerShell.git -path $ps_profile_path
 
 @(
   "git@github.com:AnthonyMastrean/remember.git",
@@ -33,7 +33,7 @@ Invoke-GitClone -remote git@github.com:AnthonyMastrean/WindowsPowerShell.git -pa
   "git@github.com:dahlbyk/posh-git.git",
   "git@github.com:dahlbyk/posh-hg.git",
   "git@github.com:Iristyle/Posh-VsVars.git",
-) | %{ Invoke-GitClone -remote $_ -path $ps_modules_path }
+) | %{ Invoke-GitClone -repository $_ -path $ps_modules_path }
 
 Install-ChocolateyPinnedTaskBarItem "${ENV:PROGRAMFILES(X86)}\Google\Chrome\Application"
 Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
