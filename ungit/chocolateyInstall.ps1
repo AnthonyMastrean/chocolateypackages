db99afc15439c25a7c4a83fb88a3d294b37f5703
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-09-13 18:22:11 -0400

    Ooo, npm-based git visualizer/controller, ungit!

---
+++ ungit/chocolateyInstall.ps1
@@ -0,0 +1,11 @@
+﻿$name    = "ungit"
+$version = "0.1.9"
+
+try {
+  npm install -g ungit@$version
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw 
+}
