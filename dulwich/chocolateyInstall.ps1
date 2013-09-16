97b99fa1c6ef4b8094095f6a7cd0ce27daca12e4
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-09-15 21:48:27 -0400

    trying to split off a dulwich package

---
+++ dulwich/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$name    = "dulwich"
+$url     = "https://github.com/jelmer/dulwich/archive/dulwich-0.9.0.zip"
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+$dulwich = Join-Path $content "dulwich-dulwich-0.9.0\dulwich"
+$hg      = Join-Path $ENV:PROGRAMFILES "Mercurial\library.zip" 
+
+Install-ChocolateyZipPackage $name $url $content
+
+try {
+  Start-ChocolateyProcessAsAdmin "
+`$shell = New-Object -ComObject shell.application; 
+`$zip = `$shell.NameSpace(`"$hg`"); 
+`$zip.CopyHere(`"$dulwich`");
+"
+  
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw 
+}
--- hggit/chocolateyInstall.ps1
+++ hggit/chocolateyInstall.ps1
@@ -1,9 +1,17 @@
-﻿$name   = 'hggit'
+﻿$name    = "hggit"
 
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
+$dulwich_url = "https://github.com/jelmer/dulwich/archive/dulwich-0.9.0.zip"
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
 
 try {
+  # download and unzip dulwich
+  
+  # move into the hg library.zip
+  
+  # download hg-git
+
   # hg clone https://bitbucket.org/durin42/hg-git $ENV:CHOCOLATEY_BIN_ROOT
   # Write-Host @"
   # Add the following lines to your .hg\hgrc or ~\.hgrc file
