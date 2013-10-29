dbe20c36758c1859c2015f43f23cd568dec1bc95
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-10-29 14:12:38 -0400

    handbrake install and portable packages

---
+++ handbrake/chocolateyInstall-install.ps1
@@ -0,0 +1,7 @@
+﻿$name   = "handbrake.install"
+$url    = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-1_i686-Win_GUI.exe"
+$url64  = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-1_x86_64-Win_GUI.exe"
+$kind   = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $name $kind $silent $url $url64
---
+++ handbrake/chocolateyInstall-portable.ps1
@@ -0,0 +1,8 @@
+﻿$name   = "handbrake.portable"
+$url    = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-i686-Win_CLI.zip"
+$url64  = "http://handbrake.fr/rotation.php?file=HandBrake-0.9.9-x86_64-Win_CLI.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage -packageName $name -url $url -url64bit $url64 -unzipLocation $content
