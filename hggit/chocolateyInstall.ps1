09a742fc5a492ce1509adfdcf07ced6dbb8e25a6
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-10-29 22:44:03 -0400

    ah, finally got the hg clone dest right!

--- hggit/chocolateyInstall.ps1
+++ hggit/chocolateyInstall.ps1
@@ -1,26 +1,19 @@
-﻿$name    = "hggit"
-
-$dulwich_url = "https://github.com/jelmer/dulwich/archive/dulwich-0.9.0.zip"
-
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) "content"
+﻿$name     = "hggit"
+$url      = "https://bitbucket.org/durin42/hg-git"
+$bin_root = Join-Path $ENV:SYSTEMDRIVE $ENV:CHOCOLATEY_BIN_ROOT
+$hggit    = Join-Path $bin_root "hg-git"
 
 try {
-  # download and unzip dulwich
+  hg clone $url $hggit
   
-  # move into the hg library.zip
-  
-  # download hg-git
+  Write-Host @"
+Add the following lines to your .hg\hgrc or ~\.hgrc file
+ 
+[extensions]
+bookmarks = 
+hggit = $hggit\hggit
 
-  # hg clone https://bitbucket.org/durin42/hg-git $ENV:CHOCOLATEY_BIN_ROOT
-  # Write-Host @"
-  # Add the following lines to your .hg\hgrc or ~\.hgrc file
-  # 
-  # [extensions]
-  # bookmarks = 
-  # hggit = $ENV:CHOCOLATEY_BIN_ROOT\hg-git\hggit
-  #
-  # @"
+"@
 
   Write-ChocolateySuccess $name
 } 
