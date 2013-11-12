bd972fff88f35027cce861771fc54f78723900c3
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-11-12 11:25:14 -0500

    Update chocolateyInstall.ps1

--- NugetPackageManager/chocolateyInstall.ps1
+++ NugetPackageManager/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$name = 'NugetPackageManager'
-$url  = 'http://visualstudiogallery.msdn.microsoft.com/27077b70-9dad-4c64-adcf-c7cf6bc9970c/file/37502/31/NuGet.Tools.vsix'
+$name = "NugetPackageManager"
+$url  = "http://visualstudiogallery.msdn.microsoft.com/27077b70-9dad-4c64-adcf-c7cf6bc9970c/file/37502/35/NuGet.Tools.vsix"
 
 Install-ChocolateyVsixPackage $name $url
