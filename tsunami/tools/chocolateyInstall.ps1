9a5d2a0bbddaf7a6d05d749ea371b47fd5c816cb
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-03 17:41:50 -0500

    tsunami ide

---
+++ tsunami/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$name   = "tsunami"
+$url    = "http://tsunami.io.s3.amazonaws.com/installers/tsunami/Tsunami_With_x86_Excel.msi"
+$url64  = "http://tsunami.io.s3.amazonaws.com/installers/tsunami/Tsunami_With_x64_Excel.msi"
+$kind   = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $name $kind $silent $url $url64
