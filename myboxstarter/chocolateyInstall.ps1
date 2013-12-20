d5d0a5a25ada5fc8ef3846ed525feb3034edcaa9
Anthony Mastrean <anthony.mastrean@gmail.com>
2013-12-20 14:13:20 -0500

    Update chocolateyInstall.ps1

--- myboxstarter/chocolateyInstall.ps1
+++ myboxstarter/chocolateyInstall.ps1
@@ -1,11 +1,10 @@
-function Invoke-GitClone($repository, $path) {
+function Invoke-GitClone($url, $path) {
   Push-Location $path
-  git clone $repository
+  git clone $url
   Pop-Location
 }
 
 Update-ExecutionPolicy Unrestricted
-Install-WindowsUpdates -AcceptEula
 Enable-RemoteDesktop
 
 # scoop!
@@ -13,27 +12,25 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://get.
 scoop install sudo
 scoop install concfg
 
-# without an MSDN license, I default to the express packages
+# enable fancy Windows features
+cinst Microsoft-Hyper-V-All -source windowsFeatures
+cinst IIS-WebServerRole -source windowsfeatures
+
+# no MSDN license here
 cinst VisualStudio2012WDX
 cinst VisualStudioExpress2012Web
 cinst VisualStudioExpress2012Windows8
 
-# and then everything else that I like ;)
+# everything else
 cinst all -source https://myget.org/F/fullblowndotnet
-
-# enable all Hyper-V and IIS features for convenience
-cinst Microsoft-Hyper-V-All -source windowsFeatures
-Get-WindowsOptionalFeature -Online `
-  | ?{ $_.FeatureName -match "IIS" } `
-  | Enable-WindowsOptionalFeature -Online -All -NoRestart
 
 $ps_profile_path = Join-Path $ENV:USERPROFILE "Documents\WindowsPowerShell"
 $ps_modules_path = Join-Path $profile_path "Modules"
 
 New-Item $ps_modules_path -Type Directory
 
-# get my source-controlled PowerShell profile down
-Invoke-GitClone -repository git@github.com:AnthonyMastrean/WindowsPowerShell.git -path $ps_profile_path
+# my PowerShell profile
+Invoke-GitClone -url git@github.com:AnthonyMastrean/WindowsPowerShell.git -path $ps_profile_path
 
 # and all my fave Modules
 @(
@@ -43,10 +40,12 @@ Invoke-GitClone -repository git@github.com:AnthonyMastrean/WindowsPowerShell.git
   "git@github.com:dahlbyk/posh-git.git",
   "git@github.com:dahlbyk/posh-hg.git",
   "git@github.com:Iristyle/Posh-VsVars.git",
-) | %{ Invoke-GitClone -repository $_ -path $ps_modules_path }
+) | %{ Invoke-GitClone -url $_ -path $ps_modules_path }
 
 # pinned applicatin shortcuts
 Install-ChocolateyPinnedTaskBarItem "${ENV:PROGRAMFILES(X86)}\Google\Chrome\Application"
 Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\WindowsPowerShell\v1.0\powershell.exe"
 Install-ChocolateyPinnedTaskBarItem "$ENV:PROGRAMFILES\Notepad2\notepad2.exe"
 Install-ChocolateyPinnedTaskBarItem "$ENV:WINDIR\System32\SnippingTool.exe"
+
+Install-WindowsUpdates -AcceptEula
