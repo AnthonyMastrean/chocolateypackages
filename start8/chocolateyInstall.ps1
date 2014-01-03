c5f1a7a94214acdcd5100b7edec62cb11d639cec
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-03 15:56:55 -0500

    Update chocolateyInstall.ps1

--- start8/chocolateyInstall.ps1
+++ start8/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
-﻿$name   = "start8"
-$url    = "http://download1us.softpedia.com/dl/446ca52a82d2543d80e1eacc755e69aa/5220edc5/100209499/software/os_enhance/Start8_setup.exe"
+$name   = "start8"
+$url    = "http://stardock.cachefly.net/Start8_setup.exe"
 $kind   = "EXE"
 $silent = "/SILENT /NOREBOOT"
 
