9eb012256c7e90faf0f95e0e783a9ef2a7a371da
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-06 17:34:05 -0500

    Trying to build some office communicator apps

---
+++ uccapisdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "uccapisdk"
+$url    = "http://download.microsoft.com/download/D/1/6/D1658355-2C30-4921-9BFC-0C889B2898E1/UccApiSdk.msi"
+$kind   = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $name $kind $silent $url
