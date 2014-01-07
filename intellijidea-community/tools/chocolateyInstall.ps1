c4c0c57ff537187524a0dac5b5f084f3a5be4a27
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-07 15:04:32 -0500

    intellijidea-community & -ultimate

---
+++ intellijidea-community/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "intellijidea-community"
+$url    = "http://download.jetbrains.com/idea/ideaIC-13.0.1.exe"
+$kind   = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $name $kind $silent $url
---
+++ intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "intellijidea-ultimate"
+$url    = "http://download.jetbrains.com/idea/ideaIU-13.0.1.exe"
+$kind   = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $name $kind $silent $url
