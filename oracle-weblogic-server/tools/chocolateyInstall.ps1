6ccb92015fee0ba3ad3107543faf400ad29983a6
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-09 13:55:24 -0500

    oracle weblog server 12c

---
+++ oracle-weblogic-server/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$name   = "oracle-weblogic-server"
+$url    = "http://download.oracle.com/otn/nt/middleware/12c/wls/1212/wls1212_dev.zip"
+$home   = Join-Path (Get-BinRoot) "wls12120"
+$config = Join-Path $home "configure.cmd"
+
+Install-ChocolateyZipPackage $name $url (Get-BinRoot)
+
+try {
+  Start-ChocolateyProcessAsAdmin @"
+[Environment]::SetEnvironmentVariable("MW_HOME", $home, "Machine")
+"@
+
+  & $config -silent
+
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
+}
