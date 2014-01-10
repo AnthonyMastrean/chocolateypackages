877e47dc769cd9002f17b4b5f719735c3c1142f3
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-10 12:19:14 -0500

    apache directory studio

---
+++ apachedirectorystudio/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$name   = "apachedirectorystudio"
+$url    = "http://psg.mtu.edu/pub/apache//directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86-2.0.0.v20130628.exe"
+$url64  = "http://psg.mtu.edu/pub/apache//directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86_64-2.0.0.v20130628.exe"
+$kind   = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $name $kind $silent $url $url64
