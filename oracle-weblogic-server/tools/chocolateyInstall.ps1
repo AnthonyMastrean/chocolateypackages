ec58dfa398dd75d844f7182c016fa4dd2dab63f0
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-13 13:20:31 -0500

    oracle-weblogic-server: Set MW_HOME and WL_HOME

--- oracle-weblogic-server/tools/chocolateyInstall.ps1
+++ oracle-weblogic-server/tools/chocolateyInstall.ps1
@@ -1,13 +1,15 @@
 ﻿$name   = "oracle-weblogic-server"
 $url    = "http://download.oracle.com/otn/nt/middleware/12c/wls/1212/wls1212_dev.zip"
-$home   = Join-Path (Get-BinRoot) "wls12120"
-$config = Join-Path $home "configure.cmd"
+$root   = Join-Path (Get-BinRoot) "wls12120"
+$server = Join-Path $root "wlserver"
+$config = Join-Path $root "configure.cmd"
 
 Install-ChocolateyZipPackage $name $url (Get-BinRoot)
 
 try {
   Start-ChocolateyProcessAsAdmin @"
-[Environment]::SetEnvironmentVariable("MW_HOME", $home, "Machine")
+[Environment]::SetEnvironmentVariable("MW_HOME", $root, "Machine")
+[Environment]::SetEnvironmentVariable("WL_HOME", $server, "Machine")
 "@
 
   & $config -silent
