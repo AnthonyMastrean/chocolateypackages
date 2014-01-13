e8194818cb18b471a3981034a0d41bdce7675667
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-13 15:20:25 -0500

    Oracle InstantClient + SQL*Plus

---
+++ oracle-instantclient-sqlplus/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$name  = "oracle-instantclient-sqlplus"
+$url   = "http://download.oracle.com/otn/nt/instantclient/11204/instantclient-sqlplus-nt-11.2.0.4.0.zip"
+$url64 = "http://download.oracle.com/otn/nt/instantclient/11204/instantclient-sqlplus-windows.x64-11.2.0.4.0.zip"
+
+Install-ChocolateyZipPackage -PackageName $name -Url $url -Url64Bit $url64 -UnzipLocation (Get-BinRoot)
---
+++ oracle-instantclient/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$name  = "oracle-instantclient"
+$url   = "http://download.oracle.com/otn/nt/instantclient/11204/instantclient-basiclite-nt-11.2.0.4.0.zip"
+$url64 = "http://download.oracle.com/otn/nt/instantclient/11204/instantclient-basiclite-windows.x64-11.2.0.4.0.zip"
+$root  = Join-Path (Get-BinRoot) "instantclient_11_2"
+
+Install-ChocolateyZipPackage -PackageName $name -Url $url -Url64Bit $url64 -UnzipLocation (Get-BinRoot)
+
+try {
+  Install-ChocolateyPath -PathToInstall $root -PathType "Machine"
+
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
+}
