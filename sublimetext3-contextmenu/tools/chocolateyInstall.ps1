357b9b3fd6fd5ac136a20bc40f752c89410eb1eb
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-13 15:34:52 -0500

    sublimetext3 context menu

---
+++ sublimetext3-contextmenu/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$name  = "sublimetext3-contextmenu"
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$reg   = Join-Path $tools "sublimetext3-contextmenu.reg"
+
+try {
+  regedit /s $reg
+
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
+}
