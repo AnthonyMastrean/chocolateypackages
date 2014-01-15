05c674f61c810132af32b28e9f6e63405ded337e
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-15 08:45:39 -0500

    ag

---
+++ ag/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$name   = "ag"
+$url    = "https://kjkpub.s3.amazonaws.com/software/the_silver_searcher/rel/0.18.1-1129/ag.exe"
+$tools  = Split-Path $MyInvocation.MyCommand.Definition
+$target = Join-Path $tools "ag.exe"
+
+try {
+  Get-ChocolateyWebFile $name $target $url
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
+}
