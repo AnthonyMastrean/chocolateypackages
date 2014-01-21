5c4cdd2ed5363942cf4fb6725c83bb28be80b52c
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-21 12:03:29 -0500

    HP Hotkey Support - Disable Notifications

---
+++ hp-hotkeysupport-off/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$name  = "hp-hotkeysupport-off"
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$reg   = Join-Path $tools "hp-hotkeysupport-off.reg"
+
+try {
+  regedit /s $reg
+
+  Write-ChocolateySuccess $name
+} 
+catch {
+  Write-ChocolateyFailure $name $_.Exception.Message
+  throw 
+}
