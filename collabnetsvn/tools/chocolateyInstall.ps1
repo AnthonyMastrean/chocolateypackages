8f1b52b54c2d0db4e02b2c52034f63ef6bd66994
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-21 13:28:50 -0500

    Found a static, no-login required URL for the downloads

--- collabnetsvn/tools/chocolateyInstall.ps1
+++ collabnetsvn/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$name   = "collabnetsvn"
-$url    = "http://downloads-members.open.collab.net/files/documents/60/8026/CollabNetSubversion-client-1.8.5-1-Win32.exe"
-$url64  = "https://www.open.collab.net/files/documents/60/8027/CollabNetSubversion-client-1.8.5-1-x64.exe"
+$url    = "http://open.collab.net/files/documents/60/8026/CollabNetSubversion-client-1.8.5-1-Win32.exe"
+$url64  = "http://open.collab.net/files/documents/60/8027/CollabNetSubversion-client-1.8.5-1-x64.exe"
 $kind   = "EXE"
 
 $tools  = Split-Path $MyCommand.MyInvocation.Definition
 $answer = Join-Path $tools "collabnetsvn.txt"
 
-$silent = "/S /Answerfile=`"$answer`"
+$silent = "/S /Answerfile=`"$answer`""
 
 Install-ChocolateyPackage $name $kind $silent $url $url64
