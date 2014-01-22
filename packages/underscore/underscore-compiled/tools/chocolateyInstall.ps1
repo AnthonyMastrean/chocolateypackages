1fc02d5c7aa08fd2855919d174e57aba92c0de06
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-22 17:33:28 -0500

    Develop underscore directly from the script package

--- packages/underscore/underscore-compiled/tools/chocolateyInstall.ps1
+++ packages/underscore/underscore-compiled/tools/chocolateyInstall.ps1
@@ -2,15 +2,16 @@
 $startup = Join-Path $ENV:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
 
 $tools   = Split-Path $MyInvocation.MyCommand.Definition
-$app     = Join-Path $tools "underscore.exe"
-$icon    = Join-Path $tools "underscore_on.ico"
+$content = Join-Path (Split-Path $tools) "content"
+$app     = Join-Path $content "underscore.exe"
+$icon    = Join-Path $content "underscore_on.ico"
 $target  = Join-Path $startup "underscore.lnk"
 
 try {
   $shell = New-Object -ComObject "Wscript.Shell"
   $shortcut = $shell.CreateShortcut($target)
   $shortcut.TargetPath = $app
-  $shortcut.WorkingDirectory = $tools
+  $shortcut.WorkingDirectory = $content
   $shortcut.IconLocation = $icon
   $shortcut.Save()
 
--- packages/underscore/underscore-script/tools/chocolateyInstall.ps1
+++ packages/underscore/underscore-script/tools/chocolateyInstall.ps1
@@ -2,15 +2,16 @@
 $startup = Join-Path $ENV:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
 
 $tools   = Split-Path $MyInvocation.MyCommand.Definition
-$script  = Join-Path $tools "underscore.ahk"
-$icon    = Join-Path $tools "underscore_on.ico"
+$content = Join-Path (Split-Path $tools) "content"
+$script  = Join-Path $content "underscore.ahk"
+$icon    = Join-Path $content "underscore_on.ico"
 $target  = Join-Path $startup "underscore.lnk"
 
 try {
   $shell = New-Object -ComObject "Wscript.Shell"
   $shortcut = $shell.CreateShortcut($target)
   $shortcut.TargetPath = $script  
-  $shortcut.WorkingDirectory = $tools
+  $shortcut.WorkingDirectory = $content
   $shortcut.IconLocation = $icon
   $shortcut.Save()
 
