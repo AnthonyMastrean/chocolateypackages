473c76005700a4a3e2d5a94da0c944e1d5bc8088
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-24 20:14:47 -0500

    tons of new icons

--- packages/keepass/chocolateyInstall.ps1
+++ packages/keepass/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = 'keepass'
-$url    = 'http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.21/KeePass-2.21-Setup.exe?r=http%3A%2F%2Fkeepass.info%2Fdownload.html&ts=1361221558'
-$kind   = 'EXE'
-$silent = '/VERYSILENT'
+﻿$name   = "keepass"
+$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.24/KeePass-2.24-Setup.exe?r=&ts=1325968281&use_mirror=voxel"
+$kind   = "EXE"
+$silent = "/VERYSILENT"
 
 Install-ChocolateyPackage $name $kind $silent $url
