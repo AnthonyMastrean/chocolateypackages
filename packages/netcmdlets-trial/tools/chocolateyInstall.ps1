10e1be11dc745c0116274caeed4f15117bd2a467
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-01-28 15:26:31 -0500

    netcmdlets, -trial, and -community

---
+++ packages/netcmdlets-trial/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$name   = "netcmdlets-trial"
+$url    = "__URL__"
+$kind   = "MSI"
+$silent = "/Q"
+
+#$tools = Split-Path $MyInvocation.MyCommand.Definition
+#$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyPackage $name $kind $silent $url $url64
+#Install-ChocolateyZipPackage $name $url $content
