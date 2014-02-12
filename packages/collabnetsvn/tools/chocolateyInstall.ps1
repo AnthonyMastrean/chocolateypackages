1bc9e8d20540f9af8d8644c2419bb55c951105b1
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-02-12 09:52:33 -0500

    The statement is System.Management.Automation.InvocationInfo, #41

--- packages/collabnetsvn/tools/chocolateyInstall.ps1
+++ packages/collabnetsvn/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $url    = "http://open.collab.net/files/documents/60/8026/CollabNetSubversion-cl
 $url64  = "http://open.collab.net/files/documents/60/8027/CollabNetSubversion-client-1.8.5-1-x64.exe"
 $kind   = "EXE"
 
-$tools  = Split-Path $MyCommand.MyInvocation.Definition
+$tools  = Split-Path $MyInvocation.MyCommand.Definition
 $answer = Join-Path $tools "collabnetsvn.txt"
 
 $silent = "/S /Answerfile=`"$answer`""
