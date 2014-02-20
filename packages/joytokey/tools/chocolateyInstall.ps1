a522946bcd4bbf6d54bc31084f7ac38b9023dab0
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-02-19 19:59:40 -0500

    Update chocolateyInstall.ps1

--- packages/joytokey/tools/chocolateyInstall.ps1
+++ packages/joytokey/tools/chocolateyInstall.ps1
@@ -1,9 +1,12 @@
-﻿$name   = "joytokey"
+$name   = "joytokey"
 $url    = "http://joytokey.net/download/JoyToKey_en.zip"
-$tools  = Split-Path $MyInvocation.MyCommand.Definition
-$gui    = Join-Path $tools "JoyToKey_en\JoyToKey.exe.gui"
 
-Install-ChocolateyZipPackage $name $url $tools
+$tools  = Split-Path $MyInvocation.MyCommand.Definition
+$cotent = Join-Path (Split-Path $tools) "content"
+
+$gui    = Join-Path $content "JoyToKey_en\JoyToKey.exe.gui"
+
+Install-ChocolateyZipPackage $name $url $content
 
 try {
   New-Item $gui -Type File -Force
