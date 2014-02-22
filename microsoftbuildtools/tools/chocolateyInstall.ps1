f0f0cd91f2b737fc80192b379693b9f5ee84dec2
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-02-21 23:29:13 -0500

    microsoftbuildtool

---
+++ microsoftbuildtools/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$name   = "microsoftbuildtools"
+$url    = "http://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe"
+$kind   = "EXE"
+$silent = "/S /NoRestart"
+
+Install-ChocolateyPackage $name $kind $silent $url
