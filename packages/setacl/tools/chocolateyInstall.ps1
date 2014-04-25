c09ca5917e900d10b7ed72e7a616de71c25408dd
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-04-24 23:56:54 -0400

    setacl.3.6.0

--- packages/setacl/tools/chocolateyInstall.ps1
+++ packages/setacl/tools/chocolateyInstall.ps1
@@ -1,25 +1,21 @@
-﻿try 
-{ 
-    $name = 'setacl'
+﻿$id = "setacl"
+$url = "http://files.helgeklein.com/downloads/SetACL/current/SetACL%20(executable%20version).zip"
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
 
-    $tools        = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-    $content      = Join-Path (Split-Path $tools) 'content'
-  
-    Install-ChocolateyZipPackage "$name" 'http://downloads.sourceforge.net/project/setacl/SetACL%202.3.0.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fsetacl%2Ffiles%2F&ts=1321923436&use_mirror=cdnetworks-us-2' "$content"
+Install-ChocolateyZipPackage $id $url $content
 
-    $processor = Get-WmiObject Win32_Processor
-    $is64bit = $processor.AddressWidth -eq 64
-    
-    # This package comes with both processor architectures, we need to identify the one we don't want and remove it.
-    $bitness = if($is64bit) { "x86" } else { "x64" }
-    $remove = "$content\SetACL 2.3.0\Command line version\$bitness"
-    
-    Remove-Item $remove -force -recurse
-  
-    Write-ChocolateySuccess "$name"
-} 
-catch 
-{
-    Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
-    throw 
+try {     
+  $is64bit = Get-ProcessorBits 64
+
+  $bits = @{$true="32 bit";$false="64 bit"}[$is64bit]
+  $folder = Join-Path "SetACL (executable version)" $bits
+  $remove = Join-Path $content $folder
+
+  Remove-Item $remove -Force -Recurse
+
+  Write-ChocolateySuccess $id
+} catch {
+  Write-ChocolateyFailure $id $_.Exception.Message
+  throw 
 }
