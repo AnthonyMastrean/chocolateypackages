f9c76ed17ef4459ebc6099f29b571bbea4f48520
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-06-05 21:58:25 -0400

    monodevelop.5.0.0

--- packages/monodevelop/tools/chocolateyInstall.ps1
+++ packages/monodevelop/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = "monodevelop"
-$url    = "http://download.xamarin.com/studio/Windows/XamarinStudio-4.0.8.msi"
-$kind   = "MSI"
-$silent = "/quiet"
+﻿$name = "monodevelop"
+$url = "http://download.xamarin.com/studio/Windows/XamarinStudio-5.0.0.878-0.msi"
+$kind = "MSI"
+$silent = "/Q"
 
 Install-ChocolateyPackage $name $kind $silent $url
