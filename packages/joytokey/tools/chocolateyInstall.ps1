cb7025f49535bf0c7e89ae969abcd57d7722e233
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-06-05 22:49:15 -0400

    joytokey.5.6.0.1

--- packages/joytokey/tools/chocolateyInstall.ps1
+++ packages/joytokey/tools/chocolateyInstall.ps1
@@ -2,14 +2,27 @@ $name = "joytokey"
 $url = "http://joytokey.net/download/JoyToKey_en.zip"
 
 $tools = Split-Path $MyInvocation.MyCommand.Definition
-$cotent = Join-Path (Split-Path $tools) "content"
+$content = Join-Path (Split-Path $tools) "content"
 
-$gui = Join-Path $content "JoyToKey_en\JoyToKey.exe.gui"
+$target = Join-Path $content "JoyToKey_en\JoyToKey.exe"
+$gui = "$target.gui"
+
+$startup = "$ENV:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+$link = Join-Path $startup "JoyToKey.lnk"
 
 Install-ChocolateyZipPackage $name $url $content
 
 try {
   New-Item $gui -Type File -Force
+
+  $shell = New-Object -ComObject "Wscript.Shell"
+  $shortcut = $shell.CreateShortcut($link)
+  $shortcut.TargetPath = $target
+  $shortcut.WorkingDirectory = $content
+  $shortcut.Save()
+
+  & $link
+
   Write-ChocolateySuccess $name
 } catch {
   Write-ChocolateyFailure $name $_.Exception.Message
