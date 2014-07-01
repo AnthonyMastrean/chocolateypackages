c2fe289a1e07bd0a6b721986ba7ff299d0dd827d
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-01 12:10:20 -0400

    Site generated at 2014-07-01 16:10:19 UTC

---
+++ packages/lolcommits/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$id = "lolcommits"
+
+try {
+  $IMAGEMAGICK_HOME = Resolve-Path(Join-Path $ENV:PROGRAMFILES "ImageMagick*") | Sort-Object -Desc | Select-Object -First 1 -Expand Path
+
+  gem install rmagick --platform=ruby -- "'--with-opt-dir=`"$IMAGEMAGICK_HOME`"'"
+  gem install lolcommits --version "= 0.5.4"
+
+  Write-ChocolateySuccess $id
+} catch {
+  Write-ChocolateyFailure $id $_.Exception.Message
+  throw
+}
