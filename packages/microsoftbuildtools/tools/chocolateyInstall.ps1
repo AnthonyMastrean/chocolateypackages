539aa9d4a6107b019b36ddee3275b25864b885ae
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-03 18:29:09 -0400

    uh... what happened here? whitespace, probably

--- packages/microsoftbuildtools/tools/chocolateyInstall.ps1
+++ packages/microsoftbuildtools/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = "microsoftbuildtools"
-$url    = "http://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe"
-$kind   = "EXE"
+﻿$name = "microsoftbuildtools"
+$url = "http://download.microsoft.com/download/9/B/B/9BB1309E-1A8F-4A47-A6C5-ECF76672A3B3/BuildTools_Full.exe"
+$kind = "EXE"
 $silent = "/S /NoRestart"
 
 Install-ChocolateyPackage $name $kind $silent $url
