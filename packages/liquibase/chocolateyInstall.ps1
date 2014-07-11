6b8af35f25baf15cadd924cae1d000d4fb7bf006
jahav <havlicek.honza@gmail.com>
2014-07-11 02:10:40 +0200

    Delete moved file

--- packages/liquibase/chocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$name   = 'liquibase'
-$url    = 'http://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-3.0.0-beta1-bin.zip'
-
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
-$bat     = Join-Path $content 'liquibase.bat'
-
-Install-ChocolateyZipPackage $name $url $content
-Generate-BinFile $name $bat
