bba64314421d2d61a71c1b31bdaccda082eab1cf
jahav <havlicek.honza@gmail.com>
2014-07-11 02:08:54 +0200

    Update liquibase package and remove shim

---
+++ packages/liquibase/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$name   = 'liquibase'
+$url    = 'http://sourceforge.net/projects/liquibase/files/Liquibase%20Core/liquibase-3.2.0-bin.zip/download'
+           
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat     = Join-Path $content 'liquibase.bat'
+
+Install-ChocolateyZipPackage $name $url $content
+Generate-BinFile $name $bat
---
+++ packages/liquibase/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$name   = 'liquibase'
+           
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat     = Join-Path $content 'liquibase.bat'
+
+Remove-BinFile $name $bat
