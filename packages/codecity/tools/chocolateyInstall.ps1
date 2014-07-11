d5f01e1b907326e4f30af4243f45c79fb218e610
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-11 14:01:54 -0400

    codecity.1.4.3

---
+++ packages/codecity/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$id = "codecity"
+$url = "http://www.inf.usi.ch/phd/wettel/download.php?f=codecity-win.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+$nogui = Join-Path $content "codecity.exe.gui"
+
+Install-ChocolateyZipPackage $id $url $content
+
+New-Item $nogui -Type File -Force
