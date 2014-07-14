2359fadc266fd5847e1c9e4003ed78f524480d48
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-14 17:07:15 -0400

    pandoc.1.12

---
+++ packages/pandoc/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "pandoc"
+$url = "https://github.com/jgm/pandoc/releases/download/1.12.4.2/pandoc-1.12.4.2-1-windows.msi"
+$kind = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $id $kind $silent $url
