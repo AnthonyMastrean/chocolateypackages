b2461ec856a04c1401118e488dc98e618606c565
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-14 17:24:50 -0400

    pandoc should install for all users with this switch

--- packages/pandoc/tools/chocolateyInstall.ps1
+++ packages/pandoc/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$id = "pandoc"
 $url = "https://github.com/jgm/pandoc/releases/download/1.12.4.2/pandoc-1.12.4.2-1-windows.msi"
 $kind = "MSI"
-$silent = "/Q"
+$silent = "/Q ALLUSERS=1"
 
 Install-ChocolateyPackage $id $kind $silent $url
