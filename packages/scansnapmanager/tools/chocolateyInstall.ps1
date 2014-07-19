e4d56963eed5ebf0ed33fbbd3768306bcd640830
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-19 16:49:00 -0400

    scansnapmanager.5.1.30

---
+++ packages/scansnapmanager/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$id = "scansnapmanager"
+$url = "http://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/w-s1300/ssv51l30W.exe"
+$kind = "EXE"
+
+$temp = Join-Path $ENV:TEMP (Join-Path "Chocolatey" $id)
+$setup = Join-Path $temp "Manager\setup.exe"
+
+New-Item $temp -Type Directory -Force | Out-Null
+Push-Location $temp
+
+Install-ChocolateyPackage -PackageName $id -FileType $kind -Url $url
+
+Pop-Location
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$response = Join-Path $tools "Setup.iss"
+
+$silent = "/S /f1`"$response`""
+
+Install-ChocolateyInstallPackage -PackageName $id -FileType $kind -SilentArgs $silent -File $setup
