ed9f4136afa55db2dd6a8b0c259a7266ac3956e9
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-19 14:40:20 -0400

    adobedigitaleditions.3.0

---
+++ packages/adobedigitaleditions/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "adobedigitaleditions"
+$url = "http://download.adobe.com/pub/adobe/digitaleditions/ADE_3.0_Installer.exe"
+$kind = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage $id $kind $silent $url $url64
