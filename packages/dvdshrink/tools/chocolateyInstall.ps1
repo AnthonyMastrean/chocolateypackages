acd194a3195a7c34feea447130e351de90d09534
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-07-19 14:16:06 -0400

    dvdshrink.3.2.0

---
+++ packages/dvdshrink/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "dvdshrink"
+$url = "http://fs40.filehippo.com/7497/86c4c175ec984d718dcde80f4f2cc4cc/dvdshrink32setup.exe"
+$kind = "EXE"
+$silent = "/VERYSILENT"
+
+Install-ChocolateyPackage $id $kind $silent $url
