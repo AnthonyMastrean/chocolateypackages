bcb7b79e3ed0e183b8a43613e3dbdab759b9eb52
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-08-13 11:44:22 -0400

    Trying to update jdk url/lic whatevs :(

--- packages/jdk/tools/chocolateyInstall.ps1
+++ packages/jdk/tools/chocolateyInstall.ps1
@@ -1,27 +1,26 @@
-﻿$name   = "jdk"
-$url    = "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-i586.exe"
-$url64  = "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-x64.exe"
-$kind   = "EXE"
+﻿$name = "jdk"
+$url32 = "http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-windows-i586.exe"
+$url64 = "http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-windows-x64.exe"
+$kind = "EXE"
 $silent = "/s" 
 
-$temp   = Join-Path $ENV:TEMP "chocolatey\jdk\jdkInstaller.exe"
-
-$java   = Join-Path $ENV:PROGRAMFILES "Java\jdk1.7.0_45"
-$bin    = Join-Path $java "bin"
+$temp = Join-Path $ENV:TEMP "chocolatey\jdk\jdkInstaller.exe"
+$java = Join-Path $ENV:PROGRAMFILES "Java\jdk1.7.0_45"
+$bin = Join-Path $java "bin"
 
 try {
-  $dl = if(Get-ProcessorBits 64) { $url64 } else { $url }
+  $url = @{$true=$url64;$false=$url32}[(Get-ProcessorBits 64)]
 
-  Write-Host "Downloading file from $dl"
+  Write-Host "Downloading file from $url"
 
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
   $client = New-Object "System.Net.WebClient"
   $client.Headers.Add("Cookie", "gpw_e24=http://www.oracle.com")
-  $client.DownloadFile($dl, $temp)
+  $client.DownloadFile($url, $temp)
   
   Install-ChocolateyInstallPackage $name $kind $silent $temp
-  Install-ChocolateyPath $bin "Machine"
-  Install-ChocolateyEnvironmentVariable "JAVA_HOME" $java "Machine"
+  # Install-ChocolateyPath $bin "Machine"
+  # Install-ChocolateyEnvironmentVariable "JAVA_HOME" $java "Machine"
         
   Write-ChocolateySuccess $name
 }
