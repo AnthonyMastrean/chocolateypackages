4a144f2b13224cfc2c223cc2e0bae6793c7deeb3
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-08-20 10:50:03 -0400

    webexeditor and webexplayer

---
+++ packages/webexeditor/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "webexeditor"
+$url = "https://akamaicdn.webex.com/client/WBXclient-T29L10NSP7EP2-4/ateditor.msi"
+$kind = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $id $kind $silent $url
---
+++ packages/webexplayer/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id = "webexplayer"
+$url = "https://welcome.webex.com/client/wbxclient/atrecply.msi"
+$kind = "MSI"
+$silent = "/Q"
+
+Install-ChocolateyPackage $id $kind $silent $url
