ad90702e4abb0e918124d5da02e75cab9a97d27e
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-09-02 17:29:17 -0400

    Veryseriousbutton.0.1-alphas

---
+++ packages/veryseriousbutton/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$id = "veryseriousbutton"
+$url = "https://github.com/gregcourville/VerySeriousButton-VerySeriousSetup/releases/download/v0.1-alpha/VerySeriousSetup.exe"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+$content = Join-Path (Split-Path $tools) "content"
+$target = Join-Path $content "VerySeriousSetup.exe"
+$gui = Join-Path $content "VerySeriousSetup.exe.gui"
+
+New-Item $content -Type "Directory" -Force | Out-Null
+New-Item $gui -Type "File" -Force | Out-Null
+
+Get-ChocolateyWebFile $id $target $url
