4b8c24bb13ef432f0f31cc6ddf83ea1cbd4060e9
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-09-10 13:29:26 -0400

    cloc.1.62

---
+++ packages/cloc/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$id = "cloc"
+$url = "http://downloads.sourceforge.net/project/cloc/cloc/v1.62/cloc-1.62.exe"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+$target = Join-Path $content "cloc-1.62.exe"
+
+try {
+  New-Item $content -Type "Directory" -Force | Out-Null
+  Get-ChocolateyWebFile $id $target $url
+
+  Write-ChocolateySuccess $id
+} catch {
+  Write-ChocolateyFailure $id $_.Exception.Message
+  throw
+}
