02ed40d0e83f57a8d48d5863878fa9fbd781fca4
TomOne <TomOne@users.noreply.github.com>
2014-10-08 19:21:59 +0200

    Bump keepass to 2.28

--- packages/keepass.install/tools/chocolateyInstall.ps1
+++ packages/keepass.install/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$name   = "keepass.install"
-$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.27/KeePass-2.27-Setup.exe"
+$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28-Setup.exe"
 $kind   = "EXE"
 $silent = "/VERYSILENT"
 
