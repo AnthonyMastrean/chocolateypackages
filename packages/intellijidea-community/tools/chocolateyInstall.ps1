edc8b3ce211cf27f576813d95498ac8a49c94347
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-09 11:43:48 -0400

    intellijidea.13.1.5

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = "intellijidea-community"
-$url    = "http://download.jetbrains.com/idea/ideaIC-13.1.2.exe"
-$kind   = "EXE"
+﻿$name = "intellijidea-community"
+$url = "http://download.jetbrains.com/idea/ideaIC-13.1.5.exe"
+$kind = "EXE"
 $silent = "/S"
 
 Install-ChocolateyPackage $name $kind $silent $url
--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = "intellijidea-ultimate"
-$url    = "http://download.jetbrains.com/idea/ideaIU-13.1.2.exe"
-$kind   = "EXE"
+﻿$name = "intellijidea-ultimate"
+$url = "http://download.jetbrains.com/idea/ideaIU-13.1.5.exe"
+$kind = "EXE"
 $silent = "/S"
 
 Install-ChocolateyPackage $name $kind $silent $url
