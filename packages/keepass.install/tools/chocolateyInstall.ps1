960e99e39d71b5269820c2a9cdcc1c3613e3ea94
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-09 11:40:10 -0400

    keepass.2.28

--- packages/keepass.install/tools/chocolateyInstall.ps1
+++ packages/keepass.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = "keepass.install"
-$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.27/KeePass-2.27-Setup.exe"
-$kind   = "EXE"
+﻿$name = "keepass.install"
+$url = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28-Setup.exe"
+$kind = "EXE"
 $silent = "/VERYSILENT"
 
 Install-ChocolateyPackage $name $kind $silent $url
--- packages/keepass.portable/tools/chocolateyInstall.ps1
+++ packages/keepass.portable/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
-﻿$name    = "keepass.portable"
-$url     = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.27/KeePass-2.27.zip"
+﻿$name = "keepass.portable"
+$url = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28.zip"
 
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
 
-$gui     = Join-Path $content "KeePass.exe.gui"
+$gui = Join-Path $content "KeePass.exe.gui"
 
-New-Item $content -Type Directory -Force
-New-Item $gui -Type File -Force
+New-Item $content -Type Directory -Force | Out-Null
+New-Item $gui -Type File -Force | Out-Null
 
 Install-ChocolateyZipPackage $name $url $content
