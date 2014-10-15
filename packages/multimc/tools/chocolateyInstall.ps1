189b6ea846cfd6836bda0644b9e86c9cf20bad9d
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-15 16:41:43 -0400

    multimc.0.4.2

---
+++ packages/multimc/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$id = "multimc"
+$url = "http://files.multimc.org/downloads/mmc-stable-win32.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+$multimc = Join-Path $content "MultiMC"
+$gui = Join-Path $multimc "MultiMC.exe.gui"
+$updater = Join-Path $multimc "updater.exe.ignore"
+
+Install-ChocolateyZipPackage $id $url $content
+
+New-Item $gui -Type File | Out-Null
+New-Item $updater -Type File | Out-Null
