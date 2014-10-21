0f97337a434fa66841d70f6abcb97cbeb8019b07
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-10-21 11:32:44 -0400

    dejavufonts.2.34

--- packages/dejavufonts/tools/chocolateyInstall.ps1
+++ packages/dejavufonts/tools/chocolateyInstall.ps1
@@ -1,15 +1,15 @@
 ﻿$name = "dejavufonts"
-$url  = "http://downloads.sourceforge.net/project/dejavu/dejavu/2.33/dejavu-fonts-ttf-2.33.zip"
+$url = "http://downloads.sourceforge.net/project/dejavu/dejavu/2.34/dejavu-fonts-ttf-2.34.zip"
 
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
-$zip     = Join-Path $content "dejavu-fonts-ttf-2.33\ttf"
+$zip = Join-Path $content "dejavu-fonts-ttf-2.34\ttf"
 
 Install-ChocolateyZipPackage $name $url $content
 
 try {
   $FONT_FOLDER = 0x14
-  $shell = New-Object -ComObject Shell.Application
+  $shell = New-Object -ComObject "Shell.Application"
   $source = $shell.Namespace($zip)
   $target = $shell.Namespace($FONT_FOLDER)
   $target.CopyHere($source.Items())
@@ -17,6 +17,6 @@ try {
   Write-ChocolateySuccess $name
 } 
 catch {
-  Write-ChocolateyFailure $name $($_.Exception.Message)
+  Write-ChocolateyFailure $name $_.Exception.Message
   throw 
 }
