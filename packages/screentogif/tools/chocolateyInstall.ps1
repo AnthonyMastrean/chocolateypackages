08925c57df8863594d2775d504388cdc4b8967d3
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-11-11 14:56:34 -0500

    screentogif.1.3

---
+++ packages/screentogif/tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$id = "screentogif"
+$url = "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=screentogif&DownloadId=922737&FileTime=130583910278970000&Build=20941"
+$url64 = ""
+$kind = ""
+$silent = ""
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage $id $url $content
