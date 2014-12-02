a4448e189427499a2559d12f030568115d73cce6
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-12-02 16:10:36 -0500

    sikuli.1.0-rc3

--- packages/sikuli/tools/chocolateyInstall.ps1
+++ packages/sikuli/tools/chocolateyInstall.ps1
@@ -1,20 +1,22 @@
 ﻿$name    = 'sikuli'
 $url     = 'http://launchpad.net/sikuli/sikulix/x1.0-rc3/+download/Sikuli-X-1.0rc3%20%28r905%29-win32.zip'
+
 $tools   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
+
 $sikuli  = Join-Path $content 'Sikuli-X-1.0rc3 (r905)-win32\Sikuli-IDE'
 $86      = Join-Path $sikuli 'sikuli-ide.bat'
 $64      = Join-Path $sikuli 'sikuli-ide-w.bat'
 $icon    = Join-Path $sikuli 'ide-icon.ico' 
 
+$is64bit = Get-ProcessorBits 64
+$target  = @{$true = $64; $false = $86}[$is64bit]
+$desktop = [Environment]::GetFolderPath("Desktop")
+$link    = Join-Path $desktop 'Sikuli IDE.lnk'
+
 try 
 {     
     Install-ChocolateyZipPackage $name $url $content
-
-    $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
-    $target  = if($is64bit) { $64 } else { $86 }
-    $desktop = [Environment]::GetFolderPath("Desktop")
-    $link    = Join-Path $desktop 'Sikuli IDE.lnk'
 	
     $shell = New-Object -ComObject "Wscript.Shell"
     $shortcut = $shell.CreateShortcut($link)
@@ -27,6 +29,6 @@ try
 } 
 catch 
 {
-    Write-ChocolateyFailure $name "$($_.Exception.Message)"
+    Write-ChocolateyFailure $name $_.Exception.Message
     throw 
 }
