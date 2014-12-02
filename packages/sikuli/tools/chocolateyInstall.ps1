6366462352bbab965977488cc82a3c9ad95dc397
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-12-02 15:30:14 -0500

    sikuli.1.0-rc3

--- packages/sikuli/tools/chocolateyInstall.ps1
+++ packages/sikuli/tools/chocolateyInstall.ps1
@@ -1,14 +1,14 @@
-﻿try 
-{ 
-    $name    = 'sikuli'
-    $url     = 'http://launchpad.net/sikuli/sikuli-x/x1.0-rc3/+download/Sikuli-X-1.0rc3%20%28r905%29-win32.zip'
-    $tools   = Split-Path -parent $MyInvocation.MyCommand.Definition
-    $content = Join-Path (Split-Path $tools) 'content'
-    $sikuli  = Join-Path $content 'Sikuli-X-1.0rc3 (r905)-win32\Sikuli-IDE'
-    $86      = Join-Path $sikuli 'sikuli-ide.bat'
-    $64      = Join-Path $sikuli 'sikuli-ide-w.bat'
-    $icon    = Join-Path $sikuli 'ide-icon.ico'	
-    
+﻿$name    = 'sikuli'
+$url     = 'http://launchpad.net/sikuli/sikulix/x1.0-rc3/+download/Sikuli-X-1.0rc3%20%28r905%29-win32.zip'
+$tools   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$sikuli  = Join-Path $content 'Sikuli-X-1.0rc3 (r905)-win32\Sikuli-IDE'
+$86      = Join-Path $sikuli 'sikuli-ide.bat'
+$64      = Join-Path $sikuli 'sikuli-ide-w.bat'
+$icon    = Join-Path $sikuli 'ide-icon.ico' 
+
+try 
+{     
     Install-ChocolateyZipPackage $name $url $content
 
     $is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
