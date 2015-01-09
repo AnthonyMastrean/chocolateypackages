971fffbfba083570f0b95de5ba7b3e45175e73d5
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-01-09 00:03:44 -0500

    Remove unpublished packages

--- packages/svn-diffcmd-diffmerge/content/diff.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-SET DIFF=C:\Program Files\SourceGear\Common\DiffMerge\sgdm.exe
-
-SET LEFT=%6
-SET RIGHT=%7
-
-"%DIFF%" "%LEFT%" "%RIGHT%"
--- packages/switchmspec/chocolateyInstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿$name  = 'mspec-version-switcher'
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-$path  = Join-Path $tools 'Install-MSpec.ps1'
-
-Install-ChocolateyPowershellCommand $name $path
