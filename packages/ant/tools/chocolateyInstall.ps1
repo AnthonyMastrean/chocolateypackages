f0330e9b2ec7baa00de3fa85187334f1935733c3
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-02-09 10:54:59 -0500

    ant.1.9.4.1

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -1,17 +1,8 @@
 ﻿$id = "ant"
 $url = "http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.zip"
-$root = Join-Path (Get-BinRoot) "apache-ant-1.9.4"
-$bin = Join-Path $root "bin"
+$bin = Join-Path (Get-BinRoot) (Join-Path "apache-ant-1.9.4" "bin")
 
 Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation (Get-BinRoot)
 
-try {
-  Install-ChocolateyPath -PathToInstall $bin -PathType "Machine"
-  Install-ChocolateyEnvironmentVariable -VariableType "Machine" -VariableName "ANT_HOME" -VariableValue $root
-  Install-ChocolateyEnvironmentVariable -VariableType "Machine" -VariableName "ANT_OPTS" -VariableValue "-Xms256M -Xmx512M"
-  
-  Write-ChocolateySuccess $id
-} catch {
-  Write-ChocolateyFailure $id $_.Exception.Message
-  throw
-}
+Write-Host "(Optional) Set your machine ANT_HOME and restart your shell:"
+Write-Host "  PS> [System.Environment]::SetEnvironmentVariable('ANT_HOME', '$bin', 'Machine')"
