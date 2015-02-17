c373014de603744efc00b31eb0f58294fd1ef80b
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-02-17 10:01:15 -0500

    keypose.1.0.0

---
+++ packages/keypose/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$id = "keypose"
+$url = "http://katastrophos.net/magnus/blog/wp-content/uploads/2008/09/keypose.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage $id $url $content
