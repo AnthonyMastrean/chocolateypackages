89726ca9c7d66e5831db14769521366ed5b4077e
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-02 14:32:54 -0500

    allparis.1.2.1

---
+++ packages/allpairs/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$id = "allpairs"
+$url = "http://www.satisfice.com/tools/pairs.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage $id $url $content
