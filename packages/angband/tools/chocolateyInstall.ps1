e36519ad772619b5d1d7737fa922ea5e067841a5
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-09 13:40:35 -0400

    angband.3.5.1 GUI Bins

--- packages/angband/tools/chocolateyInstall.ps1
+++ packages/angband/tools/chocolateyInstall.ps1
@@ -9,8 +9,11 @@ $target = Join-Path $content "angband-3.5.1\angband.exe"
 $folder = "CommonPrograms"
 $description = "Angband"
 
+. $tools\bins.ps1
 . $tools\shortcut.ps1
 
 Install-ChocolateyZipPackage $id $url $content
+
+New-GuiBin -Name $target
 
 New-Shortcut -Link $link -Target $target -SpecialFolder $folder -Description $description
