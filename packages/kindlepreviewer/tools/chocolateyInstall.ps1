7e4e462dd23a522e837ac44d095e42c3bf3d00de
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-18 19:51:36 -0400

    kindlepreviewer.2.94

---
+++ packages/kindlepreviewer/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$id     = "kindlepreviewer"
+$url    = "http://kindlepreviewer.s3.amazonaws.com/KindlePreviewerInstall.exe"
+$kind   = "EXE"
+$silent = "/S"
+
+Install-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -Url $url
---
+++ packages/kindlepreviewer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+$id     = "kindlepreviewer"
+$name   = "Kindle Previewer"
+$kind   = "EXE"
+$silent = "/S"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\uninstall.ps1
+
+$uininstaller = Get-Uninstaller -Name $name
+
+Uninstall-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -File $uninstaller
