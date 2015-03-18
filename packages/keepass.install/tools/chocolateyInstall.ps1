5cc94f3d7f3ff999f16da471b5334efe8a2c9470
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-18 18:44:37 -0400

    keepass.2.28.0.1

--- packages/keepass.install/tools/chocolateyInstall.ps1
+++ packages/keepass.install/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name = "keepass.install"
-$url = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28-Setup.exe"
-$kind = "EXE"
+﻿$id     = "keepass.install"
+$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28-Setup.exe"
+$kind   = "EXE"
 $silent = "/VERYSILENT"
 
-Install-ChocolateyPackage $name $kind $silent $url
+Install-ChocolateyPackage -PackageName $id -FileType $kind -SilentArgs $silent -Url $url
