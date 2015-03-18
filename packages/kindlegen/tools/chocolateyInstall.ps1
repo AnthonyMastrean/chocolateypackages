e9bfeb998f2b3e23feecfb82b74c99f514fd36d4
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-18 19:37:00 -0400

    kindlegen.2.9

---
+++ packages/kindlegen/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$id  = "kindlegen"
+$url = "http://kindlegen.s3.amazonaws.com/kindlegen_win32_v2_9.zip"
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
