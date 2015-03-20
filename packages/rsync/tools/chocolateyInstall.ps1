e242f1a8867cce2274a92830680cd6c614ee5bb4
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-19 23:24:17 -0400

    rsync

--- packages/rsync/tools/chocolateyInstall.ps1
+++ packages/rsync/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$id = "rsync"
+﻿$id  = "rsync"
 $url = "https://www.itefix.net/dl/cwRsync_5.4.1_x86_Free.zip"
 
-$tools = Split-Path $MyInvocation.MyCommand.Definition
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
 $cw_home = Join-Path $content "cwRsync_5.4.1_x86_Free"
 
@@ -9,12 +9,8 @@ $cw_home = Join-Path $content "cwRsync_5.4.1_x86_Free"
 
 Install-ChocolateyZipPackage $id $url $content
 
-try {
-  Get-ChildItem $cw_home\*.exe | %{ New-IgnoreBin -Name $_.fullname }
-  Install-ChocolateyPath $cw_home "Machine"
+Get-ChildItem $cw_home\*.exe | %{ New-IgnoreBin -Name $_.fullname }
 
-  Write-ChocolateySuccess $id
-} catch {
-  Write-ChocolateyFailure $id $_.Exception.Message
-  throw
-}
+# This is the only way these exes talk to each other, I've tried everything
+# else, including letting Chocolatey create shims. No luck.
+Install-ChocolateyPath $cw_home "Machine"
