1d791a48162a210e6d2f10b2fd5c294521742e54
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-23 10:47:38 -0400

    depends.2.2

--- packages/dependencywalker/tools/chocolateyInstall.ps1
+++ packages/dependencywalker/tools/chocolateyInstall.ps1
@@ -1,6 +1,13 @@
-﻿$name = 'dependencywalker'
-$url = 'http://www.dependencywalker.com/depends22_x86.zip'
-$url64 = 'http://www.dependencywalker.com/depends22_x64.zip'
-$tools = Split-Path -parent $MyInvocation.MyCommand.Definition
+﻿$id    = "dependencywalker"
+$url   = "http://www.dependencywalker.com/depends22_x86.zip"
+$url64 = "http://www.dependencywalker.com/depends22_x64.zip"
 
-Install-ChocolateyZipPackage $name $url $tools $url64
+$tools   = Split-Path -parent $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+$depends = Join-Path $content "depends.exe"
+
+. $tools\bins.ps1
+
+Install-ChocolateyZipPackage -PackageName $id -Url $url -Url64 $url64 -UnzipLocation $content
+
+New-GuiBin -Name $depends
