310525670fe9a26162de33db8287663d104b0bec
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-27 10:21:50 -0400

    liquibase.3.3.2

--- packages/liquibase/tools/chocolateyInstall.ps1
+++ packages/liquibase/tools/chocolateyInstall.ps1
@@ -1,9 +1,12 @@
-﻿$name   = 'liquibase'
-$url    = 'http://sourceforge.net/projects/liquibase/files/Liquibase%20Core/liquibase-3.3.2-bin.zip/download'
-           
+﻿$id  = 'liquibase'
+$url = 'http://sourceforge.net/projects/liquibase/files/Liquibase%20Core/liquibase-3.3.2-bin.zip/download'
+
 $tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
 $bat     = Join-Path $content 'liquibase.bat'
+$shim    = Join-Path $ENV:chocolateyInstall 'bin\liquibase.exe'
+$shimgen = Join-Path $ENV:chocolateyInstall 'tools\shimgen.exe'
 
-Install-ChocolateyZipPackage $name $url $content
-Generate-BinFile $name $bat
+Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
+
+& $shimgen --output="$shim" --path="$bat"
