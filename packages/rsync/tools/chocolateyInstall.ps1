0b80b795cf86c62480a84f157dcaadbef54bfbed
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-04-08 17:45:36 -0400

    rsync.5.4.1.1

--- packages/rsync/tools/chocolateyInstall.ps1
+++ packages/rsync/tools/chocolateyInstall.ps1
@@ -8,22 +8,19 @@ $shimgen    = Join-Path $ENV:chocolateyInstall "tools\shimgen.exe"
 
 . $tools\bins.ps1
 
-Install-ChocolateyZipPackage $id $url $content
+Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
 
 # This package requires the RSYNC_HOME directory on the PATH and a HOME
-# environment variable be available. I have provided reliable batch files that
-# perform these functions.
-Move-Item $tools\*.bat $rsync_home -Force
-
-# We have to tell Chocolatey to -ignore- the existing executables, because it"s
-# built-in shim generator cannot satisfy these requirements.
-Get-ChildItem $rsync_home\*.exe | New-IgnoreBin
-
-# Finally, we create our own shims to the custom batch files.
-Get-ChildItem $rsync_home\*.bat | %{
+# environment variable. This cannot be provided with Chocolatey's automatic
+# shimming. I have to shim custom batch files.
+Get-ChildItem $tools\*.bat | %{
   $bat      = $_
   $basename = (Split-Path -Leaf $bat) -replace ".bat", ".exe"
   $exe      = Join-Path $ENV:chocolateyInstall "bin\$basename"
 
+  (Get-Content $bat -Raw) -replace '@rsync_home@', $rsync_home | Set-Content $bat
+
   & $shimgen --output="$exe" --path="$bat"
 }
+
+Get-ChildItem $rsync_home\*.exe | New-IgnoreBin
--- packages/rsync/tools/chocolateyUninstall.ps1
+++ packages/rsync/tools/chocolateyUninstall.ps1
@@ -1,5 +1,14 @@
-# We have to clean up our custom shims
-@("rsync.exe", "ssh.exe", "ssh-keygen.exe") `
+$id   = "rsync"
+$file = "cwRsync_5.4.1_x86_Free.zip"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+Uninstall-ChocolateyZipPackage -PackageName $id -ZipFileName $file
+
+# Remove the custom shims
+Get-ChildItem $tools\*.bat `
+  | Split-Path -Leaf `
+  | %{ $_ -replace ".bat", ".exe" } `
   | %{ Join-Path $ENV:chocolateyInstall "bin\$_" } `
   | ?{ Test-Path $_ } `
   | Remove-Item -Force
