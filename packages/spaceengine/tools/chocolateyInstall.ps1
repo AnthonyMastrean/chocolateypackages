0c0cacb97298b8cef1d383832a3856feb8b2e728
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-04-15 18:00:36 -0400

    spaceengine.0.9.7.2

---
+++ packages/spaceengine/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$id      = "spaceengine"
+$url     = "https://content.wuala.com/contents/DDDuke/Latest/SE-0972-setup.exe"
+$kind    = "EXE"
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) "content"
+
+. $tools\bins.ps1
+
+$silent  = "/VERYSILENT /NORESTART /DIR='$content'"
+
+Install-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -Url $url
+
+Get-ChildItem *.exe -Recurse | New-IgnoreBin
---
+++ packages/spaceengine/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+$id     = "spaceengine"
+$name   = "SpaceEngine version 0.9.7.2"
+$kind   = "EXE"
+$silent = "/VERYSILENT /NORESTART"
+
+$tools  = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\uninstall.ps1
+
+$uninstaller = Get-Uninstaller -Name $name
+
+Uninstall-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -File $uninstaller
