2b9e91af3439e1dbb24a5c7340d607090e8e32ce
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-04-22 12:03:13 -0400

    keepass maintained in dtgm's repository

--- packages/keepass.install/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$id     = "keepass.install"
-$url    = "http://downloads.sourceforge.net/project/keepass/KeePass%202.x/2.28/KeePass-2.28-Setup.exe"
-$kind   = "EXE"
-$silent = "/VERYSILENT"
-
-Install-ChocolateyPackage -PackageName $id -FileType $kind -SilentArgs $silent -Url $url
