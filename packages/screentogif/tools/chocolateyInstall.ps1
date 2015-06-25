6aad17593024e95f85aefaf4df1ae22f03046634
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-06-25 12:00:18 -0400

    (GH-111) screen to gif uh... nothing update?

--- packages/screentogif/tools/chocolateyInstall.ps1
+++ packages/screentogif/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$id   = "screentogif"
+$name = "Screen to Gif"
 $url  = "https://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=screentogif&DownloadId=1425263&FileTime=130672346330530000&Build=20959"
-$link = "Screen to Gif"
 
 $tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
@@ -10,4 +10,4 @@ $bin     = Join-Path $content "ScreenToGif 1.4.1.exe"
 
 Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
 
-New-Shortcut -Link $link -SpecialFolder "CommonPrograms" -Target $bin
+New-Shortcut -Link $name -Target $bin -SpecialFolder "CommonPrograms"
--- packages/screentogif/tools/chocolateyUninstall.ps1
+++ packages/screentogif/tools/chocolateyUninstall.ps1
@@ -1,8 +1,8 @@
 $id   = "screentogif"
-$link = "Screen to Gif"
+$name = "Screen to Gif"
 
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$tools = Split-Path $MyInvocation.MyCommand.Definition
 
 . $tools\shortcut.ps1
 
-Remove-Shortcut -Link $link -SpecialFolder "CommonPrograms"
+Remove-Shortcut -Link $name -SpecialFolder "CommonPrograms"
