c273dcdb4153a8db6e4780b587f3a0d00c44e74f
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-06-30 11:08:15 -0400

    Update ant.1.9.5 and set remove shim.
    Fixes #117
    Fixes #116
    Fixes #114
    Fixes #112
    Fixes #90

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -1,12 +1,15 @@
 ﻿$id  = "ant"
-$url = "http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.zip"
+$url = "http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.5-bin.zip"
+$md5 = "a12331d16ba75edf64dbcb3232e5a41d"
 
 $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
-$ant_home = Join-Path $content "apache-ant-1.9.4"
-$ant_bin  = Join-Path $ant_home "bin"
+$ant_home = Join-Path $content "apache-ant-1.9.5"
+$ant_bat  = Join-Path $ant_home "bin/ant.bat"
 
-Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
+Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content -Checksum $md5 -ChecksumType "md5"
 Install-ChocolateyEnvironmentVariable -VariableName "ANT_HOME" -VariableValue $ant_home -VariableType "Machine"
+
+Install-BinFile -Name "ant" -Path $ant_bat
 
 Update-SessionEnvironment
---
+++ packages/ant/tools/chocolateyUninstall.ps1
@@ -0,0 +1,8 @@
+$id = "ant"
+
+$tools    = Split-Path $MyInvocation.MyCommand.Definition
+$content  = Split-Path $tools
+$ant_home = Join-Path $content "apache-ant-1.9.5"
+$ant_bat  = Join-Path $ant_home "bin/ant.bat"
+
+Uninstall-BinFile -Name "ant" -Path $ant_bat
