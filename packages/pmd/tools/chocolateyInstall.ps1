8b950332eb6e2e4fd6c6eb95c8f44eac81f81aa6
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-07-07 17:00:27 -0400

    (GH-120) PMD includes cpd.bat

--- packages/pmd/tools/chocolateyInstall.ps1
+++ packages/pmd/tools/chocolateyInstall.ps1
@@ -5,10 +5,12 @@ $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
 $pmd_home = Join-path $content "pmd-bin-5.3.2"
 $pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
+$cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
 
 Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
 Install-ChocolateyEnvironmentVariable -VariableName "PMD_HOME" -VariableValue $pmd_home -VariableType "Machine"
 
 Install-BinFile -Name "pmd" -Path $pmd_bin
+Install-BinFile -Name "cpd" -Path $cpd_bin
 
 Update-SessionEnvironment
--- packages/pmd/tools/chocolateyUninstall.ps1
+++ packages/pmd/tools/chocolateyUninstall.ps1
@@ -4,5 +4,7 @@ $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
 $pmd_home = Join-Path $content "pmd-bin-5.3.2"
 $pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
+$cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
 
 Uninstall-BinFile -Name "pmd" -Path $pmd_bin
+Uninstall-BinFile -Name "cpd" -Path $cpd_bin
