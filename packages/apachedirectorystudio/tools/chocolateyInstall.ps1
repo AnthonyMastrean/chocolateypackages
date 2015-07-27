b21ba1d6b3fd433411509ea78aaace4a673ae41c
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-07-27 10:47:30 -0400

    Apache Directory Studio 2.0.0-M8

--- packages/apachedirectorystudio/tools/chocolateyInstall.ps1
+++ packages/apachedirectorystudio/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
 ﻿$name   = "apachedirectorystudio"
-$url    = "http://psg.mtu.edu/pub/apache//directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86-2.0.0.v20130628.exe"
-$url64  = "http://psg.mtu.edu/pub/apache//directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86_64-2.0.0.v20130628.exe"
+$url    = "https://archive.apache.org/dist/directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86-2.0.0.v20130628.exe"
+$url64  = "https://archive.apache.org/dist/directory/studio/dist/2.0.0.v20130628/ApacheDirectoryStudio-win32-x86_64-2.0.0.v20130628.exe"
 $kind   = "EXE"
 $silent = "/S"
+$hash   = "d6dcb0537fbd9d8a301eab22a542a193"
+$hash64 = "f0758eb31062e6842e5bfea77447a4cc"
 
-Install-ChocolateyPackage $name $kind $silent $url $url64
+Install-ChocolateyPackage -PackageName $name -Type $kind -Silent $silent -Url $url -Url64 $url64 -Checksum $hash -Checksum64 $hash64
--- packages/apachedirectorystudio/tools/chocolateyUninstall.ps1
+++ packages/apachedirectorystudio/tools/chocolateyUninstall.ps1
@@ -1,1 +1,12 @@
-﻿
+﻿$id     = "apachedirectorystudio"
+$name   = "Apache Directory Studio - (remove only)"
+$kind   = "EXE"
+$silent = "/S"
+
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\uninstall.ps1
+
+$uninstaller = Get-Uninstaller -Name $name
+
+Uninstall-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -File $uninstaller
