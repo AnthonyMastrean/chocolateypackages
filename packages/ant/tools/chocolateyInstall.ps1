15b31e19c8eb88e7b6cf70da85a5991ba66cc190
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-07-27 09:56:16 -0400

    Apache Ant 1.9.6

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$id  = "ant"
-$url = "http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.5-bin.zip"
-$md5 = "a12331d16ba75edf64dbcb3232e5a41d"
+$url = "http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.6-bin.zip"
+$md5 = "a2ab149c0ef45ac6bdcb59cba46efac8"
 
 $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
-$ant_home = Join-Path $content "apache-ant-1.9.5"
+$ant_home = Join-Path $content "apache-ant-1.9.6"
 $ant_bat  = Join-Path $ant_home "bin/ant.bat"
 
 Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content -Checksum $md5 -ChecksumType "md5"
--- packages/ant/tools/chocolateyUninstall.ps1
+++ packages/ant/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $id = "ant"
 
 $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
-$ant_home = Join-Path $content "apache-ant-1.9.5"
+$ant_home = Join-Path $content "apache-ant-1.9.6"
 $ant_bat  = Join-Path $ant_home "bin/ant.bat"
 
 Uninstall-BinFile -Name "ant" -Path $ant_bat
