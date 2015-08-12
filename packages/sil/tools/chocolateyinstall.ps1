eb53131f62cd2c1ceb3f298df39a7b7b10691690
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-08-11 22:29:32 -0400

    Sil 1.2.1

---
+++ packages/sil/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$id      = 'sil'
+$name    = 'Sil'
+$url     = 'http://www.amirrorclear.net/flowers/game/sil/Sil-121-Win.zip'
+$hash    = 'c6f5237fed3a8264806afbeba675a3ac'
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$content = Join-Path $package 'Sil'
+$exe     = Join-Path $content 'sil.exe'
+
+. $tools\shortcut.ps1
+. $tools\bins.ps1
+
+$zip_args = @{
+  packageName   = $id
+  unzipLocation = $package
+  url           = $url
+  checksum      = $hash
+}
+
+$shortcut_args = @{
+  link          = $name
+  target        = $exe
+  specialFolder = 'CommonPrograms'
+}
+
+$bin_args = @{
+  path = $exe
+}
+
+Install-ChocolateyZipPackage @zip_args
+Install-Shortcut @shortcut_args
+Install-IgnoreBin @bin_args
---
+++ packages/sil/tools/chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$id      = 'sil'
+$name    = 'Sil'
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\shortcut.ps1
+
+$shortcut_args = @{
+  link          = $name
+  specialFolder = 'CommonPrograms'
+}
+
+Uninstall-Shortcut @shortcut_args
