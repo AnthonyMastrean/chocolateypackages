c9f61745624703c860058abfdb7e6e6651e80f93
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-08-24 11:49:32 -0400

    Notepad2-mod 4.2.25.955

--- packages/notepad2-mod/tools/chocolateyInstall.ps1
+++ packages/notepad2-mod/tools/chocolateyInstall.ps1
@@ -1,4 +1,11 @@
-﻿$name = 'notepad2-mod'
-$url  = 'https://github.com/XhmikosR/notepad2-mod/releases/download/4.2.25.955/Notepad2-mod.4.2.25.955.exe'
-$args = '/VERYSILENT'
-Install-ChocolateyPackage $name 'EXE' $args $url
+﻿$ErrorActionPreference = 'Stop'
+
+$package_args = @{
+  packageName = 'notepad2-mod'
+  url         = 'https://github.com/XhmikosR/notepad2-mod/releases/download/4.2.25.955/Notepad2-mod.4.2.25.955.exe'
+  checksum    = '8778ec79de94c8bedbbd0ebd27841f01'
+  fileType    = 'EXE'
+  silentArgs  = '/VERYSILENT /NORESTART'
+}
+
+Install-ChocolateyPackage @package_args
---
+++ packages/notepad2-mod/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+$tools = Split-Path $MyInvocation.MyCommand.Definition
+
+. $tools\uninstall.ps1
+
+$package_args = @{
+  packageName = 'notepad2-mod'
+  fileType    = 'EXE'
+  silentArgs  = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
+  file        = Get-Uninstaller -Name 'Notepad2-mod 4.2.25.955'
+}
+
+Uninstall-ChocolateyPackage @package_args
