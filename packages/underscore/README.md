75b1c954da9d995dc01a8adea63a304722324119
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-08-27 00:45:01 -0400

    Underscore Mode

--- packages/underscore/README.md
+++
@@ -1,3 +0,0 @@
-# Underscore Mode
-
-Make the spacebar insert underscores instead of spaces. Enter underscore mode by pressing `CTRL+SHIFT+U`. Exit underscore mode by pressing `Tab`, `Esc`, or `Enter`.
---
+++ packages/underscore/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$exe     = Join-Path $content 'underscore.exe'
+
+. $tools\shortcut.ps1
+
+Get-ChocolateyWebFile `
+  -PackageName  'underscore' `
+  -Url          'https://github.com/AnthonyMastrean/autohotkey/releases/download/20150826/underscore.exe' `
+  -Checksum     '48e961d2350cc4cdb28ab4dfe4af29be' `
+  -FileFullPath $exe
+
+Install-Shortcut `
+  -Link          'Underscore Mode' `
+  -Target        $exe `
+  -SpecialFolder 'CommonStartup' `
+  -Launch
