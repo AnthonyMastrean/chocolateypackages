f6edfdf50b60400e46f9e8f10aa3cbd68933a645
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-08-27 21:53:16 -0400

    Microsoft HAV Detection Tool 1.0

---
+++ packages/havdetectiontool/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$exe     = Join-Path $content 'havdetectiontool.exe'
+
+Get-ChocolateyWebFile `
+  -PackageName  'havdetectiontool' `
+  -Url          'http://download.microsoft.com/download/1/9/F/19FD407F-A7E9-4393-A845-D0B1F539678E/havdetectiontool.exe' `
+  -Checksum     'ed10ff694ceeb71c24af25d9f3c2b964' `
+  -FileFullPath $exe
