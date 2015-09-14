431f88b4bcddae36ab9dd7de1641ba97bbfbcbf6
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-09-14 18:44:07 -0400

    Bind Tools Only 9.10.2

--- packages/bind-toolsonly/tools/chocolateyInstall.ps1
+++ packages/bind-toolsonly/tools/chocolateyInstall.ps1
@@ -1,38 +1,35 @@
-﻿$name = "bind-toolsonly"
-$url  = "http://ftp.isc.org/isc/bind9/9.9.3-P1/BIND9.9.3-P1.zip"
-
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
-$temp    = Join-Path (Split-Path $tools) "temp"
-$content = Join-Path (Split-Path $tools) "content"
+﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
 
 $keep = @(
-  "arpaname.exe",
-  "bindevt.dll",
-  "dig.exe",
-  "host.exe",
-  "libbind9.dll",
-  "libdns.dll",
-  "libeay32.dll",
-  "libisc.dll",
-  "libisccc.dll",
-  "libisccfg.dll",
-  "liblwres.dll",
-  "libxml2.dll",
-  "nslookup.exe",
-  "nsupdate.exe",
-  "readme1st.txt"
-) | %{ Join-Path $temp $_ }
+  'arpaname.exe',
+  'bindevt.dll',
+  'BINDInstall.exe',
+  'delv.exe',
+  'dig.exe',
+  'host.exe',
+  'libbind9.dll',
+  'libdns.dll',
+  'libeay32.dll',
+  'libirs.dll',
+  'libisc.dll',
+  'libisccc.dll',
+  'libisccfg.dll',
+  'liblwres.dll',
+  'libxml2.dll',
+  'nslookup.exe',
+  'nsupdate.exe',
+  'readme1st.txt'
+)
 
-try {
-  Install-ChocolateyZipPackage $name $url $temp
-  
-  New-Item $content -Type Directory
-  Move-Item -Path $keep -Destination $content
-  Remove-Item $temp -Force -Recurse
+Install-ChocolateyZipPackage `
+  -PackageName 'bind' `
+  -Url 'http://ftp.isc.org/isc/bind9/9.10.2-P4/BIND9.10.2-P4.x86.zip' `
+  -Url64 'http://ftp.isc.org/isc/bind9/9.10.2-P4/BIND9.10.2-P4.x64.zip' `
+  -Checksum '9814f6109699c167a0a0996bbc8c2e36' `
+  -Checksum64 '0f59aa797943ccece2e16496c76535ff' `
+  -UnzipLocation $content `
 
-  Write-ChocolateySuccess $name
-} 
-catch {
-  Write-ChocolateyFailure $name $_.Exception.Message
-  throw 
-}
+Get-ChildItem $content `
+  | ?{ $keep -notcontains $_ } `
+  | Remove-Item -Force -Recurse
