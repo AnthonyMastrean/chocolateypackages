3dcd142d7079a18d8e068b8b05be3820805e1401
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-10-11 22:15:02 -0400

    Liquibase 3.4.1

--- packages/liquibase/tools/chocolateyInstall.ps1
+++ packages/liquibase/tools/chocolateyInstall.ps1
@@ -1,10 +1,12 @@
-﻿$id  = 'liquibase'
-$url = 'https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.3.5/liquibase-3.3.5-bin.zip'
-
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
+﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
 $bat     = Join-Path $content 'liquibase.bat'
 
-Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
+Install-ChocolateyZipPackage `
+  -PackageName 'liquibase' `
+  -Url 'https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.1/liquibase-3.4.1-bin.zip' `
+  -UnzipLocation $content
 
-Install-BinFile -Name $id -Path $bat
+Install-BinFile `
+  -Name 'liquibase' `
+  -Path $bat
--- packages/liquibase/tools/chocolateyUninstall.ps1
+++ packages/liquibase/tools/chocolateyUninstall.ps1
@@ -1,7 +1,7 @@
-﻿$id  = 'liquibase'
-
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
+﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
 $bat     = Join-Path $content 'liquibase.bat'
 
-Uninstall-BinFile -Name $id -Path $bat
+Uninstall-BinFile `
+  -Name 'liquibase' `
+  -Path $bat
