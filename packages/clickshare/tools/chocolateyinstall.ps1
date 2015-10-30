7ebbebfc35217ef44cf26507ab32dab2079c53a4
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-10-30 12:52:50 -0400

    ClickShare 1.7.0

---
+++ packages/clickshare/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$content = Join-Path $package 'content'
+$msi     = Join-Path $content 'ClickShare-Launcher-01_07_00_0069-Setup-Win.msi'
+
+Install-ChocolateyZipPackage `
+  -PackageName 'clickshare' `
+  -Url 'http://www.barco.com/tde/(2211021901782421)/R33050021/001007000069/Barco_ApplicationSoftware_R33050021_v01.07.00.69__ClickShare-Launcher-Installer.zip' `
+  -Checksum 'fda0e396b45c30c8c3f5ee84efa8b913' `
+  -UnzipLocation $content
+
+Install-ChocolateyInstallPackage `
+  -PackageName 'clickshare' `
+  -FileType 'MSI' `
+  -SilentArgs 'ACCEPT_EULA=YES /QN' `
+  -File $msi
---
+++ packages/clickshare/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿Uninstall-ChocolateyPackage `
+  -PackageName 'clickshare' `
+  -FileType 'MSI' `
+  -Silent '{42DEF07A-8579-431C-8B7A-0094E3D0888B} /QN'
