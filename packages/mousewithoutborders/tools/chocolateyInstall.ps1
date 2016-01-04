2f30f65302fb337e544e1bbcad147356a6bc926f
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-01-04 17:03:09 -0500

    mousewithoutborders 2.1.5

--- packages/mousewithoutborders/tools/chocolateyInstall.ps1
+++ packages/mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-﻿$name   = 'mousewithoutborders'
-$url    = 'http://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi'
-$type   = 'MSI'
-$silent = '/quiet'
-
-Install-ChocolateyPackage $name $type $silent $url
+﻿Install-ChocolateyPackage `
+  -PackageName 'mousewithoutborders' `
+  -Url 'https://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi' `
+  -Checksum '22db0f8389d7e58607983ac724356891' `
+  -FileType 'MSI' `
+  -SilentArgs '/QN'
---
+++ packages/mousewithoutborders/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+Uninstall-ChocolateyPackage `
+  -PackageName 'mousewithoutborders' `
+  -FileType    'MSI' `
+  -SilentArgs  '{D3BC954F-D661-474C-B367-30EB6E56542E} /QN'
