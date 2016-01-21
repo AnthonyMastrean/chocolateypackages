8709730b5a3f96973a8387435f8eb5a1aea2f3b4
Jakub Bereżański <kuba@berezanscy.pl>
2016-01-21 23:15:08 +0100

    notepad2-mod: make choco correctly report the bitness being installed
    Providing a 64-bit url makes Chocolatey write
     Downloading notepad2-mod 64 bit
    instead of
     Downloading notepad2-mod 32 bit
    on 64-bit systems.

--- packages/notepad2-mod/tools/chocolateyInstall.ps1
+++ packages/notepad2-mod/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$url = 'https://github.com/XhmikosR/notepad2-mod/releases/download/4.2.25.964/Notepad2-mod.4.2.25.964.exe'
 $package_args = @{
   packageName = 'notepad2-mod'
-  url         = 'https://github.com/XhmikosR/notepad2-mod/releases/download/4.2.25.964/Notepad2-mod.4.2.25.964.exe'
+  url         = $url
+  url64       = $url
   checksum    = 'b9c150d3ae4f09264a7a5b736072becb'
   fileType    = 'EXE'
   silentArgs  = '/VERYSILENT /NORESTART'
