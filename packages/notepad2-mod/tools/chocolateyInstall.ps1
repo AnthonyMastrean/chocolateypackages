5426c7432f2af8567c3d6466b3d2389792d9fff7
Jakub Bereżański <kuba@berezanscy.pl>
2016-01-21 23:21:13 +0100

    notepad2-mod: make it clear which hash algorithm is used for the checksum

--- packages/notepad2-mod/tools/chocolateyInstall.ps1
+++ packages/notepad2-mod/tools/chocolateyInstall.ps1
@@ -2,12 +2,13 @@
 
 $url = 'https://github.com/XhmikosR/notepad2-mod/releases/download/4.2.25.964/Notepad2-mod.4.2.25.964.exe'
 $package_args = @{
-  packageName = 'notepad2-mod'
-  url         = $url
-  url64       = $url
-  checksum    = 'b9c150d3ae4f09264a7a5b736072becb'
-  fileType    = 'EXE'
-  silentArgs  = '/VERYSILENT /NORESTART'
+  packageName  = 'notepad2-mod'
+  url          = $url
+  url64        = $url
+  checksum     = 'b9c150d3ae4f09264a7a5b736072becb'
+  checksumType = 'md5'
+  fileType     = 'EXE'
+  silentArgs   = '/VERYSILENT /NORESTART'
 }
 
 Install-ChocolateyPackage @package_args
