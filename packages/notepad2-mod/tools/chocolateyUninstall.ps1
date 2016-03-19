49e7c7f3a52e42e4c18b3b8cab05a9179e3eab29
Jakub Bereżański <kuba@berezanscy.pl>
2016-03-19 22:35:58 +0100

    notepad2-mod: fix uninstallation

--- packages/notepad2-mod/tools/chocolateyUninstall.ps1
+++ packages/notepad2-mod/tools/chocolateyUninstall.ps1
@@ -1,12 +1,13 @@
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 
 . $tools\uninstall.ps1
+. $tools\version.ps1
 
 $package_args = @{
   packageName = 'notepad2-mod'
   fileType    = 'EXE'
   silentArgs  = '/VERYSILENT /NORESTART /SUPPRESSMSGBOXES'
-  file        = Get-Uninstaller -Name 'Notepad2-mod 4.2.25.964'
+  file        = Get-Uninstaller -Name "Notepad2-mod ${version}"
 }
 
 Uninstall-ChocolateyPackage @package_args
