a97ad32a4876eab77fb8ebc8446e8ee53eabd103
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-03-31 16:22:53 -0400

    NetHack 3.6.0

--- packages/nethack/tools/chocolateyInstall.ps1
+++ packages/nethack/tools/chocolateyInstall.ps1
@@ -1,25 +1,25 @@
-﻿$id = "nethack"
-$name = "NetHack for Windows"
-$url = "http://sourceforge.net/projects/nethack/files/nethack/3.4.3/nethack-343-win.zip/download"
-
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) "content"
-$classic = Join-Path $content "NetHack.exe"
-$tiles = Join-Path $content "NetHackW.exe"
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$classic = Join-Path $content 'NetHack.exe'
+$tiles = Join-Path $content 'NetHackW.exe'
 
 . $tools\bins.ps1
 . $tools\shortcut.ps1
 
-Install-ChocolateyZipPackage $id $url $content
+Install-ChocolateyZipPackage `
+    -PackageName 'nethack' `
+    -Url 'https://sourceforge.net/projects/nethack/files/nethack/3.6.0/nethack-360-win-x86-2.zip/download' `
+    -Checksum 'da1dcadb67ccdbe4fa7ffda096565c14' `
+    -UnzipLocation $content
 
-try {
-  New-GuiBin $tiles
+New-GuiBin $tiles
 
-  New-Shortcut -Link "$name (Tiles)" -Target $tiles -SpecialFolder "CommonPrograms"
-  New-Shortcut -Link "$name (Classic)" -Target $classic -SpecialFolder "CommonPrograms"
+New-Shortcut `
+    -Link 'NetHack for Windows (Tiles)' `
+    -Target $tiles `
+    -SpecialFolder 'CommonPrograms'
 
-  Write-ChocolateySuccess $id
-} catch {
-  Write-ChocolateyFailure $id $_.Exception.Message
-  throw
-}
+New-Shortcut `
+    -Link 'NetHack for Windows (Classic)' `
+    -Target $classic `
+    -SpecialFolder 'CommonPrograms'
