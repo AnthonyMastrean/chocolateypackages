8237c348d42702fc96113cc4d11af56fb8472ed2
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-03-31 16:10:26 -0400

    IntelliJ IDEA checksums

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -2,4 +2,5 @@
   -PackageName 'intellijidea-community' `
   -FileType 'EXE' `
   -Silent '/S' `
+  -Checksum '33bcfb38a64a381a9586c1de1a5cb77f' `
   -Url 'https://download.jetbrains.com/idea/ideaIC-2016.1.1.exe'
--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -2,4 +2,5 @@
   -PackageName 'intellijidea-ultimate' `
   -FileType 'EXE' `
   -Silent '/S' `
+  -Checksum '0a1357351b004c31686270ffff8a0351' `
   -Url 'https://download.jetbrains.com/idea/ideaIU-2016.1.1.exe'
