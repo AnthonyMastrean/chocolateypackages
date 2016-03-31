ccf9fca70daf6144431031989822725620cff267
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-03-31 16:07:36 -0400

    Liquibase checksum

--- packages/liquibase/tools/chocolateyInstall.ps1
+++ packages/liquibase/tools/chocolateyInstall.ps1
@@ -5,6 +5,7 @@ $bat     = Join-Path $content 'liquibase.bat'
 Install-ChocolateyZipPackage `
   -PackageName 'liquibase' `
   -Url 'https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.4.2/liquibase-3.4.2-bin.zip' `
+  -Checksum 'a9dd3b92b2a24fcd4e1ac6cd53991b33' `
   -UnzipLocation $content
 
 Install-BinFile `
