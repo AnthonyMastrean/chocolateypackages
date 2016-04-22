4b7213a9f2cac350e39a0168176de99640afc37c
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-04-22 16:50:09 -0400

    Notepad2-mod nuspec improvements

--- packages/notepad2-mod/tools/chocolateyInstall.ps1
+++ packages/notepad2-mod/tools/chocolateyInstall.ps1
@@ -1,18 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $tools = Split-Path $MyInvocation.MyCommand.Definition
+
 . $tools\version.ps1
 
-$url = "https://github.com/XhmikosR/notepad2-mod/releases/download/${version}/Notepad2-mod.${version}.exe"
-$checksumType = 'sha1'
 $package_args = @{
   packageName    = 'notepad2-mod'
-  url            = $url
-  url64          = $url
-  checksum       = $checksum
-  checksum64     = $checksum
-  checksumType   = $checksumType
-  checksumType64 = $checksumType
+  url            = "https://github.com/XhmikosR/notepad2-mod/releases/download/${version}/Notepad2-mod.${version}.exe"
+  checksum       = '45F58D793308F88B19AAEE1B09C352862185DC14'
+  checksumType   = 'SHA1'
   fileType       = 'EXE'
   silentArgs     = '/VERYSILENT /NORESTART'
 }
