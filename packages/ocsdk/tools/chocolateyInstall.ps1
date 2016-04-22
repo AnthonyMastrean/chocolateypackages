1367cf1e72ba23446bfbf1d929dde5da17c24bd0
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-04-22 17:42:55 -0400

    Remove junk package

--- packages/ocsdk/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$name   = "ocsdk"
-$url    = "http://download.microsoft.com/download/6/e/0/6e0ed968-9371-4dad-9c05-5066d9e0c585/OCSDK.msi"
-$kind   = "MSI"
-$silent = "/Q"
-
-Install-ChocolateyPackage $name $kind $silent $url
