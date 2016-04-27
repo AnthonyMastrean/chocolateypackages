695dcac9e02a0e0f0bd5e6affc139f10b206bac7
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-04-27 16:14:06 -0400

    kindle updates

--- packages/kindle/tools/chocolateyInstall.ps1
+++ packages/kindle/tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@
   -PackageName 'kindle' `
   -InstallerType 'EXE' `
   -SilentArgs '/S' `
-  -Url 'http://kindleforpc.amazon.com/43019/KindleForPC-installer-1.14.43019.exe' `
-  -Checksum 'd92901bb2d62535922017bbd0f2b2378'
+  -Url 'https://s3.amazonaws.com/kindleforpc/43061/KindleForPC-installer-1.15.43061.exe' `
+  -Checksum 'a40840ae89a771892732e96685c22096'
