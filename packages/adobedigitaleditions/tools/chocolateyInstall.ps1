3c249072454d9be39fa4819cc59a37772dd4bbf0
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-04-28 11:08:17 -0400

    adobedigitaleditions: this key is supposed to suppress the norton dialog

--- packages/adobedigitaleditions/tools/chocolateyInstall.ps1
+++ packages/adobedigitaleditions/tools/chocolateyInstall.ps1
@@ -1,4 +1,11 @@
-﻿Install-ChocolateyPackage `
+﻿# This key is supposed to suppress the Norton install dialog.
+#
+#   https://support.muse.adobe.com/thread/1976740?start=0&tstart=0
+#
+New-Item -Force -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\N360' | Out-Null
+New-Item -Force -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\N360' | Out-Null
+
+Install-ChocolateyPackage `
     -PackageName 'adobedigitaleditions' `
     -FileType 'EXE' `
     -SilentArgs '/S' `
