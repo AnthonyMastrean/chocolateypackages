3cdf4869fbba994eae1799270a536ab435200e84
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-11 13:22:19 -0400

    Removing unmaintained packages

--- packages/pwnadventure2/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$id    = "pwnadventure2"
-$name  = "Choose Your Pwn Adventure 2"
-
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\shortcut.ps1
-
-Remove-Shortcut -Link $name -SpecialFolder "CommonPrograms"
-Remove-Shortcut -Link $name -SpecialFolder "CommonDesktop"
--- packages/pwnadventure3/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$id    = "pwnadventure3"
-$name  = "Pwn Adventure 3 - Pwnie Island"
-
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\shortcut.ps1
-
-Remove-Shortcut -Link $name -SpecialFolder "CommonPrograms"
-Remove-Shortcut -Link $name -SpecialFolder "CommonDesktop"
