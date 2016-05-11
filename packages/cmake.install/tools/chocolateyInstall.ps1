3763177365cfab0f7debcd100595a171182fadb3
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-11 14:13:02 -0400

    Remove transferred cmake packages

--- packages/cmake.install/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$name   = "cmake.install"
-$kind   = "EXE"
-$silent = "/S"
-$url    = "http://www.cmake.org/files/v2.8/cmake-2.8.12.1-win32-x86.exe"
-
-Install-ChocolateyPackage $name $kind $silent $url
