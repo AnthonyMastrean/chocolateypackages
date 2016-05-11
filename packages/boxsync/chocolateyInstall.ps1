7024cfb03edebe87a41a9d13a656d32b25365558
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-11 13:50:17 -0400

    Remove unmaintained package

--- packages/boxsync/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$name = "boxsync"
-$url = "https://sync.box.com/static/sync/release/BoxSyncInstaller_Win32.msi"
-$url64 = "https://sync.box.com/static/sync/release/BoxSyncInstaller_Win64.msi"
-$kind = "MSI"
-$silent = "/QUIET"
-
-Install-ChocolateyPackage $name $kind $silent $url $url64
