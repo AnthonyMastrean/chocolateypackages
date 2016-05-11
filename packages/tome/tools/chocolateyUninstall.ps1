5dadb9d8502721c32bfe650117fc84d8c5793a04
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-11 14:11:54 -0400

    Remove unmaintained game

--- packages/tome/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$id = "tome"
-$name = "Tales of Maj'Eyal"
-
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\shortcut.ps1
-
-Remove-Shortcut -Link $name -SpecialFolder "CommonDesktop"
-Remove-Shortcut -Link $name -SpecialFolder "CommonPrograms"
