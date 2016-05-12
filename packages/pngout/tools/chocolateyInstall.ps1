05019f046a4dd84b7f5b19460330f0090d7c9e23
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-12 15:25:52 -0400

    pngout update

--- packages/pngout/tools/chocolateyInstall.ps1
+++ packages/pngout/tools/chocolateyInstall.ps1
@@ -1,20 +1,10 @@
-﻿try 
-{ 
-    $name    = 'pngout'
-    $url     = 'http://advsys.net/ken/util/pngout.exe'
-    
-    $tools   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-    $content = Join-Path (Split-Path $tools) 'content'
-    
-    $target  = Join-Path $content 'pngout.exe'
+﻿$tools = Split-Path -parent $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$target = Join-Path $content 'pngout.exe'
 
-    New-Item $content -Type Directory
-    Get-ChocolateyWebFile $name $target $url
-
-    Write-ChocolateySuccess $name
-} 
-catch 
-{
-    Write-ChocolateyFailure $name "$($_.Exception.Message)"
-    throw 
-}
+Get-ChocolateyWebFile `
+    -PackageName 'pngout' `
+    -Url 'http://advsys.net/ken/util/pngout.exe' `
+    -Checksum '843F0BE42E86680C1663C4EF58EB0677ACE15FC29AB23897C83F4B7E5AF3EF36' `
+    -ChecksumType 'SHA256' `
+    -FileFullPath $target
