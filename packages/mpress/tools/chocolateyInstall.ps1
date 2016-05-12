f1c74e9b15d0d968b5e30080a70dec377d83a36c
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-12 15:58:32 -0400

    mpress update

--- packages/mpress/tools/chocolateyInstall.ps1
+++ packages/mpress/tools/chocolateyInstall.ps1
@@ -1,7 +1,9 @@
-﻿$name    = "mpress"
-$url     = "http://www.matcode.com/mpress.219.zip"
+﻿$tools = Split-Path $MyInvocation.MyCommand.Path
+$content = Join-Path (Split-Path $tools) 'content'
 
-$tools   = Split-Path $MyInvocation.MyCommand.Path
-$content = Join-Path (Split-Path $tools) "content"
-
-Install-ChocolateyZipPackage -PackageName $name -Url $url -UnzipLocation $content
+Install-ChocolateyZipPackage `
+    -PackageName 'mpress' `
+    -Url 'https://web.archive.org/web/20150506065200/http://www.matcode.com/mpress.219.zip' `
+    -Checksum '' `
+    -ChecksumType 'SHA256' `
+    -UnzipLocation $content
