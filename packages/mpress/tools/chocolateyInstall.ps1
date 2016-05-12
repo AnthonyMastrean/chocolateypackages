4d2852c627056f454df9cdc4836dfb3b0085b646
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-12 16:07:21 -0400

    mpress checksum

--- packages/mpress/tools/chocolateyInstall.ps1
+++ packages/mpress/tools/chocolateyInstall.ps1
@@ -4,6 +4,6 @@ $content = Join-Path (Split-Path $tools) 'content'
 Install-ChocolateyZipPackage `
     -PackageName 'mpress' `
     -Url 'https://web.archive.org/web/20150506065200/http://www.matcode.com/mpress.219.zip' `
-    -Checksum '' `
+    -Checksum '19FFEE93706DFF67F83D9EF48C0C794DEA761D4459B11C37F9BC65B04AF736C5' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
