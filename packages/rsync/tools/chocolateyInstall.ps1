b035344b18d9979b4a69df2cb594d9df1aefe72f
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-18 15:48:53 -0400

    rsync updates for shims/path

--- packages/rsync/tools/chocolateyInstall.ps1
+++ packages/rsync/tools/chocolateyInstall.ps1
@@ -2,6 +2,8 @@
 $package = Split-Path $tools
 $content = Join-Path $package 'cwRsync_5.5.0_x86_Free'
 
+. $tools\bins.ps1
+
 Install-ChocolateyZipPackage `
     -PackageName 'rsync' `
     -Url 'https://www.itefix.net/dl/cwRsync_5.5.0_x86_Free.zip' `
@@ -13,4 +15,4 @@ Install-ChocolateyZipPackage `
 # environment variable. This cannot be provided with Chocolatey's automatic
 # shimming. I have to shim custom batch files.
 Get-ChildItem $tools\*.bat | %{ Install-BinFile -Name $_.BaseName -Path $_ }
-Get-ChildItem $content\*.exe | %{ New-Item "$_.ignore" -Type 'File' -Force | Out-Null }
+Get-ChildItem $content\bin\*.exe | Install-IgnoreBin
