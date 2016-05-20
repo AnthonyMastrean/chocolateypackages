61d1f64121b8107011cf679d7bc8bcb35fe31490
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-20 17:45:52 -0400

    tfpt 11.0 update

--- packages/tfpt/tools/chocolateyInstall.ps1
+++ packages/tfpt/tools/chocolateyInstall.ps1
@@ -1,19 +1,14 @@
-﻿$name   = 'tfpt'
-$url    = 'http://visualstudiogallery.msdn.microsoft.com/b1ef7eb2-e084-4cb8-9bc7-06c3bad9148f/file/83775/3/Visual%20Studio%20Team%20Foundation%20Server%202012%20Update%202%20Power%20Tools.msi'
-$silent = '/Q ADDLOCAL=ALL'
-$kind   = 'MSI'
+﻿Install-ChocolateyPackage `
+    -PackageName 'tfpt' `
+    -FileType 'MSI' `
+    -SilentArgs '/Q ADDLOCAL=ALL' `
+    -Url 'https://visualstudiogallery.msdn.microsoft.com/b1ef7eb2-e084-4cb8-9bc7-06c3bad9148f/file/83775/3/Visual%20Studio%20Team%20Foundation%20Server%202012%20Update%202%20Power%20Tools.msi' `
+    -Checksum 'A988F052D1D8CB694BB89BF147ACADE2A7E870CAE89B3982D480058CE8AB0B8F' `
+    -ChecksumType 'SHA256'
 
-Install-ChocolateyPackage $name $kind $silent $url
+$programs = @{64=${ENV:PROGRAMFILES(X86)};32=$ENV:PROGRAMFILES}[(Get-ProcessorBits 64)]
+$bin = Join-Path $programs 'Microsoft Team Foundation Server 2012 Power Tools'
 
-try {
-	$is64bit = (Get-WmiObject Win32_Processor).AddressWidth -eq 64
-	$programfiles = if($is64bit){${ENV:PROGRAMFILES(X86)}}else{$ENV:PROGRAMFILES}
-	$bin = Join-Path $programfiles 'Microsoft Team Foundation Server 2012 Power Tools'
-	
-	Install-ChocolateyPath $bin 'Machine'
-	
-	Write-ChocolateySuccess $name
-} catch {
-	Write-ChocolateyFailure $name $_.Exception.Message
-	throw
-}
+Install-ChocolateyPath `
+    -PathToInstall $bin `
+    -PathType 'Machine'
---
+++ packages/tfpt/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+Uninstall-ChocolateyPackage `
+  -PackageName 'tfpt' `
+  -FileType 'MSI' `
+  -Silent '{61E0A691-53D2-4B9E-9043-5034D3CB9508} /QN'
