5efea68de6f5629d25773b13af8bb652f2fe91a1
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-20 17:10:34 -0400

    priorterminal 2.9

--- packages/priorterminal/tools/chocolateyInstall.ps1
+++ packages/priorterminal/tools/chocolateyInstall.ps1
@@ -1,23 +1,22 @@
-﻿$name   = 'priorterminal'
-$url    = 'http://prior.com/downloadcentre/documents/software/PriorTerminalSetup%202.8.zip'
-$kind   = 'MSI'
-$silent = '/Q'
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$msi = Join-Path $content 'PriorTerminalSetup.msi'
+$exe = Join-Path $content 'setup.exe'
 
-$tools = Split-Path $MyInvocation.MyCommand.Definition
-$temp  = Join-Path $ENV:TEMP "chocolatey\$name"
-$zip   = Join-Path $temp "$name.zip"
-$msi   = Join-Path $temp 'PriorTerminalSetup.msi'
+. $tools\bins.ps1
 
-try {
-  New-Item $temp -Type Directory -Force
+Install-ChocolateyZipPackage `
+    -PackageName 'priorterminal' `
+    -Url 'http://www.prior-us.com/files/PriorTerminalSetup_2.9.zip' `
+    -Checksum '5CF0797936209EE61ADB52806AD23A245C97DAD3E5E5280C998BCE70AF92403E' `
+    -ChecksumType 'SHA256' `
+    -UnzipLocation $content
 
-  Get-ChocolateyWebFile $name $zip $url
-  Get-ChocolateyUnzip $zip $temp
-  Install-ChocolateyInstallPackage $name $kind $silent $msi
+Install-ChocolateyInstallPackage `
+    -PackageName 'priorterminal' `
+    -FileType 'MSI' `
+    -SilentArgs '/Q' `
+    -File $msi
 
-  Write-ChocolateySuccess $name
-} 
-catch {
-  Write-ChocolateyFailure $name $($_.Exception.Message)
-  throw 
-}
+Install-IgnoreBin `
+    -Path $exe
---
+++ packages/priorterminal/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+Uninstall-ChocolateyPackage `
+  -PackageName 'priorterminal' `
+  -FileType 'MSI' `
+  -Silent '{3B14D7AF-E637-4D4A-9796-57A863750CF5} /QN'
