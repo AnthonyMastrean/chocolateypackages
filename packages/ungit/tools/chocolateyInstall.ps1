b6f03717f60b0074695301ad3668171b2d98a4e6
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-20 16:23:19 -0400

    ungit 0.10.1

--- packages/ungit/tools/chocolateyInstall.ps1
+++ packages/ungit/tools/chocolateyInstall.ps1
@@ -1,11 +1,1 @@
-﻿$name = "ungit"
-$version = "0.8.4"
-
-try {
-  npm install -g ungit@$version
-  Write-ChocolateySuccess $name
-} 
-catch {
-  Write-ChocolateyFailure $name $_.Exception.Message
-  throw 
-}
+﻿npm install -g ungit@"0.10.1"
