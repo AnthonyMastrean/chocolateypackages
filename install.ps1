a92be97361627b1aa7ec07f1b9f8b02a279e881a
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-24 10:50:26 -0400

    Correct package path

--- install.ps1
+++ install.ps1
@@ -2,10 +2,13 @@ param($id)
 
 $ErrorActionPreference = 'Stop'
 
+$here = Split-Path $MyInvocation.MyCommand.Definition
+$package = Join-Path $here (Join-Path 'packages' $id)
+
 # Run an arbitrary Win32 application so LASTEXITCODE is 0
 setx.exe trigger 1
 
-choco.exe install --force --debug --verbose --yes $id --source "packages\$id;http://chocolatey.org/api/v2/"
+choco.exe install --force --debug --verbose --yes $id --source "$package;http://chocolatey.org/api/v2/"
 
 # Vagrant has issues knowing that there was an error
 if ($LASTEXITCODE -ne 0) {
