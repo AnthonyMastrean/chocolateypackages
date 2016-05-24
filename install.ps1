3e123f0a33b923ffebb8c4ca967c34099cf18f50
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-05-24 10:06:38 -0400

    full CLI switches

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 # Run an arbitrary Win32 application so LASTEXITCODE is 0
 setx.exe trigger 1
 
-choco.exe install -fdvy $id --source "packages\$id;http://chocolatey.org/api/v2/"
+choco.exe install --force --debug --verbose --yes $id --source "packages\$id;http://chocolatey.org/api/v2/"
 
 # Vagrant has issues knowing that there was an error
 if ($LASTEXITCODE -ne 0) {
