cf361934549294f98998f351d07e7b0305c9efea
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-06-09 18:41:59 -0400

    ImageJ 1.51 and ImageJ2

---
+++ packages/imagej2/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$imagej = Join-Path $package 'ImageJ'
+$target = Join-Path $imagej 'ImageJ.exe'
+$shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'ImageJ.lnk'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'imagej' `
+    -Url 'http://wsr.imagej.net/distros/win/ij150-win-nojre.zip' `
+    -Checksum '44E2C83E9E877B075AAB83DDEDFDAF28' `
+    -UnzipLocation $package
+
+Install-ChocolateyShortcut `
+    -ShortcutFilePath $shortcut `
+    -TargetPath $target
