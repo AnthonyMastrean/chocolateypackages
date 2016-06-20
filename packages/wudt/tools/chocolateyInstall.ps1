fb7531af4f2bd05b376654affcb46bf8bb761221
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-06-20 14:47:24 -0400

    wudt updated hash

--- packages/wudt/tools/chocolateyInstall.ps1
+++ packages/wudt/tools/chocolateyInstall.ps1
@@ -1,8 +1,6 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
-$msi = Join-Path $content 'wudt.msi'
-
-. $tools\bins.ps1
+$install = Join-Path $content 'wudt.msi'
 
 # The installer is not silent and doesn't respect it's own flags. It is,
 # however, a self-extracting CAB. We can extract and install the MSI.
@@ -11,10 +9,11 @@ Install-ChocolateyZipPackage `
     -PackageName 'wudt' `
     -UnzipLocation $content `
     -Url 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=wudt&DownloadId=96313&FileTime=129048694816630000&Build=21031' `
-    -Checksum 'af911be206423bf440ea9d4df075a632'
+    -Checksum '6B7CEA4838D892A0A0F625BAB2DF3D378A035C365209DB3C573253F037882229' `
+    -ChecksumType 'SHA256'
 
 Install-ChocolateyInstallPackage `
     -PackageName 'wudt' `
     -FileType 'MSI' `
     -Silent '/QN' `
-    -File $msi
+    -File $install
