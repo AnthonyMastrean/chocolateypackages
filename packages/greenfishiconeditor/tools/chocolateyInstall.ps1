d49756a2856e860a4f6f5155ee2af02a05622f83
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-06-20 15:17:43 -0400

    removed greenfish, dl is on softpedia

--- packages/greenfishiconeditor/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Install-ChocolateyPackage `
-    -PackageName 'greenfishiconeditor' `
-    -Url 'http://download.softpedia.com/dl/dab1810fb472385d8dffab9a2c57500b/5759f22a/100087102/software/multimedia/graphic/gfie_setup_3.5.exe' `
-    -Checksum 'DF09C901A8D7C050438B7BBB21AACA0378A37F5CB21E3C05CC8ECAE71BDAE645' `
-    -ChecksumType 'SHA256' `
-    -FileType 'EXE' `
-    -SilentArgs '/SILENT'
--- packages/greenfishiconeditor/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-Uninstall-ChocolateyPackage `
-    -PackageName 'greenfishiconeditor' `
-    -FileType 'EXE' `
-    -SilentArgs '/SILENT' `
-    -File (Get-UninstallRegistryKey -SoftwareName 'Greenfish Icon Editor Pro 3.5').UninstallString.Trim('"')
