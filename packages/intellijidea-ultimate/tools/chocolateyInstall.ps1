89bf4304b5dfb17f9f867593403a7e991d584834
Jordi Garcia <jordigg@users.noreply.github.com>
2016-07-06 17:32:32 +0100

    Update to version 2016.1.3

--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -3,4 +3,4 @@
   -FileType 'EXE' `
   -Silent '/S' `
   -Checksum '0a1357351b004c31686270ffff8a0351' `
-  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.1.1.exe'
+  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.1.3.exe'
