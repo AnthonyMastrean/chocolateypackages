4b25916a253cf404e63c34d8f48b7dc903eca335
Marty Gentillon <marty.gentillon@gmail.com>
2016-07-20 20:47:05 -0700

    Updated intellijidea community and ultimate to 2016.2
    Changed the sources to refer to version 2016.2.
    Changed ChecksumType on packages to sha256
    Added checksums from jetbrains website (links below)
    Fixed package name in uninstall script.
    
    https://download.jetbrains.com/idea/ideaIC-2016.2.exe.sha256
    https://download.jetbrains.com/idea/ideaIU-2016.2.exe.sha256

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -2,5 +2,6 @@
   -PackageName 'intellijidea-community' `
   -FileType 'EXE' `
   -Silent '/S' `
-  -Checksum '33bcfb38a64a381a9586c1de1a5cb77f' `
-  -Url 'https://download.jetbrains.com/idea/ideaIC-2016.1.1.exe'
+  -Checksum 'baf058709d2f4bdf1fcddbedb8a7541c4a63200d7e800cb959de5a7ed1e53ee8' `
+  -ChecksumType 'sha256' `
+  -Url 'https://download.jetbrains.com/idea/ideaIC-2016.2.exe'
