643e81fece9cc23afe3d401ea82ebdbae4a7ef3d
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-08-02 11:34:52 -0400

    [scansnapmanager] URL moved, fixed 32/64-bit install and uninstall

--- packages/scansnapmanager/tools/chocolateyInstall.ps1
+++ packages/scansnapmanager/tools/chocolateyInstall.ps1
@@ -1,5 +1,8 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$response = Join-Path $tools 'install.iss'
+
+$response32 = Join-Path $tools 'install32.iss'
+$response64 = Join-Path $tools 'install64.iss'
+$response = @{32=$response32;64=$response64}[(Get-ProcessorBits)]
 
 # This an InstallShield installer, requiring a response file (.ISS) for silent
 # installation.
@@ -7,6 +10,7 @@ $response = Join-Path $tools 'install.iss'
 Install-ChocolateyPackage `
     -PackageName 'scansnapmanager' `
     -FileType 'EXE' `
-    -Url 'http://origin.pfultd.com/downloads/IMAGE/driver/ss/mgr/w-s1300/WinS1300ManagerV55L10WW.exe' `
-    -Checksum 'b43aaa8f18b9b82529770779bea5cf24' `
+    -Url 'http://origin.pfultd.com/downloads/IMAGE/driver/ss/mgr/w-s1500/WinS1500ManagerV55L10WW.exe' `
+    -Checksum '236EB287A10598E7D37F5A0F515759A8B4F2F86B5C9B375315B7D1C97DED26D4' `
+    -ChecksumType 'SHA256' `
     -SilentArgs "/S /f1`"$response`""
