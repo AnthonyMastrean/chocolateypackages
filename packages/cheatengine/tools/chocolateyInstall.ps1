3256e447f90c2ede0cf8008fedc7e5c72a5054dd
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-08-02 10:39:51 -0400

    [cheatengine] Do not install the Open Candy recommendation engine (likely considered Adware)

--- packages/cheatengine/tools/chocolateyInstall.ps1
+++ packages/cheatengine/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
   -PackageName 'cheatengine' `
   -FileType 'EXE' `
-  -Silent '/VERYSILENT /NORESTART' `
+  -Silent '/VERYSILENT /NORESTART /NOCANDY' `
   -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/6.5.1/CheatEngine651.exe' `
   -Checksum '74F96ADA4A956BE76D44B2A357C5B0065C90129AA31D6655F87FC37EF5ECC21C' `
   -ChecksumType 'SHA256'
