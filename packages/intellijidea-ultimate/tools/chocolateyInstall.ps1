28f4982a005eb2a250fc85f620be4b52ddef8dd2
Marty Gentillon <marty.gentillon@gmail.com>
2016-09-07 13:52:01 -0700

    Updated Intellij Idea to 2016.2.3
    Updated NuPackage files.
    Changed the sources to refer to version 2016.2.3.
    Updated checksums from jetbrains website (links below).
    Updated uninstall scripts.
    
    https://download.jetbrains.com/idea/ideaIC-2016.2.3.exe.sha256
    https://download.jetbrains.com/idea/ideaIU-2016.2.3.exe.sha256

--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
   -PackageName 'intellijidea-ultimate' `
   -FileType 'EXE' `
   -Silent '/S' `
-  -Checksum '4b5fb56ac8ebd19869d0e4a3652a35bb30109492ca9540b12bae7454cf06af3f' `
+  -Checksum 'b98d2c6d354762ab56eed3bd37ad482fbefa68ac4fe91c310be7b1b901f4f1a0' `
   -ChecksumType 'sha256' `
-  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.2.2.exe'
+  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.2.3.exe'
