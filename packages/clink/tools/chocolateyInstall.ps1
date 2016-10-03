af4c93a58c9352438c606991f4c77a251fbd342b
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-10-03 15:29:57 -0400

    [clink] I'm not supporting this package

--- packages/clink/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Install-ChocolateyPackage `
-  -PackageName 'clink' `
-  -Url 'https://github.com/mridgers/clink/releases/download/0.4.7/clink_0.4.7_setup.exe' `
-  -Checksum 'A661FD6C52C7FF789D383FAF4BFC6579C0660B813F0B2CD3E4CCF87DED7764C9' `
-  -ChecksumType 'SHA256' `
-  -FileType 'EXE' `
-  -SilentArgs '/S'
--- packages/clink/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\uninstall.ps1
-
-Uninstall-ChocolateyPackage `
-  -PackageName 'clink' `
-  -File (Get-Uninstaller -Name 'Clink v0.4.7') `
-  -FileType 'EXE' `
-  -SilentArgs '/S'
