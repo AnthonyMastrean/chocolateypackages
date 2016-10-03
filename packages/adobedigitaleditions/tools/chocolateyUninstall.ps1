6c486583fc7142e0df512760c81bb903029f00ee
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-10-03 15:08:46 -0400

    [adobedigitaleditions] Remove. Statis URL is too much trouble.

--- packages/adobedigitaleditions/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-. $tools\uninstall.ps1
-
-Uninstall-ChocolateyPackage `
-  -PackageName 'adobedigitaleditions' `
-  -FileType 'EXE' `
-  -SilentArgs '/S' `
-  -File (Get-Uninstaller -Name 'Adobe Digital Editions 4.5')
