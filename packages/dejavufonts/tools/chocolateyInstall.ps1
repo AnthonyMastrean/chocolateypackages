4188a376ce80d59bd02bbdde25fee944f39b7073
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-10-03 15:26:19 -0400

    [dejavufonts] 2.37

--- packages/dejavufonts/tools/chocolateyInstall.ps1
+++ packages/dejavufonts/tools/chocolateyInstall.ps1
@@ -1,10 +1,8 @@
-﻿$tools   = Split-Path $MyInvocation.MyCommand.Definition
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$fonts   = Join-Path $package 'dejavu-fonts-ttf-2.35\ttf'
+$fonts = Join-Path $package 'dejavu-fonts-ttf-2.37\ttf'
 
-function Install-Fonts {
-  param([string] $path)
-
+function Install-Fonts($path) {
   $FONT_FOLDER = 0x14
 
   $shell = New-Object -ComObject 'Shell.Application'
@@ -14,9 +12,10 @@ function Install-Fonts {
 }
 
 Install-ChocolateyZipPackage `
-  -PackageName   'dejavufonts' `
-  -Url           'http://sourceforge.net/projects/dejavu/files/dejavu/2.35/dejavu-fonts-ttf-2.35.zip' `
-  -Checksum      'd8b5214d35bcd2bfcb2cffa7795b351d' `
+  -PackageName 'dejavufonts' `
+  -Url 'https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip' `
+  -Checksum '7576310B219E04159D35FF61DD4A4EC4CDBA4F35C00E002A136F00E96A908B0A' `
+  -ChecksumType 'SHA256' `
   -UnzipLocation $package
 
 Install-Fonts `
