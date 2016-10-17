fb3cc2322013f1704715feabfc980cdf559281c4
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-10-17 15:33:46 -0400

    [mousewithoutborders] 2.1.6.1027

--- packages/mousewithoutborders/tools/chocolateyInstall.ps1
+++ packages/mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿Install-ChocolateyPackage `
   -PackageName 'mousewithoutborders' `
   -Url 'https://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi' `
-  -Checksum '22db0f8389d7e58607983ac724356891' `
+  -Checksum '53626A4793BF679B492FD9E16F1337D07B0D95121C4D82A553D320C56DF54FE3' `
+  -ChecksumType 'SHA256' `
   -FileType 'MSI' `
   -SilentArgs '/QN'
