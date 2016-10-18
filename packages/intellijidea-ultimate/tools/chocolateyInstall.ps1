f9d59d6d8ad8c492b92b3793961b758fb020962b
Jordia Garcia <jordi@improbable.io>
2016-10-18 12:27:09 +0100

    update intellij to 2016.2.5

--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
   -PackageName 'intellijidea-ultimate' `
   -FileType 'EXE' `
   -Silent '/S' `
-  -Checksum '74006EC851934C8C1A4BE27656762C6FF38CB847288CE800A2F6FFE1691B72A9' `
+  -Checksum '616161BE7ABA9AC0FC837BBF0D1BC3C1E11A864E0766F5DB5A09D525729B810E' `
   -ChecksumType 'sha256' `
-  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.2.4.exe'
+  -Url 'https://download.jetbrains.com/idea/ideaIU-2016.2.5.exe'
