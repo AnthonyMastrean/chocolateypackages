a7e5057a8bf8c31d3c18cdd703ab3cdbde39c6a1
Marty Gentillon <marty.gentillon@gmail.com>
2016-11-25 16:19:44 -0800

    Fix for race condition bug in Chocolaty 0.10.3
    In 0.10.3, the Chocolaty auto uninstaller is attempting to uninstall the package even though
    it has already been uninstalled.  This occurs when it the auto-uninstaller fails to find the
    recently removed installer.  This change adds a short wait to chocolateyUninstall.ps1 to
    allow time for the changes to propagate in the registry.
    
    This bug is fixed in Chocolaty 0.10.4, where it will no longer fail when the uninstaller is
    not found
    
    see:
    https://github.com/chocolatey/choco/issues/1035

--- packages/intellijidea-ultimate/tools/chocolateyUninstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyUninstall.ps1
@@ -29,6 +29,7 @@ if ($key.Count -eq 1) {
                                 -ValidExitCodes $validExitCodes `
                                 -File "$file"
   }
+  Start-Sleep -Seconds 10
 } elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
 } elseif ($key.Count -gt 1) {
