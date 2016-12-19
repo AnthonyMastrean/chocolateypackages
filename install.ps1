fb8df8d66d7f029a2cf20864bf37720adccac4b0
Anthony Mastrean <anthony.mastrean@gmail.com>
2016-12-19 10:18:55 -0500

    [install] try a 32-bit switch

--- install.ps1
+++ install.ps1
@@ -1,8 +1,12 @@
-param($PackageName)
-
-$ErrorActionPreference = 'Stop'
+param($PackageName, [switch]$Force32)
 
 $here = Split-Path $MyInvocation.MyCommand.Definition
 $source = Join-Path $here 'bin'
 
-choco install --force --yes --source "$source;http://chocolatey.org/api/v2/" $PackageName
+Import-Module $ENV:CHOCOLATEYINSTALL\helpers\chocolateyInstaller.psm1
+
+if ($Force32) {
+    choco install --force --yes --source "$source;http://chocolatey.org/api/v2/" $PackageName --x86
+} else {
+    choco install --force --yes --source "$source;http://chocolatey.org/api/v2/" $PackageName
+}
