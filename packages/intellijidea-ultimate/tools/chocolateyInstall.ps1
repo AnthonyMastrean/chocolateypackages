ec4efd63be12f261496ee1dc5deb41235c430425
Jordi Garcia <jordi@improbable.io>
2017-01-31 15:35:15 +0000

    Update IntelliJIDEA to 2016.3.3

--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'intellijidea-ultimate'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.jetbrains.com/idea/ideaIU-2016.3.2.exe'
-$url64       = 'https://download.jetbrains.com/idea/ideaIU-2016.3.2.exe'
+$url         = 'https://download.jetbrains.com/idea/ideaIU-2016.3.3.exe'
+$url64       = 'https://download.jetbrains.com/idea/ideaIU-2016.3.3.exe'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName   = 'IntelliJ IDEA 2016.3*'
 
-  checksum       = '7498D17DA49B71C6CF40F99288F5383DDC918BB79F4539D7E900507AAFE89F1A'
+  checksum       = 'F2B860EF5618EA11A1510AC64D9EEACF8D3D3627C34B00D43682A5BEF0E12B75'
   checksumType   = 'sha256'
-  checksum64     = '7498D17DA49B71C6CF40F99288F5383DDC918BB79F4539D7E900507AAFE89F1A'
+  checksum64     = 'F2B860EF5618EA11A1510AC64D9EEACF8D3D3627C34B00D43682A5BEF0E12B75'
   checksumType64 = 'sha256'
 
   silentArgs     = '/S'
