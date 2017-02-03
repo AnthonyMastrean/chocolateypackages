9d3a1f47964c062613473bc8c6953576074d84d0
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-02-03 16:40:37 -0500

    [ant] 1.9.8

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$ant_home = Join-Path $package 'apache-ant-1.9.7'
+$ant_home = Join-Path $package 'apache-ant-1.9.8'
 $ant_bat = Join-Path $ant_home 'bin/ant.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'ant' `
-    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.7-bin.zip' `
-    -Checksum '28c89951fa4752b7f0c0126fb6d6fffb9d7cc63cde822f2b8adafa45e96abaf1ff153ffe49be68f9235b8419a1d2fea468dadc19c331cdd4bdbf44354a3ef48a' `
+    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.8-bin.zip' `
+    -Checksum 'ae11cdc41fcf66cbdf62d3c4aee28dcecb3e7bdde01cdd3ddf3005d7e21e24d0c57c96abb4b792f54ca7f9cb8fb0ed2551e1c1860f364357623c5e3ccd4b693a' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $package
 
