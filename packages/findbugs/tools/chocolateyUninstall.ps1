afac6e987354c41bd2d279384fa92c17fb824043
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-02-14 12:12:10 -0500

    findbugs 3.0.1

--- packages/findbugs/tools/chocolateyUninstall.ps1
+++ packages/findbugs/tools/chocolateyUninstall.ps1
@@ -1,8 +1,3 @@
-﻿$id  = "findbugs"
+﻿$shortcut = Join-Path ([System.Environment]::GetFolderPath('CommonPrograms')) 'FindBugs.lnk'
 
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
-$content = Split-Path $tools
-$pkg_home = Join-Path $content "findbugs-3.0.1"
-$bat     = Join-Path $pkg_home "bin/findbugs.bat"
-
-Uninstall-BinFile -Name "findbugs" -Path $bat
+Remove-Item -Path $shortcut -Force | Out-Null
