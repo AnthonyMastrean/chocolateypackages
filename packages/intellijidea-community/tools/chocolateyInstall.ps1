2b217ff561ab6bb21ed329e2eaf3a28ad7e11892
Jordi Garcia <jordi@improbable.io>
2017-04-18 02:25:25 +0100

    v2017.1.1

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'intellijidea-community'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.jetbrains.com/idea/ideaIC-2017.1.exe'
-$url64       = 'https://download.jetbrains.com/idea/ideaIC-2017.1.exe'
+$url         = 'https://download.jetbrains.com/idea/ideaIC-2017.1.1.exe'
+$url64       = 'https://download.jetbrains.com/idea/ideaIC-2017.1.1.exe'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName   = 'IntelliJ IDEA Community Edition 2017.1*'
 
-  checksum       = 'D764EBFE7ED256FBE2796F11024C85D6B34E2E658A2262D6126A0B005A661EF9'
+  checksum       = '520C8DD9321266FA2BAF9FB052EF803FF6DBCFD2C11E541A84A92602B5B3C2C9'
   checksumType   = 'sha256'
-  checksum64     = 'D764EBFE7ED256FBE2796F11024C85D6B34E2E658A2262D6126A0B005A661EF9'
+  checksum64     = '520C8DD9321266FA2BAF9FB052EF803FF6DBCFD2C11E541A84A92602B5B3C2C9'
   checksumType64 = 'sha256'
 
   silentArgs     = '/S'
--- packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
+++ packages/intellijidea-ultimate/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'intellijidea-ultimate'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.jetbrains.com/idea/ideaIU-2017.1.exe'
-$url64       = 'https://download.jetbrains.com/idea/ideaIU-2017.1.exe'
+$url         = 'https://download.jetbrains.com/idea/ideaIU-2017.1.1.exe'
+$url64       = 'https://download.jetbrains.com/idea/ideaIU-2017.1.1.exe'
 
 $packageArgs = @{
   packageName    = $packageName
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName   = 'IntelliJ IDEA 2017.1*'
 
-  checksum       = '8263080FCDB6F356E8CE31F722B09CD411E1142E7301B2551D1A847653C6B595'
+  checksum       = 'AD031F11D786A7A353140A348AA53170A9C040036E0ABD22D5FB1980443FD1FA'
   checksumType   = 'sha256'
-  checksum64     = '8263080FCDB6F356E8CE31F722B09CD411E1142E7301B2551D1A847653C6B595'
+  checksum64     = 'AD031F11D786A7A353140A348AA53170A9C040036E0ABD22D5FB1980443FD1FA'
   checksumType64 = 'sha256'
 
   silentArgs     = '/S'
