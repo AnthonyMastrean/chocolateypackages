79957fe4f971bf39e81a89528315c622619c7fe9
jarlave <jarlave@users.noreply.github.com>
2017-05-19 11:23:27 +0200

    Checksum changed

--- packages/cheatengine/tools/chocolateyInstall.ps1
+++ packages/cheatengine/tools/chocolateyInstall.ps1
@@ -3,5 +3,5 @@
   -FileType 'EXE' `
   -Silent '/VERYSILENT /NORESTART /NOCANDY' `
   -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/6.6/CheatEngine66.exe' `
-  -Checksum '3F3D4CC77F7192A7E55A8C44CDADE07FCF9D12A2DFE82AAC46CB5F2E12CCCF4E' `
+  -Checksum '2923AA77E59B8455E98B961B2D99405CA8C31137A859FFB43CA4C624BEB201E6' `
   -ChecksumType 'SHA256'
