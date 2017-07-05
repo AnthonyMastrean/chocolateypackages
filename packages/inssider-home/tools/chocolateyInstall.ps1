5258b0e54cb9983910f356b0bab286a92e3d984e
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-07-05 16:18:33 -0400

    inssider-home.3.1.2.1 URLs

--- packages/inssider-home/tools/chocolateyInstall.ps1
+++ packages/inssider-home/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
     -PackageName 'inssider-home' `
     -FileType 'MSI' `
     -SilentArgs '/QN' `
-    -Url 'https://github.com/AnthonyMastrean/chocolateypackages/releases/download/inssider-home.3.1.2.1/inSSIDer-installer.msi' `
+    -Url 'http://files01.techspot.com/temp/inSSIDer-installer.msi' `
     -Checksum '05C0442D092C5AD067AAA2DCBD4119866FE2BEF01B22A5906EF6AE9E592D3D3E' `
     -ChecksumType 'SHA256'
