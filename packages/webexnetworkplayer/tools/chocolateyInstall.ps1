0e614266825fcc155b07d867568db45b103375f8
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-08-07 15:52:52 -0400

    webexnetworkplayer.31.14.3.30

--- packages/webexnetworkplayer/tools/chocolateyInstall.ps1
+++ packages/webexnetworkplayer/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$id = "webexnetworkplayer"
-$url = "https://welcome.webex.com/client/wbxclient/nbr2player.msi"
-$kind = "MSI"
-$silent = "/Q"
-
-Install-ChocolateyPackage $id $kind $silent $url
+﻿Install-ChocolateyPackage `
+    -PackageName 'webexnetworkplayer' `
+    -FileType 'MSI' `
+    -SilentArgs '/QN' `
+    -Url 'https://welcome.webex.com/client/T31L/nbr2player.msi' `
+    -Checksum '2A09286C245C97A3B33C57BF5C8A725E0ECC6697586D1A47A3BDF48FB9137997' `
+    -ChecksumType 'SHA256'
---
+++ packages/webexnetworkplayer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+Uninstall-ChocolateyPackage `
+  -PackageName 'webexnetworkplayer' `
+  -FileType 'MSI' `
+  -Silent '{18F708DC-B680-4D8B-9B09-F1E406FCC54A} /QN'
