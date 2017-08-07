41a295d6d2972fec923ccca8288426cd0de48780
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-08-07 15:48:19 -0400

    webexplayer.31.14.3.30

--- packages/webexplayer/tools/chocolateyInstall.ps1
+++ packages/webexplayer/tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
-﻿$id = "webexplayer"
-$url = "https://welcome.webex.com/client/wbxclient/atrecply.msi"
-$kind = "MSI"
-$silent = "/Q"
-
-Install-ChocolateyPackage $id $kind $silent $url
+﻿Install-ChocolateyPackage `
+    -PackageName 'webexplayer' `
+    -FileType 'MSI' `
+    -SilentArgs '/QN' `
+    -Url 'https://welcome.webex.com/client/T31L/atrecply.msi' `
+    -Checksum 'BB40A036D53DB40F5D9A7C97E7C99BD93D75694431540F084DC99469AE5707E2' `
+    -ChecksumType 'SHA256'
---
+++ packages/webexplayer/tools/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+Uninstall-ChocolateyPackage `
+  -PackageName 'webexplayer' `
+  -FileType 'MSI' `
+  -Silent '{A2E5B82E-9713-4D21-9C75-5B0242ED71A2} /QN'
