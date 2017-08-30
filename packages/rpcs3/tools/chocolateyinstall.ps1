2056be1a26d42b66b976f4fd2f39137877b6b31c
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-08-30 09:33:33 -0400

    rpcs3.0.0.3-build5939

---
+++ packages/rpcs3/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$content = Join-Path (Get-ToolsLocation) 'rpcs3'
+$target = Join-Path $content 'rpcs3.exe'
+$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'RPCS3.lnk'
+
+# This package must be installed in a low-privilege location. I could not get
+# it to run in the default %PROGRAMDATA%\Chocolatey\lib location. I was forced
+# to use the old-style "tools" location.
+Install-ChocolateyZipPackage `
+  -PackageName 'rpcs3' `
+  -UnzipLocation $content `
+  -Url 'https://ci.appveyor.com/api/buildjobs/0ke37r4pvilnqp02/artifacts/rpcs3-v0.0.3-2017-08-27-64a6fbe3_win64.zip' `
+  -Checksum 'A1F6B37335559A59B91CD374B1A9E9DA76CFF48AA10E80E67D2F33B735BF717A' `
+  -ChecksumType 'SHA256' `
+
+Install-ChocolateyShortcut `
+  -ShortcutFilePath $shortcut `
+  -TargetPath $target `
+  -WorkingDirectory $content
+
+New-Item -Path "$target.ignore" -Type 'File' -Force | Out-Null
