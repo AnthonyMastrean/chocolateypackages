6736c66f8e3b0f5fbb51df0b8df865aa7847d1f7
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-09-07 11:08:22 -0400

    colortool.1.0.0

---
+++ packages/colortool/tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'colortool' `
+    -Url 'https://github.com/Microsoft/console/releases/download/1708.14008/colortool.zip' `
+    -Checksum 'EF317660A8CACA83C708B2915274B2B4D611A3AB90C4DE128D21B58461FCDDFA' `
+    -ChecksumType 'SHA256' `
+    -UnzipLocation $content
