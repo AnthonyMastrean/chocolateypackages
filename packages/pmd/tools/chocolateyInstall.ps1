9d915b7f4f7c4816e3b58bebea2c37a57adb3434
Sanshiro <ncolskdslv02r9f2929kfj@tutanota.com>
2017-09-08 22:11:13 +0900

    update to 5.8.1

--- packages/pmd/tools/chocolateyInstall.ps1
+++ packages/pmd/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$id  = "pmd"
-$url = "http://downloads.sourceforge.net/project/pmd/pmd/5.3.2/pmd-bin-5.3.2.zip"
+$url = "https://github.com/pmd/pmd/releases/download/pmd_releases%2F5.8.1/pmd-bin-5.8.1.zip"
 
 $tools    = Split-Path $MyInvocation.MyCommand.Definition
 $content  = Split-Path $tools
@@ -7,7 +7,7 @@ $pmd_home = Join-path $content "pmd-bin-5.3.2"
 $pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
 $cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
 
-Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
+Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content -Checksum 2D854E30717B66E253213F36D4BCD202B83B180EA427836797D13A857C19A6B4 -ChecksumType sha256
 Install-ChocolateyEnvironmentVariable -VariableName "PMD_HOME" -VariableValue $pmd_home -VariableType "Machine"
 
 Install-BinFile -Name "pmd" -Path $pmd_bin
