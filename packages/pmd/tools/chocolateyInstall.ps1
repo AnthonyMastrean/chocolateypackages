6ac6d050f0a88b071fd6913f7de56d80624dd4f6
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-09-12 13:26:59 -0400

    pmd.5.8.1

--- packages/pmd/tools/chocolateyInstall.ps1
+++ packages/pmd/tools/chocolateyInstall.ps1
@@ -1,16 +1,22 @@
-﻿$id  = "pmd"
-$url = "https://github.com/pmd/pmd/releases/download/pmd_releases%2F5.8.1/pmd-bin-5.8.1.zip"
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$pmd_home = Join-path $package 'pmd-bin-5.8.1'
+$pmd_bin = Join-Path $pmd_home 'bin/pmd.bat'
+$cpd_bin = Join-Path $pmd_home 'bin/cpd.bat'
 
-$tools    = Split-Path $MyInvocation.MyCommand.Definition
-$content  = Split-Path $tools
-$pmd_home = Join-path $content "pmd-bin-5.8.1"
-$pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
-$cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
+Install-ChocolateyZipPackage `
+    -PackageName 'pmd' `
+    -Url 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F5.8.1/pmd-bin-5.8.1.zip' `
+    -Checksum '2D854E30717B66E253213F36D4BCD202B83B180EA427836797D13A857C19A6B4' `
+    -ChecksumType 'SHA256'`
+    -UnzipLocation $package `
 
-Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content -Checksum 2D854E30717B66E253213F36D4BCD202B83B180EA427836797D13A857C19A6B4 -ChecksumType sha256
-Install-ChocolateyEnvironmentVariable -VariableName "PMD_HOME" -VariableValue $pmd_home -VariableType "Machine"
+Install-ChocolateyEnvironmentVariable `
+    -VariableName 'PMD_HOME' `
+    -VariableValue $pmd_home `
+    -VariableType 'Machine'
 
-Install-BinFile -Name "pmd" -Path $pmd_bin
-Install-BinFile -Name "cpd" -Path $cpd_bin
+Install-BinFile -Name 'pmd' -Path $pmd_bin
+Install-BinFile -Name 'cpd' -Path $cpd_bin
 
 Update-SessionEnvironment
--- packages/pmd/tools/chocolateyUninstall.ps1
+++ packages/pmd/tools/chocolateyUninstall.ps1
@@ -1,10 +1,8 @@
-﻿$id  = "pmd"
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Split-Path $tools
+$pmd_home = Join-path $package 'pmd-bin-5.8.1'
+$pmd_bin = Join-Path $pmd_home 'bin/pmd.bat'
+$cpd_bin = Join-Path $pmd_home 'bin/cpd.bat'
 
-$tools    = Split-Path $MyInvocation.MyCommand.Definition
-$content  = Split-Path $tools
-$pmd_home = Join-Path $content "pmd-bin-5.8.1"
-$pmd_bin  = Join-Path $pmd_home "bin/pmd.bat"
-$cpd_bin  = Join-Path $pmd_home "bin/cpd.bat"
-
-Uninstall-BinFile -Name "pmd" -Path $pmd_bin
-Uninstall-BinFile -Name "cpd" -Path $cpd_bin
+Uninstall-BinFile -Name 'pmd' -Path $pmd_bin
+Uninstall-BinFile -Name 'cpd' -Path $cpd_bin
