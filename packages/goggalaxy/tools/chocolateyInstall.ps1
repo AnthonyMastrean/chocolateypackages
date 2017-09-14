ba0dc7e39663ba55f3c2c0bfa1a7a6e836a3e1f5
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-09-14 16:28:56 -0400

    goggalaxy.1.2.23.4

--- packages/goggalaxy/tools/chocolateyInstall.ps1
+++ packages/goggalaxy/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'goggalaxy' `
-    -Url 'https://cdn.gog.com/open/galaxy/client/setup_galaxy_1.1.30.5.exe' `
-    -Checksum '2422D2FF3AC93CE04824D7E6DF988E8DB2407DEADCEC698AB627C854E0F2EA08' `
+    -Url 'https://cdn.gog.com/open/galaxy/client/setup_galaxy_1.2.23.4.exe' `
+    -Checksum '4D5BC6B515DA36C7B4E8A7F35F355C9D79EA2AF06A2043FC3445EA74784B063F' `
     -ChecksumType 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/VERYSILENT /NORESTART'
