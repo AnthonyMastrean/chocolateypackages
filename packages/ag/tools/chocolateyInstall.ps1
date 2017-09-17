88ce09f5971b525f7175d70caa91cba8775093a4
i-give-up <freemium.model@gmail.com>
2017-09-17 13:02:27 +0800

    Use k-takata's Windows build of ag and update package information
    Solves #250. Switch from Krzysztof Kowalczyk's to k-takata's up-to-date
    builds.
    
    Other changes
    
    - Add `foss` tag to ag.nuspec
    - Add packageSourceUrl to ag.nuspec
    - Format arguments as a hashtable and pass that to
      `Install-ChocolateyZipPackage`.

--- packages/ag/tools/chocolateyInstall.ps1
+++ packages/ag/tools/chocolateyInstall.ps1
@@ -1,7 +1,14 @@
-﻿$id  = "ag"
-$url = "https://kjkpub.s3.amazonaws.com/software/the_silver_searcher/rel/0.29.1-1641/ag.zip"
+﻿$id    = "ag"
+$url   = "https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x86.zip"
+$url64 = "https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x64.zip"
 
-$tools   = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) "content"
+$toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
 
-Install-ChocolateyZipPackage -PackageName $id -Url $url -UnzipLocation $content
+$packageArgs = @{
+    packageName   = $id
+    url           = $url
+    url64         = $url64
+    unzipLocation = $toolsPath
+}
+
+Install-ChocolateyZipPackage @packageArgs
