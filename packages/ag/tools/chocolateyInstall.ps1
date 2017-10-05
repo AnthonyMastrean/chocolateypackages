ad301e7ccba89699e6004fe70d936d28b75f791b
i-give-up <freemium.model@gmail.com>
2017-10-06 00:52:46 +0800

    Make changes based on PR #253 comments
    Make changes as requested in #253, i.e. add SHA256 checksums, fix/add
    some urls, change wording, and make `Install-ChocolateyZipPackage`
    arguments inline.
    
    Additionally changed the wording `$toolsPath` to `$toolsDir` since the
    latter seems to be the convention. Also added `$ErrorActionPreference`
    which seems to be the convention.

--- packages/ag/tools/chocolateyInstall.ps1
+++ packages/ag/tools/chocolateyInstall.ps1
@@ -1,14 +1,20 @@
-﻿$id    = "ag"
-$url   = "https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x86.zip"
-$url64 = "https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x64.zip"
+﻿$url        = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x86.zip'
+$checksum   = '28456A424A30B12BE3BE2B9D427BC67031BC436F87F1100607CFA9A44C1AF95D'
+$url64      = 'https://github.com/k-takata/the_silver_searcher-win32/releases/download/2017-08-31%2F2.1.0-1/ag-2017-08-31_2.1.0-1-x64.zip'
+$checksum64 = '2F0B83E705D6224DF82BE92014726875BEC925C6B56EF84170D1F19EDDFAA439'
 
-$toolsPath  = Split-Path $MyInvocation.MyCommand.Definition
+$id                    = 'ag'
+$checksumType          = 'sha256'
+$checksumType64        = 'sha256'
+$ErrorActionPreference = 'Stop';
+$toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
 
-$packageArgs = @{
-    packageName   = $id
-    url           = $url
-    url64         = $url64
-    unzipLocation = $toolsPath
-}
-
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyZipPackage `
+    -PackageName $id `
+    -UnzipLocation $toolsDir `
+    -Url $url `
+    -Url64 $url64 `
+    -Checksum $checksum `
+    -ChecksumType $checksumType `
+    -Checksum64 $checksum64 `
+    -ChecksumType64 $checksumType64 `
