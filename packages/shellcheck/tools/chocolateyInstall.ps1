8ddc68d99d20911a111a34583881baff9cbeb760
Anthony Mastrean <anthony.mastrean@gmail.com>
2017-11-13 09:31:16 -0500

    shellcheck.0.4.6 fix URL and uninstall

--- packages/shellcheck/tools/chocolateyInstall.ps1
+++ packages/shellcheck/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
-$original = Join-Path $content 'shellcheck-latest.exe'
+$original = Join-Path $content 'shellcheck-v0.4.6.exe'
 $target = Join-Path $content 'shellcheck.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'shellcheck' `
-    -Url 'https://shellcheck.storage.googleapis.com/shellcheck-latest.zip' `
-    -Checksum 'c22305929dfa6a5b337341ec07c245d26a63f7c79533f2e86d217c1f7821793677cec2e28029b6082707781d817f6ee7a713ecf3c633d93ac691c0be528f80a7' `
+    -Url 'https://shellcheck.storage.googleapis.com/shellcheck-v0.4.6.zip' `
+    -Checksum 'e806c117a99a32a7789d3972ca146f9b8915ef97e6b01bbfcd230af4fb8aa9459631bc11e7085d9476da790a471d4fe103465f89385853c3cb815ff6dfc95f46' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $content
 
--- packages/shellcheck/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-﻿$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
-$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'ADOM (Classic).lnk'
-
-Remove-Item -Path $shortcut -Force | Out-Null
