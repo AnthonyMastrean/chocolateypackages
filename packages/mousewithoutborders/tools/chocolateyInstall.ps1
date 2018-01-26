733da246fcb39a60051eb71e9523211b7655a8a3
Bo Osterud <git@conjuro.net>
2018-01-26 19:53:11 +0100

    [mousewithoutborders] 2.1.8.0105

--- packages/mousewithoutborders/tools/chocolateyInstall.ps1
+++ packages/mousewithoutborders/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
   -PackageName 'mousewithoutborders' `
   -Url 'https://download.microsoft.com/download/6/5/8/658AFC4C-DC02-4CB8-839D-10253E89FFF7/MouseWithoutBordersSetup.msi' `
-  -Checksum '53626A4793BF679B492FD9E16F1337D07B0D95121C4D82A553D320C56DF54FE3' `
+  -Checksum 'c73d373275519de5545824ff20e886e4c2d76770cb77f8b685c0b52a1c07e97d' `
   -ChecksumType 'SHA256' `
   -FileType 'MSI' `
   -SilentArgs '/QN'
