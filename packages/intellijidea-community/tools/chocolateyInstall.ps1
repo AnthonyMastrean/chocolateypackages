04981cd78d64ae88b0ca29a595bdfd7015404dea
Zed <dev@zedcn.com>
2018-02-01 13:03:05 +0800

    intellijidea: update to 2017.3.4

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -2,21 +2,21 @@
 
 $packageName = 'intellijidea-community'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.jetbrains.com/idea/ideaIC-2017.3.3.exe'
-$url64       = 'https://download.jetbrains.com/idea/ideaIC-2017.3.3.exe'
+$url         = 'https://download.jetbrains.com/idea/ideaIC-2017.3.4.exe'
+$sha256sum   = '9d13fb36f6a2cd83097cf1093bf472a7dbac2894add29e035b46469110846467'
 
 $packageArgs = @{
   packageName    = $packageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
   url            = $url
-  url64bit       = $url64
+  url64bit       = $url
 
   softwareName   = 'IntelliJ IDEA Community Edition 2017.3*'
 
-  checksum       = '2083bf6be659b50e7f7672193c659bb83efa8011536e915c5159e6255ce5f9b6'
+  checksum       = $sha256sum
   checksumType   = 'sha256'
-  checksum64     = '2083bf6be659b50e7f7672193c659bb83efa8011536e915c5159e6255ce5f9b6'
+  checksum64     = $sha256sum
   checksumType64 = 'sha256'
 
   silentArgs     = '/S'
