eadb864dea0712ae88c06f0d46d19c75a0c63b10
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-02-08 10:33:34 -0500

    ultramon.3.4.0

--- packages/ultramon/tools/chocolateyInstall.ps1
+++ packages/ultramon/tools/chocolateyInstall.ps1
@@ -1,7 +1,10 @@
-﻿$id     = "ultramon"
-$kind   = "MSI"
-$silent = "/Q /NORESTART"
-$url    = "http://www.realtimesoft.com/files/UltraMon_3.3.0_en_x32.msi"
-$url64  = "http://www.realtimesoft.com/files/UltraMon_3.3.0_en_x64.msi"
-
-Install-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -Url $url -Url64 $url64
+﻿Install-ChocolateyPackage `
+    -PackageName 'ultramon' `
+    -FileType 'MSI' `
+    -Silent '/Q /NORESTART' `
+    -Url 'http://www.realtimesoft.com/files/UltraMon_3.4.0_en_x32.msi' `
+    -Checksum 'E88EDEF08C1785CF1A1C5ADFD4E83589A5EFE5E04E6472D41E1B2BD953367C2E' `
+    -ChecksumType 'SHA256' `
+    -Url64 'http://www.realtimesoft.com/files/UltraMon_3.4.0_en_x64.msi' `
+    -Checksum64 '6A567BFC642ABDBAC0E9C236F793D0A6381BF926E5784637039FFF06167F6661' `
+    -ChecksumType64 'SHA256'
--- packages/ultramon/tools/chocolateyUninstall.ps1
+++ packages/ultramon/tools/chocolateyUninstall.ps1
@@ -1,5 +1,4 @@
-﻿$id     = "ultramon"
-$kind   = "MSI"
-$silent = "{D4E62D29-31A1-4938-8CB7-7D275C1AEAC6} /Q /NORESTART"
-
-Uninstall-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent
+﻿Uninstall-ChocolateyPackage `
+    -PackageName 'ultramon' `
+    -FileType 'MSI' `
+    -Silent '{D4E62D29-31A1-4938-8CB7-7D275C1AEAC6} /Q /NORESTART'
