07ed6566cf06395cb25cd6b78f28ea93a34ecf04
Zed <dev@zedcn.com>
2018-04-11 08:53:13 +0800

    intellij: update to 2018.1.1

--- packages/intellijidea-community/tools/chocolateyInstall.ps1
+++ packages/intellijidea-community/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'intellijidea-community'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = 'https://download.jetbrains.com/idea/ideaIC-2018.1.exe'
-$sha256sum   = '1fb9f6c7f54922df06e2a248e0d383d40564b508bca9b8426720878c7a7eae5e'
+$url         = 'https://download.jetbrains.com/idea/ideaIC-2018.1.1.exe'
+$sha256sum   = '5f5dfe3a6f0e5d34674d1e0449c0b09ae7574b14a3b7419b9d7812831feed01e'
 
 $packageArgs = @{
   packageName    = $packageName
