e24e035f15cdb96b265bf0d161175105289aefb4
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-05-11 16:11:29 -0400

    op 0.4.1

--- packages/op/tools/chocolateyInstall.ps1
+++ packages/op/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $content = Join-Path (Split-Path $tools) 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'op' `
-    -Url 'https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_windows_386_v0.1.1.zip' `
-    -Checksum '49F4590D9344B938BD3519A9471591B8DEE1DBE6EEA83864969CFF7C0F1E9D43' `
+    -Url 'https://cache.agilebits.com/dist/1P/op/pkg/v0.4.1/op_windows_386_v0.4.1.zip' `
+    -Checksum '88C8D5B0E8149A6C204AECD31A4572BF69F55A5BDA9F9D36F377B017B3F3258D' `
     -ChecksumType 'SHA256' `
-    -Url64 'https://cache.agilebits.com/dist/1P/op/pkg/v0.1.1/op_windows_amd64_v0.1.1.zip' `
-    -Checksum64 '63F5026FB9FE2431050C7EF9D3AF178B8108D70D995DC0732A4184160B1DF946' `
+    -Url64 'https://cache.agilebits.com/dist/1P/op/pkg/v0.4.1/op_windows_amd64_v0.4.1.zip' `
+    -Checksum64 '3A04B24EB5CAB7EF403B7E6283898E3CBAEC23AA1EEE78935E680725AAB08393' `
     -ChecksumType64 'SHA256' `
     -UnzipLocation $content
