30fd0cdf2e74d50f75b3810fcecce9d7551d586f
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-05-14 16:24:23 -0400

    isorecorder 3.0.0

--- packages/isorecorder/tools/chocolateyInstall.ps1
+++ packages/isorecorder/tools/chocolateyInstall.ps1
@@ -1,10 +1,19 @@
-﻿Install-ChocolateyPackage `
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$target = Join-Path $content 'ISORecorderV3.msi'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'isorecorder' `
+    -Url 'http://isorecorder.alexfeinman.com/static/ISORecorderV3RC1x86.zip' `
+    -Checksum 'D4C5D259FB334E56ACF5BF04753F6904C1A782BFF17D2C2CAC50E503B5E61DC0' `
+    -ChecksumType 'SHA256' `
+    -Url64 'http://isorecorder.alexfeinman.com/static/ISORecorderV3RC1x64.zip' `
+    -Checksum64 '20BCE41719774FCE7082819950B468DE068F41F88D1CC099C23BB012E92E42CF' `
+    -ChecksumType64 'SHA256' `
+    -UnzipLocation $content
+
+Install-ChocolateyInstallPackage `
     -PackageName 'isorecorder' `
     -FileType 'MSI' `
     -SilentArgs '/QN' `
-    -Url 'http://alexfeinman.com/download/IsoRecorder/ISORecorder3.1.3.x86.msi' `
-    -Checksum '79BFBBD0D4ACB29776E91EAFB22B1143B4D5F9975FBF0C13A84FA5ED13826408' `
-    -ChecksumType 'SHA256' `
-    -Url64 'http://alexfeinman.com/download/IsoRecorder/ISORecorder3.1.3.x64.msi' `
-    -Checksum64 '1DB4C5DECD077357F996017A6D69062045CF0FAFDE5A282EF0581F106084E4B0' `
-    -ChecksumType64 'SHA256'
+    -File $target
--- packages/isorecorder/tools/chocolateyUninstall.ps1
+++ packages/isorecorder/tools/chocolateyUninstall.ps1
@@ -1,5 +1,5 @@
-$productcode32 = '{1235083F-52F9-44CC-9DF5-F9B7802BB9B7}'
-$productcode64 = '{2D7ED2A0-9553-412B-939F-D6E0AEB2ABE1}'
+$productcode32 = '{39600969-41C3-4658-876E-16F108FC5C92}'
+$productcode64 = '{39600969-41C3-4658-876E-16F108FC5C92}'
 $productcode = @{32=$productcode32;64=$productcode64}[(Get-ProcessorBits)]
 
 Uninstall-ChocolateyPackage `
