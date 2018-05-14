1026d8f4c6505e7df39802467b4a4f239e280581
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-05-14 16:37:52 -0400

    isowriter 0.6.1.20180514

--- packages/isowriter/tools/chocolateyInstall.ps1
+++ packages/isowriter/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $content = Join-Path (Split-Path $tools) 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'isowriter' `
-    -Url 'http://alexfeinman.com/download/ISORecorder/ISOWriter.0.6.1.0.zip' `
+    -Url 'http://isorecorder.alexfeinman.com/static/ISOWriter.0.6.1.0.zip' `
     -Checksum '1983E9144BFF25571B9D4897F82406AC68B2A016B3458D4D7210DE014F18DA2B' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
