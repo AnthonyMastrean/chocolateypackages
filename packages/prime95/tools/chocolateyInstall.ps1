bca0768bf5d29b105c4570acef27d71716b3ef96
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-05-16 09:53:58 -0400

    prime95 29.4

--- packages/prime95/tools/chocolateyInstall.ps1
+++ packages/prime95/tools/chocolateyInstall.ps1
@@ -7,11 +7,11 @@ $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'prime
 
 Install-ChocolateyZipPackage `
     -PackageName 'prime95' `
-    -Url 'http://www.mersenne.org/ftp_root/gimps/p95v2810.win32.zip' `
-    -Url64 'http://www.mersenne.org/ftp_root/gimps/p95v2810.win64.zip' `
-    -Checksum 'CFC3E7C172AE321C8F99D6DE41CB019DFF9BF3A94FADF6B8F888FE3922FDDBE8' `
-    -Checksum64 '25D7C9C8BA632410B964AF14106FCD017F8B10D48C37F81671C2D6FC951D8A57' `
+    -Url 'http://www.mersenne.org/ftp_root/gimps/p95v294b7.win32.zip' `
+    -Checksum '896A2F90D198AA4A6D1FFC912FCE10F30A0BBC127F711BE64871E2887A48B283' `
     -ChecksumType 'SHA256' `
+    -Url64 'http://www.mersenne.org/ftp_root/gimps/p95v294b8.win64.zip' `
+    -Checksum64 '00C24E7D131529A08D0C0C268CC8DC1349F69C3B46D04B7FCAA391B373B184D6' `
     -ChecksumType64 'SHA256' `
     -UnzipLocation $content
 
