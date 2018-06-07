310b53d110026af6af500b8826769cde03a65108
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-06-07 17:11:42 -0400

    smallpdf

---
+++ packages/smallpdf/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿Install-ChocolateyPackage `
+    -packageName 'smallpdf' `
+    -fileType 'EXE' `
+    -url 'https://download.smallpdf.com/desktop/win32/Smallpdf-Installer.exe' `
+    -checksum 'BEBBEC4019AFE9A8FBB1C83F716AD02DA0468C6A9D96914A819C6FE4BF1DCD1D' `
+    -checksumType 'sha256' `
+    -url64bit 'https://download.smallpdf.com/desktop/win/Smallpdf-Installer.exe' `
+    -checksum64 '79C7FCED71CB5B9790D4EE113346FB8813E8B07BC0E57B1E55B088F39C05B22C' `
+    -checksumType64 'sha256' `
+    -silentArgs '--silent'
---
+++ packages/smallpdf/tools/chocolateyuninstall.ps1
@@ -0,0 +1,5 @@
+﻿Uninstall-ChocolateyPackage `
+    -packageName 'smallpdf' `
+    -fileType 'EXE' `
+    -silentArgs '--uninstall -s' `
+    -file ((Get-UninstallRegistryKey -SoftwareName 'Smallpdf').UninstallString -split '"')[1]
