f235d6d85de2c1299aa12c24577ad278a836fa8c
Dave Barnum <dave_barnum@wycliffe.org>
2018-08-24 08:47:05 -0400

    Release version 7 of sqljdbc

--- packages/sqljdbc/tools/chocolateyUninstall.ps1
+++ packages/sqljdbc/tools/chocolateyUninstall.ps1
@@ -1,3 +1,3 @@
-$target = Join-Path $ENV:PROGRAMFILES 'Microsoft JDBC DRIVER 6.0 for SQL Server'
+$target = Join-Path $ENV:PROGRAMFILES 'Microsoft JDBC DRIVER 7.0 for SQL Server'
 
 Remove-Item -Force -Recurse -Path $target | Out-Null
