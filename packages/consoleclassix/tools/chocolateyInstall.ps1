ef3efcd4ca871e89f7a42f0ad7148cd43f4b722b
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-09-03 21:48:02 -0400

    consoleclassix.4.30.0 fixes #294

---
+++ packages/consoleclassix/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿Install-ChocolateyPackage `
+    -PackageName 'consoleclassix' `
+    -FileType 'EXE' `
+    -SilentArgs '/VERYSILENT' `
+    -Url 'http://www.consoleclassix.com/clients/consoleclassixsetup.exe' `
+    -Checksum '1A98D682066F5AA3D349A31B55E31F748A472E56BD6BA80C173829E2B9FA3B3F' `
+    -ChecksumType 'SHA256'
---
+++ packages/consoleclassix/tools/chocolateyUninstall.ps1
@@ -0,0 +1,5 @@
+﻿Uninstall-ChocolateyPackage `
+  -PackageName 'consoleclassix' `
+  -FileType 'EXE' `
+  -Silent '/VERYSILENT' `
+  -File (Get-UninstallRegistryKey -SoftwareName 'Console Classix version 4.30').UninstallString
