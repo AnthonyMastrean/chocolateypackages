5be283ffc0a4752c0796fa8eddb2db608e9e5537
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-12-12 12:22:47 -0500

    stigviewer.2.8

---
+++ packages/stigviewer/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$tools = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$content = Join-Path -Path (Split-Path -Parent $tools) -ChildPath 'content'
+$target = Join-Path -Path $content -ChildPath "STIGViewer-2.8.jar"
+
+$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'STIG Viewer.lnk'
+
+Install-ChocolateyZipPackage `
+    -PackageName $env:ChocolateyPackageName `
+    -UnzipLocation $content `
+    -Url 'http://iasecontent.disa.mil/stigs/zip/U_STIGViewer_2-8.zip' `
+    -Checksum '66D4A6EFC863774929D84F4FAAF2F3484BA57607ECFFAA39731FF6547A117B7E' `
+    -ChecksumType 'SHA256'
+
+Install-ChocolateyShortcut `
+    -ShortcutFilePath $shortcut `
+    -TargetPath $target
