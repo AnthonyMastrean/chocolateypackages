f948764827782b0178a895759b8526694aef4840
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-12-24 12:49:55 -0500

    ultramon.3.4.1

--- packages/ultramon/tools/chocolateyInstall.ps1
+++ packages/ultramon/tools/chocolateyInstall.ps1
@@ -2,9 +2,9 @@
     -PackageName 'ultramon' `
     -FileType 'MSI' `
     -Silent '/Q /NORESTART' `
-    -Url 'http://www.realtimesoft.com/files/UltraMon_3.4.0_en_x32.msi' `
-    -Checksum 'E88EDEF08C1785CF1A1C5ADFD4E83589A5EFE5E04E6472D41E1B2BD953367C2E' `
+    -Url 'http://www.realtimesoft.com/files/UltraMon_3.4.1_en_x32.msi' `
+    -Checksum 'A5FFB1632626008BDFF8E5C8D6CD94677878272257DCC22D33980C982CC8863F' `
     -ChecksumType 'SHA256' `
-    -Url64 'http://www.realtimesoft.com/files/UltraMon_3.4.0_en_x64.msi' `
-    -Checksum64 '6A567BFC642ABDBAC0E9C236F793D0A6381BF926E5784637039FFF06167F6661' `
+    -Url64 'http://www.realtimesoft.com/files/UltraMon_3.4.1_en_x64.msi' `
+    -Checksum64 'C950C739883802137B650C4BE9EDA050AFFA9BDD08F6D138A0D58B698D105A17' `
     -ChecksumType64 'SHA256'
