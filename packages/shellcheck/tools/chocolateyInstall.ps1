dfcca36972e4b501edfca9a4e36eb4f3512a2923
Anthony Mastrean <anthony.mastrean@gmail.com>
2018-12-24 12:35:04 -0500

    shellcheck.0.6.0

--- packages/shellcheck/tools/chocolateyInstall.ps1
+++ packages/shellcheck/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
-$original = Join-Path $content 'shellcheck-v0.5.0.exe'
+$original = Join-Path $content 'shellcheck-v0.6.0.exe'
 $target = Join-Path $content 'shellcheck.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'shellcheck' `
-    -Url 'https://shellcheck.storage.googleapis.com/shellcheck-v0.5.0.zip' `
-    -Checksum '5e1cd46b052f5669a021cd308daacf1221fd314964ceb96fd494c806e7620eba86bc32dcf5e3b9959904b374cce809b90e43fbc4df7e0f6289adf42ea1956465' `
+    -Url 'https://shellcheck.storage.googleapis.com/shellcheck-v0.6.0.zip' `
+    -Checksum '2d8171e79cafeeaefd1dc3be30cf44f8c3f2df0f18f6b54d4a028ab26c5159e07c9a1f0bc67603d1ae52bc2ba8d337df596079fec28bf1258255956bb552ce53' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $content
 
