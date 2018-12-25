651862dc3dc672b1fc286e047a6fc27fd1384de0
adriens <adrien.sales@gmail.com>
2018-12-25 11:00:07 +1100

    Implementing 3.6.2 release see https://github.com/AnthonyMastrean/chocolateypackages/issues/305

--- packages/liquibase/tools/chocolateyInstall.ps1
+++ packages/liquibase/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+﻿ï»¿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) 'content'
 $bat = Join-Path $content 'liquibase.bat'
 
 Install-ChocolateyZipPackage `
   -PackageName 'liquibase' `
-  -Url 'https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.1/liquibase-3.6.1-bin.zip' `
-  -Checksum 'EF9A08D10979A1149386AFEEA6E7B1539EB70C6D97B5BE28B57494057A6F2633' `
+  -Url 'https://github.com/liquibase/liquibase/releases/download/liquibase-parent-3.6.2/liquibase-3.6.2-bin.zip' `
+  -Checksum '3BE254587334FEB99D1FC434CA70C13D264BC9BB03596C028F7D222B40278555' `
   -ChecksumType 'SHA256' `
   -UnzipLocation $content
 
