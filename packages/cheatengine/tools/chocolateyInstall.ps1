c2ce689541f53395568c2afeda8952b6f509b1f5
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-02-04 14:19:01 -0500

    cheatengine.6.8.2

--- packages/cheatengine/tools/chocolateyInstall.ps1
+++ packages/cheatengine/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyPackage `
   -PackageName 'cheatengine' `
-  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/v6.8.1/CheatEngine681.exe' `
-  -Checksum '6F795F11FA3983B4779426DD6DCE8346E279B099F62AD4BAB5D246B932E09885' `
+  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/6.8.2/CheatEngine682.exe' `
+  -Checksum '61A35039E8DA357E9D6F06642F417DC897ED9A077F6A99E84B46ADA1A211D949' `
   -ChecksumType 'SHA256' `
   -FileType 'EXE' `
   -Silent '/VERYSILENT /NORESTART /NOCANDY'
