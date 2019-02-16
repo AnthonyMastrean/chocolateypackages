ba1a10ea2999c430bdc621587ac7b08299bbe0e6
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-02-16 12:40:51 -0500

    liquibase moved to adriens repo

--- packages/liquibase/tools/chocolateyUninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
-$bat = Join-Path $content 'liquibase.bat'
-
-Uninstall-BinFile `
-  -Name 'liquibase' `
-  -Path $bat
