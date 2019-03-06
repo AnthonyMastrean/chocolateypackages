2ad8e70fb446c04c46f4155dedc190ccf1d45654
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-03-06 16:40:06 -0500

    ghidra.9.0

---
+++ packages/ghidra/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$tools = Split-Path -Path $MyInvocation.MyCommand.Definition
+$package = Split-Path -Path $tools
+$workdir = Join-Path -Path $package -ChildPath 'ghidra_9.0'
+$target = Join-Path -Path $workdir -ChildPath 'ghidraRun.bat'
+$icon = Join-Path -Path $workdir -ChildPath 'support/ghidra.ico'
+
+$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Ghidra.lnk'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'ghidra' `
+    -Url 'https://www.ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip' `
+    -Checksum '3b65d29024b9decdbb1148b12fe87bcb7f3a6a56ff38475f5dc9dd1cfc7fd6b2' `
+    -ChecksumType 'SHA256' `
+    -UnzipLocation $package
+
+Install-ChocolateyShortcut `
+    -IconLocation $icon `
+    -ShortcutFilePath $shortcut `
+    -TargetPath $target
+
+Get-ChildItem -Include '*.exe' -Path $workdir -Recurse `
+    | %{ New-Item -Path "$_.ignore" -Type File | Out-Null }
