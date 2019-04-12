0d86fd5b36901d48b2ca45b60e5b340b471aeddc
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-12 15:58:16 -0400

    kindle.1.25.52064

--- packages/kindle/tools/chocolateyInstall.ps1
+++ packages/kindle/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
   -PackageName 'kindle' `
   -InstallerType 'EXE' `
   -SilentArgs '/S' `
-  -Url 'https://s3.amazonaws.com/kindleforpc/51068/KindleForPC-installer-1.24.51068.exe' `
-  -Checksum 'C7A1A93763D102BCA0FED9C16799789AE18C3322B1B3BDFBE8C00422C32F83D7' `
+  -Url 'https://s3.amazonaws.com/kindleforpc/52064/KindleForPC-installer-1.25.52064.exe' `
+  -Checksum '314678A0A3B867BF412936ADAA67C6AB6D41C961F359A46F99C3AFA591702EF7' `
   -ChecksumType 'SHA256'
