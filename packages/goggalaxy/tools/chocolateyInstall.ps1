f1a18e7abefad9e64220273a110e9f0064a8d60e
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-12 14:34:22 -0400

    goggalaxy is maintained in another repo now

--- packages/goggalaxy/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Install-ChocolateyPackage `
-    -PackageName 'goggalaxy' `
-    -Url 'https://content-system.gog.com/open_link/download?path=/open/galaxy/client/setup_galaxy_1.2.46.172.exe' `
-    -Checksum 'EDAE0A99386A56DADD858D08B97D6B2211F3361E498793BA44D573403BD1AFD0' `
-    -ChecksumType 'SHA256' `
-    -FileType 'EXE' `
-    -SilentArgs '/VERYSILENT /NORESTART'
--- packages/goggalaxy/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿Uninstall-ChocolateyPackage `
-  -PackageName 'goggalaxy' `
-  -FileType 'EXE' `
-  -Silent '/VERYSILENT /NORESTART' `
-  -File (Get-UninstallRegistryKey -SoftwareName 'GOG Galaxy').UninstallString
