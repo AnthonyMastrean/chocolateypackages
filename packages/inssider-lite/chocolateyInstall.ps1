c5730dadf058204ad1073a0d4b627c0b116552e8
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-12 15:04:42 -0400

    inssider-lite.1.6.5

---
+++ packages/inssider-lite/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿Install-ChocolateyPackage `
+    -PackageName 'inssider-lite' `
+    -FileType 'EXE' `
+    -SilentArgs '/VERYSILENT' `
+    -Url 'https://metageek.link/inssider-update-url-v1-0-2/inSSIDerSetup.exe' `
+    -Checksum '1262774A0D3E983719C851645CA798FED4830C367A267E26257BFC9F5AAA4C5E' `
+    -ChecksumType 'SHA256'
---
+++ packages/inssider-lite/chocolateyUninstall.ps1
@@ -0,0 +1,4 @@
+﻿Uninstall-ChocolateyPackage `
+  -PackageName 'inssider-office' `
+  -FileType 'MSI' `
+  -Silent '{D8F64983-6F31-4F07-86C5-EA1C7270DADD} /QN'
