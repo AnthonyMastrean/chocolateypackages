86da114d220f584f699b0e2661d9ef687054f51b
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-12 15:53:12 -0400

    joytokey.6.2.0

--- packages/joytokey/tools/chocolateyInstall.ps1
+++ packages/joytokey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$content = Join-Path (Get-ToolsLocation) 'joytokey'
-$target = Join-Path $content 'JoyToKey_en\JoyToKey.exe'
+$target = Join-Path $content 'JoyToKey.exe'
 
 $shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
 $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Joy to Key.lnk'
@@ -7,8 +7,8 @@ $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Joy t
 # Joy to Key writes to its installation directory, so it can't be in %PROGRAMDATA%
 Install-ChocolateyZipPackage `
     -PackageName 'joytokey' `
-    -Url 'http://joytokey.net/download/JoyToKey_en.zip' `
-    -Checksum 'C2CCA97814225D22A0B144B4AC2CCAD8B9CE6F2ED71EB25D643ED6B89C846F4B' `
+    -Url 'https://joytokey.net/download/JoyToKey_en.zip' `
+    -Checksum '38A3DC7829AD6A9DEEFCF163D23E31F2056A75EC18B56DE6D27DE6D198548BDD' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
