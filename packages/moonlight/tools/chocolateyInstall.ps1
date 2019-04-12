5c553efcde7ef999505efc1675e42499f7095f66
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-12 17:01:22 -0400

    moonlight.0.10.0

--- packages/moonlight/tools/chocolateyInstall.ps1
+++ packages/moonlight/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿Install-ChocolateyPackage `
     -PackageName 'moonlight' `
-    -Url 'https://github.com/moonlight-stream/moonlight-qt/releases/download/v0.8.0/MoonlightSetup-x86-0.8.0.exe' `
-    -Checksum 'CC9CC50AFDB795B038251E0DA850D9DBAC6704DABA6A6325BFBAE43634DE339B' `
+    -Url 'https://github.com/moonlight-stream/moonlight-qt/releases/download/v0.10.0/MoonlightSetup-x86-0.10.0.exe' `
+    -Checksum 'CC0F48FEE4212418DDB2EBA2886AF69A9FEF5110B42F1B79EAAFF421A7DA6127' `
     -ChecksumType 'SHA256' `
-    -Url64 'https://github.com/moonlight-stream/moonlight-qt/releases/download/v0.8.0/MoonlightSetup-x64-0.8.0.exe' `
-    -Checksum64 '4101003961F4FF11C333628450881B2DC127634C1776FE9145519C38832B1A20' `
+    -Url64 'https://github.com/moonlight-stream/moonlight-qt/releases/download/v0.10.0/MoonlightSetup-x64-0.10.0.exe' `
+    -Checksum64 'E19DD0C5150C1BA5C66A90FB630DBE8FCD81806BBD2A3FF486AA4FA84A74A8DE' `
     -ChecksumType64 'SHA256' `
     -FileType 'EXE' `
     -SilentArgs '/QUIET /NORESTART'
