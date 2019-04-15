ebb3fb54dfbbbc7f696b0c1a4ef25613c74194cb
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-15 13:46:20 -0400

    Remove apm

--- packages/apm/tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-$shortcutdir = @{$true='CommonStartup';$false='Startup'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
-$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Advanced Process Manipulation.lnk'
-
-Remove-Item -Path $shortcut -Force | Out-Null
