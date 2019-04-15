329463e148408e5364f21c731c7fdfabfb3e4c5f
Anthony Mastrean <anthony.mastrean@vaeit.com>
2019-04-15 13:54:12 -0400

    Remove visioviewer2010 and 2013

--- packages/visioviewer2010/tools/chocolateyInstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$id     = "visioviewer2010"
-$url    = "http://download.microsoft.com/download/A/B/3/AB3C56B5-B1B3-41CB-A445-D4FB03F8A1BA/visioviewer.exe"
-$kind   = "EXE"
-$silent = "/quiet /passive /norestart"
-
-Install-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -Url $url
--- packages/visioviewer2013/tools/chocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-﻿$id     = "visioviewer2013"
-$url    = "http://download.microsoft.com/download/A/6/8/A682A83B-4866-4F88-AE83-61388629A891/visioviewer32bit.exe"
-$url64  = "http://download.microsoft.com/download/A/6/8/A682A83B-4866-4F88-AE83-61388629A891/visioviewer64bit.exe"
-$kind   = "EXE"
-$silent = "/quiet /passive /norestart"
-
-Install-ChocolateyPackage -PackageName $id -FileType $kind -Silent $silent -Url $url -Url64 $url64
-
