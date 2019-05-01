8a8f297205919e2a218116689164fd284ff990dc
Anthony Mastrean <anthony.mastrean@gmail.com>
2019-05-01 11:17:27 -0400

    dotcover-cli.2019.1

--- packages/dotcover-cli/tools/chocolateyInstall.ps1
+++ packages/dotcover-cli/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $target = Join-Path -Path $content -ChildPath 'dotCover.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'dotcover-cli' `
-    -Url 'https://download.jetbrains.com/resharper/ReSharperUltimate.2018.3.4/JetBrains.dotCover.CommandLineTools.2018.3.4.zip' `
-    -Checksum 'a0fb72a6aa70202a8720932c0eb0719fcded488664733184b3ca3d145160b44a' `
+    -Url 'https://download.jetbrains.com/resharper/ReSharperUltimate.2019.1/JetBrains.dotCover.CommandLineTools.2019.1.zip' `
+    -Checksum 'baaba77ff70041b6ff2c2033ee8ef3e26661f76faaf4646c4c9317ce0525133a' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
