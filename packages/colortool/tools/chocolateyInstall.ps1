fcbe15ef0175581181ef6dcfca73c44e931dd515
Anthony Mastrean <anthony.mastrean@gmail.com>
2019-05-07 10:35:48 -0400

    colortool.19.04.29002

--- packages/colortool/tools/chocolateyInstall.ps1
+++ packages/colortool/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-$content = Join-Path (Split-Path $tools) 'content'
+﻿$tools = Split-Path -Path $MyInvocation.MyCommand.Definition
+$content = Join-Path -Path (Split-Path -Path $tools) -ChildPath 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'colortool' `
-    -Url 'https://github.com/Microsoft/console/releases/download/1810.02002/ColorTool.zip' `
-    -Checksum 'FA3373D02B0835502BF0FA643EF3D7CF247B082357A33C6BCEEE07416371B672' `
+    -Url 'https://github.com/microsoft/Terminal/releases/download/1904.29002/ColorTool.zip' `
+    -Checksum '2732E3E4E7BFDAA3230BAE7A9A43744E01E6F3C8C4A9627E52B14AD460F1C000' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
