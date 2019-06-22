0ae483ccb1edd265027177c47fcea4a8bb9dd83d
netscylla <github@netscylla.com>
2019-06-22 09:51:45 +0100

    add filelight package

---
+++ packages/filelight/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$tools = Split-Path -Path $MyInvocation.MyCommand.Definition
+$package = Split-Path -Path $tools
+$workdir = Join-Path -Path $package -ChildPath 'bin'
+$target = Join-Path -Path $workdir -ChildPath 'filelight.exe'
+$icon = Join-Path -Path $package -ChildPath 'filelight.ico'
+
+$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Filelight.lnk'
+
+Install-ChocolateyZipPackage `
+    -PackageName 'filelight' `
+    -Url 'https://download.kde.org/stable/filelight/17.12.0/filelight-17.12.0-win32.exe' `
+    -Checksum '6222e29d21b0a5f9c759aea26bce3e7f04c14e0e0a0a968e7ad267b44d509c20' `
+    -ChecksumType 'SHA256' `
+    -UnzipLocation $package
+
+Install-ChocolateyShortcut `
+    -IconLocation $icon `
+    -ShortcutFilePath $shortcut `
+    -TargetPath $target
+
+Get-ChildItem -Include '*.exe' -Path $workdir -Recurse `
+    | %{ New-Item -Path "$_.ignore" -Type File | Out-Null }
---
+++ packages/filelight/tools/chocolateyuninstall.ps1
@@ -0,0 +1,4 @@
+﻿$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+$shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'Filelight.lnk'
+
+Remove-Item -Path $shortcut -Force | Out-Null
