4b0533450700dd74604cae742f3c3f5401c585d2
Chris Privitere <cprivite@users.noreply.github.com>
2019-09-14 21:43:03 -0500

    Version Bump to 7.0.0

--- packages/cheatengine/tools/chocolateyInstall.ps1
+++ packages/cheatengine/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿Install-ChocolateyPackage `
+Install-ChocolateyPackage `
   -PackageName 'cheatengine' `
-  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/6.8.3/CheatEngine683.exe' `
-  -Checksum '961CD20DACA654599FB48030FE7C8EBB97E5423E44A194899BC93040EC0424C4' `
+  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/7.0/CheatEngine70.exe' `
+  -Checksum '7A8B67E21610207B3BD6012C0D798450D3127280A5EDC5E78C8026581DC021F4' `
   -ChecksumType 'SHA256' `
   -FileType 'EXE' `
   -Silent '/VERYSILENT /NORESTART /NOCANDY'
