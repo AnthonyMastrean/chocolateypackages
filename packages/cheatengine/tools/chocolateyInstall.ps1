4179c671cddcef1300c6730cf587044f6ee6d952
Anthony Mastrean <anthony.mastrean@gmail.com>
2019-09-26 10:58:02 -0400

    Delete cheatengine

--- packages/cheatengine/tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿Install-ChocolateyPackage `
-  -PackageName 'cheatengine' `
-  -Url 'https://github.com/cheat-engine/cheat-engine/releases/download/6.8.3/CheatEngine683.exe' `
-  -Checksum '961CD20DACA654599FB48030FE7C8EBB97E5423E44A194899BC93040EC0424C4' `
-  -ChecksumType 'SHA256' `
-  -FileType 'EXE' `
-  -Silent '/VERYSILENT /NORESTART /NOCANDY'
--- packages/cheatengine/tools/chocolateyUninstall.ps1
+++
@@ -1,5 +0,0 @@
-﻿Uninstall-ChocolateyPackage `
-  -PackageName 'cheatengine' `
-  -FileType 'EXE' `
-  -Silent '/VERYSILENT /NORESTART' `
-  -File (Get-UninstallRegistryKey -SoftwareName 'Cheat Engine 6.8.3').UninstallString.Trim('"')
