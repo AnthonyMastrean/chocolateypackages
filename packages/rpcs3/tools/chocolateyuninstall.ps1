89a444169ec0177441d7b31438634a210f565042
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-01-09 11:18:11 -0600

    RPCS3 update dependencies and change to statically icon link

--- packages/rpcs3/tools/chocolateyuninstall.ps1
+++ packages/rpcs3/tools/chocolateyuninstall.ps1
@@ -1,4 +1,5 @@
-﻿$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
+﻿$ErrorActionPreference = 'Stop'
+$shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
 $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'RPCS3.lnk'
 
 Remove-Item -Path $shortcut -Force | Out-Null
