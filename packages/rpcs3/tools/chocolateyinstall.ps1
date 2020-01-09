44717b3cc7e1ff7a4807d995ce2d79e7d6f7847f
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-01-09 11:17:02 -0600

    RPCS3 add AU update script and change install script for compatibility

--- packages/rpcs3/tools/chocolateyinstall.ps1
+++ packages/rpcs3/tools/chocolateyinstall.ps1
@@ -1,16 +1,19 @@
-﻿$content = Join-Path (Get-ToolsLocation) 'rpcs3'
+﻿$ErrorActionPreference = 'Stop'
+$content = Join-Path (Get-ToolsLocation) 'rpcs3'
 $target = Join-Path $content 'rpcs3.exe'
 $shortcutdir = @{$true='CommonPrograms';$false='Programs'}[($PSVersionTable.PSVersion -gt '2.0.0.0')]
 $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'RPCS3.lnk'
+$url64 = 'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-db5d03c3407f272966411ae97342f26ed60a648f/rpcs3-v0.0.8-9355-db5d03c3_win64.7z'
+$checksum64 = 'fc84caa1a16ea355ae549f88876542b96976aae6cf3b37b43fc1d9b6dc61682c'
 
 # This package must be installed in a low-privilege location. I could not get
 # it to run in the default %PROGRAMDATA%\Chocolatey\lib location. I was forced
 # to use the "tools" location.
 Install-ChocolateyZipPackage `
   -PackageName 'rpcs3' `
-  -Url 'https://github.com/RPCS3/rpcs3-binaries-win/releases/download/build-4037225e989ec798593d64e0ac1a1f887469d11e/rpcs3-v0.0.6-8135-4037225e_win64.7z' `
-  -Checksum '930B4581347762A3488C1A8CA66B8C15A478C9304EFACE518A4BF755202898F8' `
-  -ChecksumType 'SHA256' `
+  -Url64bit $url64 `
+  -Checksum64 $checksum64 `
+  -ChecksumType64 'SHA256' `
   -UnzipLocation $content
 
 Install-ChocolateyShortcut `
---
+++ packages/rpcs3/update.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module AU
+
+function global:au_SearchReplace {
+    @{
+        "tools\chocolateyInstall.ps1" = @{
+			"(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"          
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+		}
+	}
+}
+
+
+function global:au_GetLatest {
+	$download_page = Invoke-WebRequest -Uri https://rpcs3.net/download  -UseBasicParsing
+	$regex       = 'rpcs3-.*_win64.7z'
+    
+	$url         = $download_page.links | ? href -match $regex | select -First 1 -expand href
+	$version       = (($url -split '[-]' | select -Last 2 -Skip 1) -join "-").tostring().Substring(1).replace('-', '-alpha')
+	
+	return @{ Version = $version; URL64 = $url }
+}
+
+Update-Package -checksumfor 64
