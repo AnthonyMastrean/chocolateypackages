01443627467c321a9f54808f5a81de75ed29d32a
Andrew M. Stoltz <andrew.stoltz@chrobinson.com>
2020-02-03 11:03:48 -0600

    Upgrade dotCover to 2019.3.1

--- packages/dotcover-cli/tools/chocolateyInstall.ps1
+++ packages/dotcover-cli/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@ $content = Join-Path -Path (Split-Path -Path $tools) -ChildPath 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'dotcover-cli' `
-    -Url 'https://download.jetbrains.com/resharper/ReSharperUltimate.2019.1/JetBrains.dotCover.CommandLineTools.2019.1.zip' `
-    -Checksum 'baaba77ff70041b6ff2c2033ee8ef3e26661f76faaf4646c4c9317ce0525133a' `
+    -Url 'https://download.jetbrains.com/resharper/ReSharperUltimate.2019.3.1/JetBrains.dotCover.CommandLineTools.2019.3.1.zip' `
+    -Checksum '52d748750cd270756da2e08e4bb587137ab0441a4037be481555f044b4c75834' `
     -ChecksumType 'SHA256' `
     -UnzipLocation $content
 
