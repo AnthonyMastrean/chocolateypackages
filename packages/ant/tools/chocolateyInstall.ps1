dcdfe7e80590db4fd9a965674b94e31776f716af
Jeremy Cornett <jeremy.cornett@forcepoint.com>
2020-02-11 09:54:46 -0700

    Update to Ant 1.10.7.

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$ant_home = Join-Path $package 'apache-ant-1.10.5'
+$ant_home = Join-Path $package 'apache-ant-1.10.7'
 $ant_bat = Join-Path $ant_home 'bin/ant.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'ant' `
-    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.zip' `
-    -Checksum '2E48F9E429D67708F5690BC307232F08440D01EBE414059292B6543971DA9C7CD259C21533B9163B4DD753321C17BD917ADF8407D03245A0945FC30A4E633163' `
+    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.7-bin.zip' `
+    -Checksum '4afbf7d474b38da07992e820f22f2979e28fa8db71997ccb8ee63fe5cb32478fe26e1675feabb70321af24f25a37747c0f6d198a9e43ba747b65e3f8076f3dff' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $package
 
