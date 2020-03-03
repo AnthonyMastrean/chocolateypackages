05c10883d29f1ad01616b05e6aa1059eb642dc98
MJECloud <mjecld@outlook.com>
2020-03-03 09:13:55 +0100

    update op package to 0.9.2

--- packages/op/tools/chocolateyInstall.ps1
+++ packages/op/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@ $content = Join-Path (Split-Path $tools) 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'op' `
-    -Url 'https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_windows_386_v0.5.5.zip' `
-    -Checksum 'DCFB3376CE93D2C529C1C723A00E1FD1506CB30E9064B2998C3B3B505F7CFD73' `
+    -Url 'https://cache.agilebits.com/dist/1P/op/pkg/v0.9.2/op_windows_386_v0.9.2.zip' `
+    -Checksum 'B9083DC349B59F4D82EB138D6349B93739CC7F7882630D4A018D8E163D9CB457' `
     -ChecksumType 'SHA256' `
-    -Url64 'https://cache.agilebits.com/dist/1P/op/pkg/v0.5.5/op_windows_amd64_v0.5.5.zip' `
-    -Checksum64 '520C743040F9D989F1D441EE38135182E8AE8A8A8050BF8A496EB7689602E4B0' `
+    -Url64 'https://cache.agilebits.com/dist/1P/op/pkg/v0.9.2/op_windows_amd64_v0.9.2.zip' `
+    -Checksum64 'E6E326258DFE6421E8214C06E1FCCBF319708ABAD49AD42433B49E42A0634FEA' `
     -ChecksumType64 'SHA256' `
     -UnzipLocation $content
