ed76a41c565396bfacac39eb5e19428edeea9b48
TheJanzap <jannis.tschan@gmail.com>
2020-06-03 14:30:49 +0200

    Update prime95 to 29.8.6

--- packages/prime95/tools/chocolateyInstall.ps1
+++ packages/prime95/tools/chocolateyInstall.ps1
@@ -7,10 +7,10 @@ $shortcut = Join-Path ([System.Environment]::GetFolderPath($shortcutdir)) 'prime
 
 Install-ChocolateyZipPackage `
     -PackageName 'prime95' `
-    -Url 'http://www.mersenne.org/ftp_root/gimps/p95v294b7.win32.zip' `
+    -Url 'http://www.mersenne.org/ftp_root/gimps/p95v298b6.win32.zip' `
     -Checksum '896A2F90D198AA4A6D1FFC912FCE10F30A0BBC127F711BE64871E2887A48B283' `
     -ChecksumType 'SHA256' `
-    -Url64 'http://www.mersenne.org/ftp_root/gimps/p95v294b8.win64.zip' `
+    -Url64 'http://www.mersenne.org/ftp_root/gimps/p95v298b6.win64.zip' `
     -Checksum64 '00C24E7D131529A08D0C0C268CC8DC1349F69C3B46D04B7FCAA391B373B184D6' `
     -ChecksumType64 'SHA256' `
     -UnzipLocation $content
