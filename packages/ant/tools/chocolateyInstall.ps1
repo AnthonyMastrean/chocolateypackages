9fa7555fb53d94e32b3d5e40543f38efc754eff0
Zhiyuan Lin <2121340+zhiyuan-lin@users.noreply.github.com>
2020-06-05 00:37:53 +0900

    Updated Apache Ant to version 1.10.8

--- packages/ant/tools/chocolateyInstall.ps1
+++ packages/ant/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $ant_bat = Join-Path $ant_home 'bin/ant.bat'
 
 Install-ChocolateyZipPackage `
     -PackageName 'ant' `
-    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.5-bin.zip' `
-    -Checksum '2E48F9E429D67708F5690BC307232F08440D01EBE414059292B6543971DA9C7CD259C21533B9163B4DD753321C17BD917ADF8407D03245A0945FC30A4E633163' `
+    -Url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.10.8-bin.zip' `
+    -Checksum '24A49F9EFD19D1202700192BA811E4A0A714B2E16A37CA8124309DDEC6754A22FA12C7E5A07D35A41A104C6B6BB514D5C99A2438B758E5B6C75CC583A2B2385F' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $package
 
