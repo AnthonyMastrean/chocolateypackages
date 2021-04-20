561c9a36db20ad213751dcbeb13828ee527173c7
dgalbraith <dg@lbraith.io>
2021-04-20 08:04:19 +0100

    Remove rsync package

--- packages/rsync/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
-
-# This package requires the CWRSYNC_HOME directory on the PATH and a HOME
-# environment variable. This cannot be provided with Chocolatey's automatic
-# shimming. I have to shim custom batch files.
-Get-ChildItem $tools\*.bat | %{ Uninstall-BinFile -Name $_.BaseName -Path $_ }
